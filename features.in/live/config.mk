cf81cc678590356ed8ff18330917d30d65a0ed7b
Michael Shigorin <mike@altlinux.org>
2011-08-30 00:11:31 +0300

    live, rescue, and use tags
    It's still very immature -- base lists should be really base,
    and functionality should be sorted out in more consistent way.
    So far moved to tagged lists (which need some more experimentation
    anyways to get both lists and their use more elegant).
    
    - fixed live.iso (now actually useful):
      + extra cleanup was being done (coming from install2 case)
      + root user was password blocked
      + there was no unprivileged user (added "altlinux" w/o password)
      + added xdm setup hook for future X-based livecd flavours
      + several picks for a less slim "base" list
      + xdm login
    - tweaked rescue.iso (added ext3grep)

--- features.in/live/config.mk
+++ features.in/live/config.mk
@@ -1,3 +1,3 @@
 use/live: use/stage2 sub/stage2/live
 	@$(call add,FEATURES,live)
-#	@$(call add,LIVE_LISTS,base-live)
+	@$(call add,LIVE_LISTS,$(call tags,(base || desktop) && (live || network || icewm)))
--- features.in/rescue/config.mk
+++ features.in/rescue/config.mk
@@ -1,3 +1,4 @@
 use/rescue: use/stage2 sub/stage2/rescue
 	@$(call add,FEATURES,rescue)
-	@$(call add,RESCUE_LISTS,base-rescue)
+	@$(call add,RESCUE_LISTS,$(call tags,base && (rescue || network)))
+	@$(call add,RESCUE_LISTS,$(call tags,extra network))
