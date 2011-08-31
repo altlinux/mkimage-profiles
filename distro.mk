4dfd689810993cfe44244777916ac5e809059166
Michael Shigorin <mike@altlinux.org>
2011-08-31 22:41:34 +0300

    minicd.iso -> server-mini.iso (and more tags)
    minicd target was a hasty hack indeed, let's put it in line;
    an attempt at practical use discovered the lack of important
    packages like dhcpcd, these were added on per package basis
    and quickly remade to use tags -- which seem to fit better.

--- distro.mk
+++ distro.mk
@@ -49,22 +49,25 @@ distro/server-base: distro/installer sub/main \
 	use/syslinux/ui-menu use/memtest use/bootloader/grub
 	@$(call add,BASE_LISTS,server-base)
 
+distro/server-mini: distro/server-base
+	@$(call set,KFLAVOURS,el-smp)
+	@$(call add,KMODULES,e1000e igb)
+	@$(call add,BASE_LISTS,\
+		$(call tags,base && (server || network || security || pkg)))
+	@$(call add,BASE_LISTS,$(call tags,extra network))
+
 distro/server-ovz: distro/server-base use/hdt use/rescue use/firmware/server
 	@$(call set,STAGE1_KFLAVOUR,std-def)
 	@$(call set,KFLAVOURS,std-def ovz-el)
 	@$(call add,KMODULES,bcmwl e1000e igb ndiswrapper rtl8168 rtl8192)
 	@$(call add,KMODULES,ipset ipt-netflow opendpi pf_ring xtables-addons)
 	@$(call add,KMODULES,drbd83 kvm)
-	@$(call add,MAIN_LISTS,kernel-wifi)
 	@$(call add,BASE_LISTS,ovz-server)
 	@$(call add,BASE_LISTS,$(call tags,base server))
+	@$(call add,MAIN_LISTS,kernel-wifi)
 	@$(call add,GROUPS,dns-server http-server ftp-server kvm-server)
 	@$(call add,GROUPS,ipmi mysql-server dhcp-server mail-server)
 	@$(call add,GROUPS,monitoring diag-tools)
-
-distro/minicd: distro/server-base
-	@$(call set,KFLAVOURS,pure-emerald)	# usually recent drivers
-	@$(call add,MAIN_PACKAGES,etcnet-full)
 
 # desktop distributions
 
