720a579690077cc445b5d77810719abbf4e547a8
Michael Shigorin <mike@altlinux.org>
2011-09-19 10:55:45 +0300

    wash, rinse, repeat
    install2 cleanups:
    
    - functionally indifferent ones: particularly, install2/*/98system's
      "mkdir -p /image" was superfluous as it was done by that time already
      by sub.in/stage2/image-scripts.d/00stage1
    
    - taken apart, prepared for tags: so far it's a mostly moot change
      since the installer cleanup scripts themselves are mostly the same as
      preceding 90cleanup was (with some additions corresponding to recent
      kernel development); it's still unclear what the mechanism for
      configuring the cleanups in effect will be, either directory/package
      regex lists or tagged scripts excluded from execution by yet another tag
    
    fixes:
    
    - image.in/Makefile: fix metadata related test; the actual test was
      assuming that stage1 kernel means installer, which is not the case
      since generic stage2 introduction; oh well
    
    - 85cleanup-lowmem: a "_" too much was the culprit in destroying the
      needed translations along with those deemed superfluous; thanks go to
      Oleg Ivanov and Lenar Shakirov for finding the bug and proposing the
      fix altogether
    
    additions:
    
    - features.in/Makefile: reworked help target; it was rather inaccessible
      due to BUILDDIR normally undefined at the time of direct make
      invocation, and BUILDDIR is normally defined during normal builds
      anyways so let's try it this way.
    
    - README++
    
    daydreams:
    
    - 01-genbasedir: we should drop bzip2 compressed pkglists some day
      but see genbasedir and apt-cdrom first, 90-pkg.sh (alterator-pkg)
      will fail miserably otherwise

--- features.in/firmware/config.mk
+++ features.in/firmware/config.mk
@@ -9,12 +9,10 @@ use/firmware/server: use/firmware
 	@$(call add,SYSTEM_PACKAGES,firmware-aic94xx-seq)
 	@$(call add,MAIN_PACKAGES_REGEXP,firmware-ql.*)
 
-use/firmware/desktop: use/firmware
-	@$(call add,SYSTEM_PACKAGES,firmware-psb)
-
 use/firmware/wireless: use/firmware
 	@$(call add,MAIN_PACKAGES,firmware-acx100)
 	@$(call add,MAIN_PACKAGES,firmware-i2400m)
+	@$(call add,MAIN_PACKAGES,firmware-carl9170)
 	@$(call add,MAIN_PACKAGES_REGEXP,firmware-ipw.*)
 	@$(call add,MAIN_PACKAGES_REGEXP,firmware-iwl.*)
 	@$(call add,MAIN_PACKAGES_REGEXP,firmware-rt.*)
--- features.in/hdt/config.mk
+++ features.in/hdt/config.mk
@@ -2,5 +2,3 @@ use/hdt: use/syslinux use/memtest
 	@$(call add,FEATURES,hdt)
 	@$(call add,STAGE1_PACKAGES,pciids)
 	@$(call add,SYSLINUX_MODULES,hdt)
-### moved to scripts.d/02-hdt along with compression
-#	@$(call add,SYSLINUX_FILES,/usr/share/misc/pci.ids)
