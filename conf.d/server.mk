781c98bb3dcdee4623539fd17bb2dc52ce085d9f
Michael Shigorin <mike@altlinux.org>
2011-11-16 14:27:39 +0200

    server-ovz: rebase onto server-mini
    The culprit was missing dhcpcd, although the rest
    of niceties already in server-mini were welcome to
    server-ovz either.

--- conf.d/server.mk
+++ conf.d/server.mk
@@ -14,16 +14,14 @@ distro/server-mini: distro/server-base use/cleanup/x11-alterator
 
 distro/server-systemd: distro/server-mini use/systemd
 
-distro/server-ovz: distro/server-base \
-	use/hdt use/rescue use/firmware/server use/power/acpi/button \
-	use/cleanup/x11-alterator
+distro/server-ovz: distro/server-mini \
+	use/hdt use/rescue use/firmware/server use/power/acpi/button
 	@$(call set,STAGE1_KFLAVOUR,std-def)
 	@$(call set,KFLAVOURS,std-def ovz-el)
-	@$(call add,KMODULES,bcmwl e1000e igb ndiswrapper rtl8168 rtl8192)
+	@$(call add,KMODULES,bcmwl ndiswrapper rtl8168 rtl8192)
 	@$(call add,KMODULES,ipset ipt-netflow opendpi pf_ring xtables-addons)
 	@$(call add,KMODULES,drbd83 kvm)
 	@$(call add,BASE_LISTS,ovz-server)
-	@$(call add,BASE_LISTS,$(call tags,base server))
 	@$(call add,MAIN_LISTS,kernel-wifi)
 	@$(call add,MAIN_GROUPS,dns-server http-server ftp-server kvm-server)
 	@$(call add,MAIN_GROUPS,ipmi mysql-server dhcp-server mail-server)
