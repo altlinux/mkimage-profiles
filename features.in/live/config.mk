cd998fef41de98f3b8ed866ecd5cdc1a8e1a56f9
Michael Shigorin <mike@altlinux.org>
2011-11-28 14:59:55 +0200

    initial live autologin
    use/live/autologin target tries hard to configure any available
    autologin means, including a dedicated package and a few DMs.
    
    Thanks gns@ as liveflash.eeepc got robbed somewhat.

--- features.in/live/config.mk
+++ features.in/live/config.mk
@@ -4,8 +4,13 @@ use/live: use/stage2 sub/stage2/live
 use/live/base: use/live use/syslinux/ui-menu
 	@$(call add,LIVE_LISTS,$(call tags,base && (live || network)))
 
-use/live/icewm: use/live/base
-	@$(call add,LIVE_LISTS,\
+# NB: there's an unconditional live/image-scripts.d/40-autologin script
+#     *but* it only configures some of the *existing* means; let's add one
+use/live/autologin: use/live/base
+	@$(call add,LIVE_PACKAGES,autologin xinit)
+
+use/live/icewm: use/live/autologin
+	@$(call add,LIVE_LISTS, \
 		$(call tags,(base || desktop) && (live || network || icewm)))
 
 # this is a manual installation script (text-based)
