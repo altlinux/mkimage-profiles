41a0b6305124a6059f159ce45a5c032272b42039
Michael Shigorin <mike@altlinux.org>
2011-12-19 22:06:27 +0200

    conf.d/desktop.mk: split out distro/desktop-mini
    Let's prepare some moderately sane base for future desktop
    installers, it's not only icewm around anymore.

--- conf.d/desktop.mk
+++ conf.d/desktop.mk
@@ -4,10 +4,11 @@ ifeq (distro,$(IMAGE_CLASS))
 distro/desktop-base: distro/installer use/repo/main \
 	use/syslinux/ui-vesamenu use/x11/xorg use/bootloader/grub
 
-distro/icewm: distro/desktop-base \
-	use/lowmem use/x11/xdm use/x11/runlevel5 \
-	use/bootloader/lilo use/power/acpi/button \
-	use/cleanup/alterator
+distro/desktop-mini: distro/desktop-base \
+	use/lowmem use/x11/xdm use/bootloader/lilo \
+	use/power/acpi/button use/cleanup/alterator
+
+distro/icewm: distro/desktop-mini
 	@$(call add,BASE_LISTS,$(call tags,icewm desktop))
 
 distro/desktop-systemd: distro/icewm use/systemd
