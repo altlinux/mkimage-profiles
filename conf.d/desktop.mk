5a7b6b5739b42592c893b6ccc46fa446b9f8a287
Michael Shigorin <mike@altlinux.org>
2012-01-11 12:25:54 +0200

    icewm.iso: use desktop installer
    It's burdensome for testing but at least the result
    is somewhat useful then.

--- conf.d/desktop.mk
+++ conf.d/desktop.mk
@@ -3,6 +3,7 @@ ifeq (distro,$(IMAGE_CLASS))
 
 distro/desktop-base: distro/installer use/repo/main \
 	use/syslinux/ui-vesamenu use/x11/xorg use/bootloader/grub
+	@$(call set,INSTALLER,desktop)
 
 distro/desktop-mini: distro/desktop-base \
 	use/lowmem use/x11/xdm use/bootloader/lilo \
