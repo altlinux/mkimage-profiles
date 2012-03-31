3d649db65677d53771ef1b2ad77a36bddb855f68
Michael Shigorin <mike@altlinux.org>
2012-03-31 16:14:00 +0300

    live: introduce homepage redirector knobs
    There are three more variables to adjust the redirect page:
    HOMEPAGE, HOMENAME and HOMEWAIT; e.g. live-webkiosk is now
    quite a bit more whitelabel.

---
+++ features.in/live/generate.mk
@@ -0,0 +1,37 @@
+# set up livecd browser redirection page
+
+ifdef BUILDDIR
+
+include $(BUILDDIR)/distcfg.mk
+
+ifndef HOMEPAGE
+HOMEPAGE = http://www.altlinux.org/
+endif
+
+ifndef HOMENAME
+HOMENAME = ALT Linux
+endif
+
+ifndef HOMEWAIT
+HOMEWAIT = 3
+endif
+
+INDEXHTML := $(BUILDDIR)/stage1/files/index.html
+
+all: debug
+	@if [ -s "$(INDEXHTML)" ]; then \
+		sed -i \
+			-e 's,@homepage@,$(HOMEPAGE),' \
+			-e 's,@homename@,$(HOMENAME),' \
+			-e 's,@homewait@,$(HOMEWAIT),' \
+			$(INDEXHTML); \
+	fi
+
+debug:
+	@if [ -n "$(DEBUG)" ]; then \
+		echo "** HOMEPAGE: $(HOMEPAGE)"; \
+		echo "** HOMENAME: $(HOMENAME)"; \
+		echo "** HOMEWAIT: $(HOMEWAIT)"; \
+	fi
+
+endif
