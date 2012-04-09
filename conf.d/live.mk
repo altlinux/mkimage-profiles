1432bb1607d5e2743c38407ab00315345dc72fc5
Gleb Fotengauer-Malinovskiy <glebfm@altlinux.org>
2012-04-09 15:44:55 +0400

    add dummy distro/livecd-install
    sub.in/stage2/image-scripts.d/99cleanup-boot,
    livecd-install need /boot files too

--- conf.d/live.mk
+++ conf.d/live.mk
@@ -30,6 +30,9 @@ distro/live-builder: distro/.live-base use/dev/mkimage use/dev/repo
 distro/live-install: distro/.live-base use/syslinux/localboot.cfg
 	@$(call add,LIVE_PACKAGES,live-install)
 
+distro/livecd-install: distro/.live-base use/syslinux/localboot.cfg
+	@$(call add,LIVE_PACKAGES,livecd-install)
+
 distro/live-icewm: distro/.live-desktop use/live/autologin +icewm; @:
 distro/live-razorqt: distro/.live-desktop +razorqt; @:
 distro/live-tde: distro/.live-desktop use/live/ru +tde; @:
