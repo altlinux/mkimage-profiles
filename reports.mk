6e6f572b43c0db8ce1b830a2b08d4f5808170eb0
Michael Shigorin <mike@altlinux.org>
2012-04-23 21:56:35 +0300

    better output directory and results structuring
    Following m-p-d, a more involved default output directory
    structure is feasible now:
    
      ~/out/name/date/name-date-arch.type
    
    instead of plain
    
      ~/out/name-date-arch.type
    
    This particular behaviour can be achieved by passing
    SORTDIR='$(IMAGE_NAME)/$(DATE)'; note the single quotes.
    
    Reports are also saved in this resulting structure
    albeit the place is still highly debatable.

--- reports.mk
+++ reports.mk
@@ -2,20 +2,30 @@
 
 ifneq (,$(REPORT))
 
-BUILDDIR := $(shell sed -n 's/^.* BUILDDIR = \(.*\)/\1/p' "$(REPORT_PATH)")
-
 include lib/common.mk
 
+BUILDDIR := $(shell sed -n 's/^.* BUILDDIR = \(.*\)/\1/p' "$(REPORT_PATH)")
+BUILDLOG := $(BUILDDIR)/$(BUILD_LOG)
+REPORTDIR := $(BUILDDIR)/reports/
+IMAGE_OUTPATH := $(shell sed -n 's/^IMAGE_OUTPATH = \(.*\)/\1/p' $(BUILDLOG))
+
+# for a multi-image build there's no sense to refer to buildroot
+# contained reports as these are very ephemeral between builds
+ifneq (1,$(NUM_TARGETS))
+SHORTEN := >/dev/null
+endif
+
 all: reports/targets reports/scripts
+	@cp -a $(REPORTDIR) $(IMAGE_OUTPATH).reports
 
 reports/prep:
-	@mkdir -p "$(BUILDDIR)/reports/"
+	@mkdir -p "$(REPORTDIR)"
 
 reports/scripts: reports/prep
-	@grep "^mki.*scripts: Run: " $(BUILDDIR)/$(BUILD_LOG) \
+	@grep "^mki.*scripts: Run: " $(BUILDLOG) \
 	| sed -rn "s,^.*($(BUILDDIR)|$(SYMLINK))/(.*)'$$,\2,p" \
-	> "$(BUILDDIR)/$@.log" \
-	&& echo "** scripts report: $(BUILDDIR)/$@.log" $(SHORTEN)
+	> "$(REPORTDIR)/$*.log" \
+	&& echo "** scripts report: $(BUILDDIR)/$(@F).log" $(SHORTEN)
 
 reports/targets: reports/prep
 	@if ! [ -n "$(REPORT_PATH)" -a -s "$(REPORT_PATH)" ]; then \
@@ -32,7 +42,7 @@ reports/targets: reports/prep
 		&& echo "** graphviz missing, " \
 			"target graph dot file: $$REPORT_DOT"; \
 	fi $(SHORTEN); \
-	mv "$(REPORT_PATH)" "$(BUILDDIR)/$@.log"
+	mv "$(REPORT_PATH)" "$(REPORTDIR)/$(@F).log"
 
 else
 all:; @:
