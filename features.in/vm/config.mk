a30446969958a0b58490e7cae443f2d6ac5eaca1
Michael Shigorin <mike@altlinux.org>
2012-05-16 15:17:57 +0300

    vm: tweak virtualbox modules
    virtualbox-addition modules need drm (see also #27344).

--- features.in/vm/config.mk
+++ features.in/vm/config.mk
@@ -2,7 +2,7 @@
 
 use/vm/virtualbox/guest:
 	@$(call add_feature)
-	@$(call add,THE_KMODULES,virtualbox-addition)
+	@$(call add,THE_KMODULES,virtualbox-addition drm)
 	@$(call add,THE_PACKAGES,virtualbox-guest-additions)
 
 # NB: only reasonable for X11-bearing images
