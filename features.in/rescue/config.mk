70793436c71450f2dfcafe996cab8aa720def140
Michael Shigorin <mike@altlinux.org>
2012-08-08 20:27:59 +0400

    rescue += openssh
    Found myself pretty silly while sittin' at the rescue console
    and bein' unable to leave the cool server room for a way
    more comfortable armchair and a laptop's keyboard...
    
    (yes, it was that disk array needing GPT tools)

--- features.in/rescue/config.mk
+++ features.in/rescue/config.mk
@@ -1,4 +1,4 @@
 use/rescue: use/stage2 sub/stage2/rescue
 	@$(call add_feature)
-	@$(call add,RESCUE_LISTS, \
+	@$(call add,RESCUE_LISTS, openssh \
 		$(call tags,(base || extra) && (rescue || network)))
