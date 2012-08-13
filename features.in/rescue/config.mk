3a3893b664225e1eb2d923c843cb78592c3eded0
Michael Shigorin <mike@altlinux.org>
2012-08-13 15:51:53 +0400

    rescue += firmware (server, wireless)
    There's no use of a rescue image when it has no firmware
    for the controller in the system being worked on...
    so added some.

--- features.in/rescue/config.mk
+++ features.in/rescue/config.mk
@@ -1,4 +1,5 @@
-use/rescue: use/stage2 sub/stage2@rescue use/syslinux/sdab.cfg
+use/rescue: use/stage2 sub/stage2@rescue use/syslinux/sdab.cfg \
+	use/firmware/server use/firmware/wireless
 	@$(call add_feature)
 	@$(call add,RESCUE_LISTS, openssh \
 		$(call tags,(base || extra) && (rescue || network)))
