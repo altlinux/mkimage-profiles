aaddfb740c45a5cd26c1300ee09ec9393620ec86
Michael Shigorin <mike@altlinux.org>
2012-10-16 12:13:48 +0300

    stage2: cope with current make-initrd-propagator
    The issue that appeared pretty hard to diagnose occured
    to be the enhancement made in make-initrd-propagator=0.8.1-alt1.2
    (that didn't hit Sisyphus until merged into 0.10-alt1) which
    drops propagator dependency.
    
    And that was optimized out in m-p, of course.

--- features.in/stage2/config.mk
+++ features.in/stage2/config.mk
@@ -1,7 +1,7 @@
 # "1" is not a typo
 use/stage2: sub/stage1
 	@$(call add_feature)
-	@$(call add,STAGE1_PACKAGES,file make-initrd make-initrd-propagator)
+	@$(call add,STAGE1_PACKAGES,file make-initrd make-initrd-propagator propagator)
 
 use/stage2/kms:
 	@$(call add,STAGE1_KMODULES_REGEXP,drm.*)
