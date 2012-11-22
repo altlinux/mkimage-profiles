4df837b9d368e8b4d259dc80967f1235e2d72ed4
Michael Shigorin <mike@altlinux.org>
2012-11-22 11:16:21 +0200

    live.mk: split live-builder into proper and -mini
    While ildar@ has some reason for the slimmer image
    the somewhat standalone one is documented in examples
    for offline use, ruining it in-place is not an option.
    
    Let's just do a split (and lose a target-specific variable
    example in favour of a commodity pkglist by the way; oh well).

--- conf.d/live.mk
+++ conf.d/live.mk
@@ -15,15 +15,18 @@ distro/.live-kiosk: distro/.base use/live/base use/live/autologin \
 	@$(call add,LIVE_PACKAGES,fonts-ttf-dejavu)
 	@$(call add,CLEANUP_PACKAGES,'alterator*' 'guile*' 'vim-common')
 
-distro/live-builder: pkgs := livecd-tmpfs livecd-online-repo mkimage-profiles
-distro/live-builder: distro/.live-base use/dev/mkimage use/dev \
+distro/live-builder-mini: distro/.live-base use/dev/mkimage use/dev \
 	use/syslinux/timeout/30
 	@$(call set,KFLAVOURS,$(BIGRAM))
-	@$(call add,LIVE_LISTS,$(call tags,base && (server || builder)))
+	@$(call add,LIVE_LISTS,\
+		$(call tags,(base || live) && (server || builder)))
 	@$(call add,LIVE_PACKAGES,livecd-qemu-arch strace)
 	@$(call add,LIVE_PACKAGES,qemu-user-binfmt_misc)
 	@$(call add,LIVE_PACKAGES,zsh sudo)
-	@$(call add,LIVE_PACKAGES,$(pkgs))
+
+distro/live-builder: distro/live-builder-mini use/dev/repo
+	@$(call add,MAIN_LISTS,$(call tags,live builder))
+	@$(call add,MAIN_PACKAGES,syslinux pciids memtest86+ mkisofs)
 
 distro/live-install: distro/.live-base use/live/textinstall; @:
 distro/.livecd-install: distro/.live-base use/live/install; @:
