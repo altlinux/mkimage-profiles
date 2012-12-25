d0d76633bb99b1ee89d8cc945ecdb35aa2cffe88
Michael Shigorin <mike@altlinux.org>
2012-12-25 22:06:26 +0200

    initial regular images
    See http://www.altlinux.org/Регулярные_сборки_образов
    for the idea behind this set of images; in short, it's
    *not* a bunch of polished end-user distros in a moment
    but rather to facilitate Sisyphus package base testing
    in as-is state during branch pre-release time.

---
+++ conf.d/regular.mk
@@ -0,0 +1,37 @@
+# regular build/usage images
+ifeq (distro,$(IMAGE_CLASS))
+
+# TODO: use/plymouth/live when luks+plymouth is done, see also #28255
+distro/.regular-desktop: distro/.base +live use/live/ru \
+	use/live/install use/live/repo use/x11/3d-free use/systemd \
+	use/efi use/luks +vmguest use/memtest \
+	use/branding use/syslinux/ui/gfxboot
+	@$(call add,LIVE_PACKAGES,openssh strace alterator-standalone)
+	@$(call add,LIVE_PACKAGES,cpufreq-simple)
+	@$(call add,LIVE_PACKAGES,livecd-online-repo apt-repo)
+	@$(call add,LIVE_PACKAGES,xdg-user-dirs)
+	@$(call add,LIVE_PACKAGES,synaptic-usermode)
+	@$(call add,LIVE_PACKAGES,firefox-ru)
+	@$(call add,THE_BRANDING,indexhtml notes alterator bootloader)
+	@$(call try,SAVE_PROFILE,yes)
+
+distro/.regular-gtk: distro/.regular-desktop use/x11/gdm2.20; @:
+
+distro/regular-icewm: distro/.regular-gtk +icewm; @:
+distro/regular-xfce: distro/.regular-gtk use/x11/xfce; @:
+distro/regular-lxde: distro/.regular-gtk use/x11/lxde; @:
+
+distro/regular-mate: distro/.regular-gtk
+	@$(call add,LIVE_LISTS,$(call tags,(desktop || mobile) && (mate || nm)))
+
+distro/regular-e17: distro/.regular-gtk use/x11/e17; @:
+	@$(call add,LIVE_PACKAGES,xterm xorg-xnest)
+
+distro/regular-cinnamon: distro/.regular-desktop use/x11/cinnamon
+	@$(call set,META_VOL_ID,ALT Linux $(IMAGE_NAME)) # see also #28271
+
+distro/regular-tde: distro/.regular-desktop +tde; @:
+distro/regular-kde4: distro/.regular-desktop use/x11/kde4 use/x11/kdm4; @:
+distro/regular-razorqt: distro/.regular-desktop +razorqt; @:
+
+endif
