cd0db8d3639886d737178f53a548ed824a78f7fa
Michael Shigorin <mike@altlinux.org>
2012-12-26 17:21:24 +0400

    efi: handle rescue as well
    The rescue feature intentionally doesn't pick up THE_PACKAGES
    and THE_LISTS into stage2, so add EFI ones explicitly.

--- features.in/efi/config.mk
+++ features.in/efi/config.mk
@@ -1,9 +1,12 @@
 ifeq (x86_64,$(ARCH))
 
+EFI_LISTS := $(call tags,base efi)
+
 use/efi:
 	@$(call add_feature)
 	@$(call set,MKI_VER_MINIMAL,0.2.5)	# see #28219
-	@$(call add,THE_LISTS,$(call tags,base efi))
+	@$(call add,THE_LISTS,$(EFI_LISTS))
+	@$(call add,RESCUE_LISTS,$(EFI_LISTS))
 	@$(call add,INSTALL2_PACKAGES,dosfstools)
 	@$(call set,EFI_BOOTLOADER,elilo)	### no choice right now
 
