dd140074b980c7e16a08637c172d2619321d1b53
Michael Shigorin <mike@altlinux.org>
2013-01-20 19:17:18 +0400

    wireless: added crda package
    See also http://wireless.kernel.org/en/developers/Regulatory/#CRDA

--- features.in/wireless/config.mk
+++ features.in/wireless/config.mk
@@ -3,4 +3,4 @@
 use/wireless:
 	@$(call add_feature)
 	@$(call add,THE_KMODULES,bcmwl rt3070 staging)
-	@$(call add,THE_PACKAGES,wireless-tools rfkill)
+	@$(call add,THE_PACKAGES,wireless-tools rfkill crda)
