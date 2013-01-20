b603622dcde40209e8ddb510c58e0ce20ca0a849
Michael Shigorin <mike@altlinux.org>
2013-01-20 19:23:58 +0400

    wireless: added iw as well
    It's a next-get CLI for wireless interfaces, see also
    http://linuxwireless.org/en/users/Documentation/iw

--- features.in/wireless/config.mk
+++ features.in/wireless/config.mk
@@ -3,4 +3,4 @@
 use/wireless:
 	@$(call add_feature)
 	@$(call add,THE_KMODULES,bcmwl rt3070 staging)
-	@$(call add,THE_PACKAGES,wireless-tools rfkill crda)
+	@$(call add,THE_PACKAGES,wireless-tools rfkill crda iw)
