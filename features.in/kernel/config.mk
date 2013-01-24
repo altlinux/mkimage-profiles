2d3e5f2b1ce57b58ecffbc346e5841640bfaad91
Michael Shigorin <mike@osdn.org.ua>
2013-01-25 00:55:56 +0400

    kernel: factor the former lib/kernel.mk in
    It wasn't spotted when introducing the distinct kernel feature;
    will need fixup as we meet packaged ARM/PPC kernels.

--- features.in/kernel/config.mk
+++ features.in/kernel/config.mk
@@ -1,3 +1,10 @@
+# choose std kernel flavour for max RAM size support
+ifeq (i586,$(ARCH))
+BIGRAM := std-pae
+else
+BIGRAM := std-def
+endif
+
 use/kernel:
 	@$(call add_feature)
 	@$(call set,KFLAVOURS,std-def)
