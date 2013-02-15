5f44404182f910750d8f9276a8cf41d281792a57
Michael Shigorin <mike@altlinux.org>
2013-02-15 20:13:05 +0300

    initial fonts feature
    This includes an updated version of 50-fontconfig script
    which actually works (the preliminary one attached to #28612
    didn't); thanks zerg@ and cow@ for providing the incentive
    to introduce it.
    
    Based on m-p-d and installer-feature-kdesktop-fontconfig.

---
+++ features.in/fonts/config.mk
@@ -0,0 +1,11 @@
+use/fonts:
+	@$(call add_feature)
+	@$(call xport,FONT_FEATURES_ENABLE)
+	@$(call xport,FONT_FEATURES_DISABLE)
+
+# just stating that kernels and font habits are pretty individual
+use/fonts/zerg: use/fonts
+	@$(call set,FONT_FEATURES_ENABLE,antialias lcdfilter-default \
+		hinting style-full sub-pixel-rgb)
+	@$(call set,FONT_FEATURES_DISABLE,no-antialias lcdfilter-none \
+		unhinted no-sub-pixel)
