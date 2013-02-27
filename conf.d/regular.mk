83705cd8abeb020744f5e72453b61f544cc1182f
Michael Shigorin <mike@altlinux.org>
2013-02-27 17:47:55 +0400

    regular-kde4: refactored and extended pkglist
    The metapackage was fine, the "only one" additional package
    was more or less okay, but there came a dozen more so it's
    now reasonable to stash these into a separate pkglist.

--- conf.d/regular.mk
+++ conf.d/regular.mk
@@ -51,8 +51,7 @@ distro/regular-tde: distro/.regular-desktop +tde +plymouth
 	@$(call add,LIVE_PACKAGES,kdegames kdeedu)
 
 distro/regular-kde4: distro/.regular-desktop use/x11/kde4 use/x11/kdm4 +plymouth
-	@$(call add,LIVE_PACKAGES,kde4-regular)
-	@$(call add,LIVE_PACKAGES,plasma-applet-networkmanager)
+	@$(call add,LIVE_LISTS,$(call tags,regular kde4))
 
 distro/regular-razorqt: distro/.regular-desktop +razorqt +plymouth; @:
 
