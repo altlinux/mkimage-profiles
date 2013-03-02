e03f1031698e8bce0a6d09d001f4ca621ae4169b
Michael Shigorin <mike@altlinux.org>
2013-03-02 19:55:06 +0200

    vmguest: fix targets
    Thanks make(1) for the warning, `vmguest' target was
    getting pulled in twice indeed.

--- features.in/vmguest/config.mk
+++ features.in/vmguest/config.mk
@@ -1,13 +1,14 @@
 +vmguest: use/vmguest/virtualbox use/vmguest/kvm; @:
 
-use/vmguest/virtualbox:
+use/vmguest:
 	@$(call add_feature)
+
+use/vmguest/virtualbox: use/vmguest
 	@$(call add,THE_KMODULES,virtualbox-addition drm)
 	@$(call add,THE_PACKAGES,virtualbox-guest-additions)
 
 # NB: only reasonable for X11-bearing images
-use/vmguest/kvm:
-	@$(call add_feature)
+use/vmguest/kvm: use/vmguest
 	@$(call add,THE_PACKAGES,spice-vdagent)
 
 # see also use/install2/kvm
