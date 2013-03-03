546fae5961ffe0b40ec4b5bf34bc01da318ba7f5
Michael Shigorin <mike@altlinux.org>
2013-03-03 23:52:28 +0400

    rescue: extended firmware support
    As the firmware feature got more involved, this one
    has successfully dodged the change but is amended now.

--- features.in/rescue/config.mk
+++ features.in/rescue/config.mk
@@ -1,5 +1,5 @@
 use/rescue: use/stage2 sub/stage2@rescue use/syslinux/sdab.cfg \
-	use/firmware/server +wireless
+	use/firmware/full +wireless
 	@$(call add_feature)
 	@$(call add,RESCUE_LISTS, openssh \
 		$(call tags,(base || extra) && (rescue || network)))
