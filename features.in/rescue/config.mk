1f94112e7fc0db4425c11cac5fb119768eced417
Michael Shigorin <mike@altlinux.org>
2013-03-04 01:16:00 +0400

    initial rescue+misc
    Things that weren't fit in the other rescue pkglists went here;
    more are sure to follow.

--- features.in/rescue/config.mk
+++ features.in/rescue/config.mk
@@ -2,4 +2,5 @@ use/rescue: use/stage2 sub/stage2@rescue use/syslinux/sdab.cfg \
 	use/firmware/full +wireless
 	@$(call add_feature)
 	@$(call add,RESCUE_LISTS, openssh \
-		$(call tags,(base || extra || server ) && (rescue || network)))
+		$(call tags,(base || extra || server || misc) && \
+			(rescue || network)))
