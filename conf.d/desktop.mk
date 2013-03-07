cdf86ff1da2069029d79b28ecdb2b1efe270c303
Michael Shigorin <mike@altlinux.org>
2013-03-07 23:55:13 +0400

    desktop.mk: the now-missing links
    Added sysklogd and udev-rule-generator-net there too; if someone
    needs to inherit a systemd based image from distro/.desktop-network
    don't break the existing behaviour please.
    
    Rebased tde flavours to .desktop-network as these were the actual
    target to fix.

--- conf.d/desktop.mk
+++ conf.d/desktop.mk
@@ -8,6 +8,7 @@ distro/.desktop-mini: distro/.desktop-base use/x11/xdm +power; @:
 
 distro/.desktop-network: distro/.desktop-mini use/stage2/net-eth +vmguest
 	@$(call add,SYSTEM_PACKAGES,fonts-ttf-google-croscore-arimo)
+	@$(call add,BASE_PACKAGES,udev-rule-generator-net sysklogd)
 	@$(call add,BASE_LISTS, \
 		$(call tags,(base || desktop) && (l10n || network)))
 
@@ -17,7 +18,7 @@ distro/.desktop-extra:
 distro/kde4-lite: distro/.desktop-mini distro/.desktop-network distro/.desktop-extra +kde4-lite
 	@$(call set,KFLAVOURS,std-def)
 
-distro/tde: distro/.desktop-mini +tde; @:
+distro/tde: distro/.desktop-network +tde; @:
 distro/icewm: distro/.desktop-network use/lowmem use/install2/fs +icewm; @:
 distro/ltsp-tde: distro/tde +ltsp; @:
 distro/ltsp-icewm: distro/icewm +ltsp; @:
