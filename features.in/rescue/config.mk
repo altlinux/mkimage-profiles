6023d11521fece8bccc1c9f134930ae8a2754a60
Michael Shigorin <mike@altlinux.org>
2013-04-22 22:44:51 +0400

    rescue: tweaked pkglist and feature config
    Moved the packages which impeded pkglist reuse for live distros
    so that these stay within dedicated rescue images but don't
    neccessarily go into the more generic ones where things like
    fdisk are still quite useful.

--- features.in/rescue/config.mk
+++ features.in/rescue/config.mk
@@ -1,6 +1,8 @@
 use/rescue: use/stage2 sub/stage2@rescue use/syslinux/sdab.cfg \
 	use/firmware/full +wireless
 	@$(call add_feature)
+	@$(call add,RESCUE_PACKAGES,interactivesystem sysvinit udev)
+	@$(call add,RESCUE_PACKAGES,startup startup-rescue lilo)
 	@$(call add,RESCUE_LISTS, openssh \
 		$(call tags,(base || extra || server || misc) && \
 			(rescue || network || security)))
