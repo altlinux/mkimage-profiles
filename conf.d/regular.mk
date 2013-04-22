d849d263978e5564578d054cab83f05eb6ff0c60
Michael Shigorin <mike@altlinux.org>
2013-04-22 23:29:41 +0400

    regular.mk: reduced rescue participation somewhat
    The regular images became a bit too fat and rescueish
    with all the good stuff going into rescue+extra pkglist;
    that stuff does belong to dedicated rescue images but not
    to each and every one.
    
    The base+rescue pkglist has been tailored to take this
    into account so we can now make regular-*.iso more fit too.

--- conf.d/regular.mk
+++ conf.d/regular.mk
@@ -11,7 +11,7 @@ distro/.regular-base: distro/.regular-bare +vmguest +live \
 	use/live/ru use/live/install use/live/repo use/live/rw \
 	use/x11/3d-free use/branding
 	@$(call add,LIVE_LISTS,$(call tags,(base || desktop) && regular))
-	@$(call add,LIVE_LISTS,$(call tags,rescue extra))
+	@$(call add,LIVE_LISTS,$(call tags,base rescue))
 	@$(call add,THE_BRANDING,indexhtml notes alterator)
 	@$(call add,THE_BRANDING,graphics)
 
