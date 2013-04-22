ae441691390edbd17a6bb95109f5f87e696ead19
Michael Shigorin <mike@altlinux.org>
2013-04-22 19:24:33 +0400

    vmguest: apply vboxdrv related workaround
    This is to cope with #28782 while the culprit is being found out;
    not much of a loss while #27340 is open (thus no 3D with vboxdrv
    anyways).
    
    I chose to avoid pulling the service related machinery into
    vmguest (and haven't got around to factoring it out from live
    feature's scripts into a standalone form) so had to tweak these
    as well.

--- features.in/vmguest/config.mk
+++ features.in/vmguest/config.mk
@@ -6,6 +6,7 @@ use/vmguest:
 use/vmguest/virtualbox: use/vmguest
 	@$(call add,THE_KMODULES,virtualbox-addition drm)
 	@$(call add,THE_PACKAGES,virtualbox-guest-additions)
+	@$(call add,THE_PACKAGES,livecd-virtualbox-noglx)	### #28782
 
 # NB: only reasonable for X11-bearing images
 use/vmguest/kvm: use/vmguest
