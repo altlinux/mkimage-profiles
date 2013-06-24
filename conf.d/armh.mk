b7fd499aa2a6b67891bd4599d09a97ac206951e4
Michael Shigorin <mike@altlinux.org>
2013-06-24 23:16:10 +0400

    armh.mk: employ net-usershares feature
    It even works so far!

--- conf.d/armh.mk
+++ conf.d/armh.mk
@@ -51,7 +51,7 @@ vm/.cubox-bare: vm/.arm-base use/armh use/armh-cubox use/services/ssh \
 vm/.cubox-base: vm/.cubox-bare use/deflogin/altlinuxroot; @:
 vm/.cubox-gtk: vm/.cubox-base use/x11/lightdm/gtk; @:
 
-vm/cubox-xfce: vm/.cubox-bare use/slinux/arm use/oem; @:
+vm/cubox-xfce: vm/.cubox-bare use/slinux/arm use/oem use/net-usershares; @:
 
 vm/cubox-xfce-ru: vm/.cubox-gtk use/slinux/arm use/x11-autologin
 	@$(call add,BASE_PACKAGES,livecd-ru)
