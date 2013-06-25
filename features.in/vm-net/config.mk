91f0c4217c7cb1574782d81faf1f3a112cd170e0
Michael Shigorin <mike@altlinux.org>
2013-06-25 20:09:05 +0400

    obsoleted vm-net feature
    Hooray, this one is almost retired by now!

--- features.in/vm-net/config.mk
+++ features.in/vm-net/config.mk
@@ -1,11 +1,8 @@
 use/vm-net:
-	@$(call add_feature)
-	@$(call add,THE_PACKAGES,etcnet)
+	@$(warning obsolete feature, please upgrade to use/net-eth)
 
-use/vm-net/dhcp: use/vm-net
-	@$(call add,THE_PACKAGES,dhcpcd)
-	@$(call set,VM_NET,dhcp)
+use/vm-net/dhcp: use/vm-net use/net-eth/dhcp
 
 # need to further add VM_NET_IPV4ADDR and VM_NET_IPV4GW
-use/vm-net/static: use/vm-net
-	@$(call set,VM_NET,static)
+use/vm-net/static: use/vm-net use/net-eth
+	@$(call add,NET_ETH,eth0:static:$(VM_NET_IPV4ADDR):$(VM_NET_IPV4GW))
