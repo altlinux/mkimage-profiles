f43d68ce620612d53a85f68d740b520d3a46ad3e
Michael Shigorin <mike@altlinux.org>
2013-09-09 13:48:47 +0400

    power: employ services feature
    rootfs got handy either though managing modules to preload
    might be enhanced further.

--- features.in/power/config.mk
+++ features.in/power/config.mk
@@ -1,14 +1,14 @@
 +power: use/power/acpi/button use/power/acpi/cpufreq; @:
 
 # common
-# TODO: invent multi-target scripts and integrate that 08-powerbutton
-use/power:
+use/power: sub/rootfs use/services
 	@$(call add_feature)
 	@$(call add,COMMON_PACKAGES,installer-feature-powerbutton-stage3)
 
 # modern power management
 use/power/acpi: use/power
 	@$(call add,COMMON_PACKAGES,acpid acpi)
+	@$(call add,DEFAULT_SERVICES_ENABLE,acpid)
 
 use/power/acpi/button: use/power/acpi
 	@$(call add,COMMON_PACKAGES,acpid-events-power)
@@ -18,7 +18,10 @@ use/power/acpi/cpufreq: use/power/acpi
 
 use/power/acpi/powersave: use/power/acpi
 	@$(call add,COMMON_PACKAGES,powersave)
+	@$(call add,DEFAULT_SERVICES_DISABLE,acpid)	# override
+	@$(call add,DEFAULT_SERVICES_ENABLE,powersaved)
 
 # legacy power management
 use/power/apm: use/power
 	@$(call add,COMMON_PACKAGES,apmd lphdisk)
+	@$(call add,DEFAULT_SERVICES_ENABLE,apmd)
