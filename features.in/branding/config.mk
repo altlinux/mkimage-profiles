b1c18d43dac91c9fffe58e388f9deb1b88455b0c
Michael Shigorin <mike@altlinux.org>
2013-11-14 00:29:38 +0400

    branding: added bootloader to components
    branding-*-bootloader was handled for stage1 by syslinux feature
    but somehow got missed regarding the installed system; let's fix.

--- features.in/branding/config.mk
+++ features.in/branding/config.mk
@@ -4,7 +4,7 @@ use/branding:
 
 # NB: not every distro might have all the branding of its own
 use/branding/full: use/branding use/syslinux/ui/gfxboot
-	@$(call add,THE_BRANDING,alterator bootsplash graphics)
+	@$(call add,THE_BRANDING,alterator bootloader bootsplash graphics)
 	@$(call add,THE_BRANDING,indexhtml notes slideshow)
 
 use/branding/complete: use/branding/full use/plymouth/full
