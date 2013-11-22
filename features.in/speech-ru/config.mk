e469a19e2ac74a9cd414297f0b300f1f2938209c
Michael Shigorin <mike@altlinux.org>
2013-11-22 16:28:52 +0400

    speech-*: refactored and unified
    These two features were largely an internal fork,
    let's refactor them into a single tunable one.

--- features.in/speech-ru/config.mk
+++
@@ -1,5 +0,0 @@
-use/speech-ru:
-	@$(call add_feature)
-	@$(call add,THE_LISTS,speech/voiceman)
-	@$(call add,THE_LISTS,speech/ru)
-	@$(call add,DEFAULT_SERVICES_ENABLE,voiceman)
---
+++ features.in/speech/config.mk
@@ -0,0 +1,8 @@
+use/speech:
+	@$(call add_feature)
+	@$(call add,THE_LISTS,speech/voiceman speech/$$(SPEECH_LANG))
+	@$(call add,DEFAULT_SERVICES_ENABLE,voiceman)
+	@$(call xport,SPEECH_LANG)
+
+use/speech/en use/speech/ru:  use/speech/%: use/speech
+	@$(call set,SPEECH_LANG,$*)
