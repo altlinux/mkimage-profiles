e4bb17279d35bfec36a7e6ca6f0435276b6af01f
Michael Shigorin <mike@altlinux.org>
2013-12-13 21:26:45 +0400

    branding: support refind branding
    Everything is handled within mki-copy-efiboot currently
    but it needs an image to process; extracting one from
    bootloader branding seems less hassle than forcing it
    into every flavour of branding.

--- features.in/branding/lib/20-branding.mk
+++ features.in/branding/lib/20-branding.mk
@@ -2,3 +2,6 @@
 # NB: this comes just before build-distro.mk
 
 DOT_BASE += $(call branding,$(THE_BRANDING))
+
+CHROOT_PACKAGES_REGEXP += $(call branding,bootloader)
+CHROOT_PACKAGES += ImageMagick-tools
