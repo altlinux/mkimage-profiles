d65a61ea47ccad2251fd12eb05ea076106be918e
Michael Shigorin <mike@altlinux.org>
2014-03-01 22:28:28 +0400

    vm.mk: vm/net should use/repo
    IOW one has every right to expect that package manager
    is set up already in a networked virtual machine.
    
    (backport)

--- conf.d/vm.mk
+++ conf.d/vm.mk
@@ -1,9 +1,9 @@
 # virtual machines
 ifeq (vm,$(IMAGE_CLASS))
 
-vm/net: vm/bare use/vm-net/dhcp use/vm-ssh; @:
+vm/net: vm/bare use/vm-net/dhcp use/vm-ssh use/repo; @:
 
 # NB: use/x11 employs some installer-feature packages
-vm/icewm: vm/net use/cleanup/installer use/repo +icewm; @:
+vm/icewm: vm/net use/cleanup/installer +icewm; @:
 
 endif
