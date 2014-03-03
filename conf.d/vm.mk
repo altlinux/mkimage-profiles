d60492e87cf30f08d47e1e70c3ca05b3936850fb
Michael Shigorin <mike@altlinux.org>
2014-03-03 19:10:39 +0400

    vm.mk: extend vm/net base image size to 5 Gb
    qemu-img makes use of sparse files so the image will take up
    reasonable space when handled appropriately (and compress nicely
    as well); making images of minimal size is going to result in
    grave usage problems starting with apt-get update hitting ENOSPC.

--- conf.d/vm.mk
+++ conf.d/vm.mk
@@ -1,7 +1,8 @@
 # virtual machines
 ifeq (vm,$(IMAGE_CLASS))
 
-vm/net: vm/bare use/vm-net/dhcp use/vm-ssh use/repo; @:
+vm/net: vm/bare use/vm-net/dhcp use/vm-ssh use/repo
+	@$(call set,VM_SIZE,5368709120)	# 5 Gb is enough for everyone
 
 # NB: use/x11 employs some installer-feature packages
 vm/icewm: vm/net use/cleanup/installer +icewm; @:
