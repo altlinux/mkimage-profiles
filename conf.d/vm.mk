7a2b624b05bf33c3f4e4e4313658df37b4211877
Michael Shigorin <mike@altlinux.org>
2014-04-28 21:24:37 +0400

    vm.mk: demo icewm image with "OEM" setup
    This PoC will boot into locale and credentials setup GUI once
    and should boot as usual ever since, that is into lightdm
    in this case.

--- conf.d/vm.mk
+++ conf.d/vm.mk
@@ -4,11 +4,22 @@ ifeq (vm,$(IMAGE_CLASS))
 vm/bare: vm/.bare +sysvinit
 	@$(call add,BASE_PACKAGES,apt)
 
-vm/net: vm/bare use/net-eth/dhcp use/net-ssh use/repo
-	@$(call add,BASE_PACKAGES,su)
+vm/net: vm/bare use/net-eth/dhcp use/net-ssh use/repo use/control/sudo-su
+	@$(call add,BASE_PACKAGES,su udev-rule-generator-net)
 
 # NB: use/x11 employs some installer-feature packages
-vm/icewm: vm/net use/cleanup/installer use/repo use/deflogin/altlinuxroot \
-	use/x11/xorg use/x11/lightdm/gtk use/x11-autologin +icewm; @:
+vm/.desktop-bare: vm/net use/x11/xorg use/cleanup/installer use/repo; @:
+
+vm/.desktop-base: vm/.desktop-bare \
+	use/deflogin/altlinuxroot use/x11-autologin; @:
+
+mixin/icewm: use/x11/lightdm/gtk +icewm; @:
+
+vm/icewm: vm/.desktop-base mixin/icewm; @:
+
+vm/icewm-setup: vm/.desktop-bare mixin/icewm use/oem use/x11-autostart
+	@$(call set,BRANDING,simply-linux)
+	@$(call add,THE_BRANDING,graphics)
+	@$(call add,THE_LISTS,$(call tags,base l10n))
 
 endif
