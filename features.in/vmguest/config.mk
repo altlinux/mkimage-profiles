0276e25dd26a40d6b441d59fff1cbbed33a275d2
Michael Shigorin <mike@altlinux.org>
2014-04-29 20:17:06 +0400

    vmguest: add vmware support
    Thanks ildar@ for #30024 and specifically mentioning
    that mptspi.ko is missing in jeos flavour (using led-ws).

--- features.in/vmguest/config.mk
+++ features.in/vmguest/config.mk
@@ -1,4 +1,4 @@
-+vmguest: use/vmguest/vbox use/vmguest/kvm; @:
++vmguest: use/vmguest/vbox use/vmguest/kvm use/vmguest/vmware; @:
 
 use/vmguest:
 	@$(call add_feature)
@@ -8,7 +8,10 @@ use/vmguest/vbox: use/vmguest
 	@$(call add,THE_PACKAGES,virtualbox-guest-additions)
 
 # NB: only reasonable for X11-bearing images
+# see also use/install2/kvm
 use/vmguest/kvm: use/vmguest
 	@$(call add,THE_PACKAGES,spice-vdagent)
 
-# see also use/install2/kvm
+# see also use/install2/vmware
+use/vmguest/vmware:
+	@$(call add,THE_KMODULES,scsi)	# mptspi.ko
