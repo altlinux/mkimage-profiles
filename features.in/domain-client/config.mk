db1e57d24a4901f0ea106d5746b46384b7c2c3c0
Michael Shigorin <mike@altlinux.org>
2014-05-13 16:57:11 +0400

    domain-client: split into two targets
    The "full" one adds Kerberos ticket watcher GUI
    and sets up avahi service to start automatically.

--- features.in/domain-client/config.mk
+++ features.in/domain-client/config.mk
@@ -1,4 +1,7 @@
 use/domain-client: use/net/dhcp
 	@$(call add_feature)
 	@$(call add,THE_LISTS,domain-client)
+
+use/domain-client/full: use/domain-client
+	@$(call add,THE_PACKAGES,krb5-ticket-watcher)
 	@$(call add,DEFAULT_SERVICES_ENABLE,avahi-daemon)
