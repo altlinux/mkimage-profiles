362fd3f048806ac430d3490bdd9421dcc324bf18
Michael Shigorin <mike@altlinux.org>
2014-08-19 20:00:47 +0400

    fonts: add a few font collection targets
    Font packages are sprinkled all over the metaprofiles,
    let's try and help make their use more systematic.
    
    This is a sort of a feature abuse as it was conceived
    for fontconfig setup originally but spawning features
    with confusing names looks grim; so let all things fonts
    live within a feature named "fonts" for the time being.

--- features.in/fonts/config.mk
+++ features.in/fonts/config.mk
@@ -1,3 +1,4 @@
+# fontconfig setup
 use/fonts:
 	@$(call add_feature)
 	@$(call xport,FONT_FEATURES_ENABLE)
@@ -13,3 +14,26 @@ use/fonts/zerg: use/fonts
 # nothing configured to add_feature but let's not skip that for consistency
 use/fonts/infinality: use/fonts
 	@$(call add,THE_PACKAGES,libfreetype-infinality fontconfig-infinality)
+
+# a few typical font collections
+# NB: dejavu is ugly thus missing
+# NB: not depending on use/fonts intentionally,
+#     the scripts are unneeded to add packages
+use/fonts/otf/adobe:
+	@$(call add,THE_PACKAGES,fonts-otf-adobe-source-code-pro)
+	@$(call add,THE_PACKAGES,fonts-otf-adobe-source-sans-pro)
+
+use/fonts/otf/mozilla:
+	@$(call add,THE_PACKAGES,fonts-otf-mozilla-fira)
+
+use/fonts/ttf/google:
+	@$(call add,THE_PACKAGES,fonts-ttf-droid)
+	@$(call add,THE_PACKAGES,fonts-ttf-google-croscore-arimo)
+	@$(call add,THE_PACKAGES,fonts-ttf-google-croscore-cousine)
+	@$(call add,THE_PACKAGES,fonts-ttf-google-croscore-tinos)
+
+use/fonts/ttf/redhat:
+	@$(call add,THE_PACKAGES,fonts-ttf-liberation)
+
+use/fonts/ttf/ubuntu:
+	@$(call add,THE_PACKAGES,fonts-ttf-ubuntu-font-family)
