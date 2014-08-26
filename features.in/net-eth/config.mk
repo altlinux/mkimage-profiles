8533f7e2b725d18e9385a74f437494954ffc89f8
Michael Shigorin <mike@altlinux.org>
2014-08-26 22:20:22 +0400

    net-eth: added plus-shortcut
    "use/stage2/net-eth use/net-eth" would be common enough
    for installers or livecd images to just get these pulled in
    together by a convenient shortcut thus reducing confusion
    and chances to just forget one of these counterparts.

--- features.in/net-eth/config.mk
+++ features.in/net-eth/config.mk
@@ -1,10 +1,12 @@
+# NB: this is aimed at LiveCD/installer images
++net-eth: use/net-eth use/stage2/net-eth
+
+# see also use/stage2/net-eth; do not depend on it though
+# as stage2-less images need preconfigured networking too
 use/net-eth: use/net
 	@$(call add_feature)
 	@$(call xport,NET_ETH)
 	@$(call add,BASE_PACKAGES,udev-rule-generator-net)
-
-# see also use/stage2/net-eth; do not depend on it though
-# as stage2-less images need preconfigured networking too
 
 # typical boilerplate
 use/net-eth/dhcp: use/net-eth use/net/dhcp
