bf3365a376a790e1a5c133c7af8d5023955ab86b
Michael Shigorin <mike@altlinux.org>
2014-09-12 18:20:30 +0400

    regular.mk: oops, sysv-tde needs better fonts too
    This has missed 20140912 starterkits unfortunately,
    nightly builds excluding installers didn't catch it
    and beta testing hasn't uncovered this either.

--- conf.d/regular.mk
+++ conf.d/regular.mk
@@ -172,7 +172,8 @@ distro/regular-rescue: distro/.regular-bare use/rescue/rw use/luks \
 distro/regular-sysv-tde: distro/.regular-install-x11 \
 	mixin/desktop-installer mixin/regular-tde \
 	use/branding/complete use/branding/slideshow/once \
-	use/net-eth/dhcp use/efi/refind use/efi/shell use/rescue/base
+	use/net-eth/dhcp use/efi/refind use/efi/shell use/rescue/base \
+	use/fonts/otf/adobe use/fonts/otf/mozilla
 	@$(call set,KFLAVOURS,led-ws)
 	@$(call add,RESCUE_LISTS,$(call tags,rescue misc))
 	@$(call add,THE_LISTS,$(call tags,base desktop))
