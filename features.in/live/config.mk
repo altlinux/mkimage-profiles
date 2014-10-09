b76aa22729f1256ff3ce3bb3540d1d8628139d29
Michael Shigorin <mike@altlinux.org>
2014-10-09 19:38:16 +0400

    live: implemented kernel driver cleanup
    This one is alike to install2's one; it's not a shared rootfs
    script/variable though as contexts differ a lot, let's be careful.
    
    The commit has been missing from 1.1.64 somehow, found in patch
    series while figuring out why LIVE_CLEANUP_KDRIVERS seems to be
    just ignored in live-privacy *after* the massive rebase of that
    branch...

--- features.in/live/config.mk
+++ features.in/live/config.mk
@@ -18,6 +18,7 @@ use/live: use/stage2 sub/rootfs@live sub/stage2@live use/services
 	@$(call add,DEFAULT_SERVICES_ENABLE,$(_ON))
 	@$(call add,DEFAULT_SERVICES_DISABLE,$(_OFF))
 	@$(call add,CONTROL,rpcbind:local)
+	@$(call xport,LIVE_CLEANUP_KDRIVERS)
 
 use/live/.base: use/live use/syslinux/ui/menu
 	@$(call add,LIVE_LISTS,$(call tags,base live))
