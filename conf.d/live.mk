74126092a40c7f98b4f559ce35e004e0c4b46221
Michael Shigorin <mike@altlinux.org>
2014-12-10 22:14:16 +0300

    dev, live.mk: refactor live-builder bits
    It feels like time to introduce regular-builder.iso,
    let's help it by factoring most of the common part out
    into the feature.

--- conf.d/live.mk
+++ conf.d/live.mk
@@ -21,20 +21,12 @@ distro/.live-kiosk: distro/.base use/live/base use/live/autologin \
 	use/fonts/otf/adobe +power
 	@$(call add,CLEANUP_PACKAGES,'alterator*' 'guile*' 'vim-common')
 
-distro/live-builder-mini: distro/.live-base use/dev/mkimage use/dev \
-	use/stage2/net-eth use/net-eth/dhcp use/syslinux/timeout/30 \
-	use/isohybrid
-	@$(call set,KFLAVOURS,$(BIGRAM))
-	@$(call add,LIVE_LISTS,\
-		$(call tags,(base || live) && (server || builder)))
-	@$(call add,LIVE_PACKAGES,livecd-qemu-arch strace)
-	@$(call add,LIVE_PACKAGES,qemu-user-binfmt_misc)
-	@$(call add,LIVE_PACKAGES,zsh sudo)
+distro/live-builder-mini: distro/.live-base use/dev/builder/base \
+	use/syslinux/timeout/30 use/isohybrid \
+	use/stage2/net-eth use/net-eth/dhcp; @:
 
 distro/live-builder: distro/live-builder-mini \
-	use/efi/signed use/live/rw use/live/repo use/dev/repo
-	@$(call add,MAIN_LISTS,$(call tags,live builder))
-	@$(call add,MAIN_PACKAGES,syslinux pciids memtest86+ mkisofs)
+	use/dev/builder/full use/live/rw +efi; @:
 
 distro/live-install: distro/.live-base use/live/textinstall; @:
 distro/.livecd-install: distro/.live-base use/live/install; @:
--- features.in/dev/config.mk
+++ features.in/dev/config.mk
@@ -11,3 +11,15 @@ use/dev/repo: use/dev use/repo/main
 	@$(call add,MAIN_PACKAGES,rpm-build basesystem)
 	@$(call add,MAIN_PACKAGES,fakeroot sisyphus_check)
 	@$(call add,MAIN_PACKAGES,file make-initrd make-initrd-propagator)
+
+use/dev/builder/base: use/dev/mkimage use/dev
+	@$(call set,KFLAVOURS,$(BIGRAM))
+	@$(call add,LIVE_LISTS,\
+		$(call tags,(base || live) && (server || builder)))
+	@$(call add,LIVE_PACKAGES,livecd-qemu-arch strace)
+	@$(call add,LIVE_PACKAGES,qemu-user-binfmt_misc)
+
+use/dev/builder/full: use/dev/builder/base use/dev/repo
+	@$(call add,MAIN_LISTS,$(call tags,live builder))
+	@$(call add,MAIN_PACKAGES,syslinux pciids memtest86+ xorriso)
+	@$(call add,LIVE_PACKAGES,sudo)
