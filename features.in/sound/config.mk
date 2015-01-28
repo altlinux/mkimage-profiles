15a3cb12e89ba16e1fdf133d6005ddb24932ffc5
Denis Smirnov <mithraen@altlinux.org>
2015-01-28 11:38:42 +0300

    sound feature refactoring
    Split package lists:
    
    - base alsa packages (also needed in pulseaudio-based installations)
    - additional alsa packages (needed only if not using pulseaudio)
    - pulseaudio packages

--- features.in/sound/config.mk
+++ features.in/sound/config.mk
@@ -1,11 +1,16 @@
 +pulse: use/sound/pulse; @:
++alsa:  use/sound/alsa; @:
 
 # "bare" ALSA (which is good enough for many of us) with persistent levels
 use/sound:
 	@$(call add_feature)
 	@$(call add,THE_KMODULES,alsa sound)
-	@$(call add,THE_PACKAGES,amixer alsa-utils aplay udev-alsa)
+	@$(call add,THE_LISTS,sound/base)
+
+# ALSA only sound (additional utils needed if using pulseaudio)
+use/sound/alsa: use/sound
+	@$(call add,THE_PACKAGES,apulse)
 
 # PulseAudio (useful for per-app levels, dynamic devices and networked sound)
 use/sound/pulse: use/sound
-	@$(call add,THE_LISTS,pulseaudio)
+	@$(call add,THE_LISTS,sound/pulseaudio)
