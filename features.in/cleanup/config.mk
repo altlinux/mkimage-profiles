620c827c5f037b48b630ec8393598a841df2821e
Michael Shigorin <mike@altlinux.org>
2015-02-25 00:41:23 +0300

    cleanup: introduce use/cleanup/libs
    The installer feature added is a trivial wrapper around
    apt-cache nodeps to uninstall the ^lib packages that have
    no more dependencies upon those when the temporarily
    installed packages like alterator-browser-qt get removed.

--- features.in/cleanup/config.mk
+++ features.in/cleanup/config.mk
@@ -2,10 +2,14 @@ use/cleanup:
 	@$(call add_feature)
 	@$(call xport,CLEANUP_PACKAGES)
 
+use/cleanup/libs:
+	@$(call add,BASE_PACKAGES,apt-scripts)
+	@$(call add,INSTALL2_PACKAGES,installer-feature-cleanup-libs-stage3)
+
 use/cleanup/installer: use/cleanup
 	@$(call add,CLEANUP_PACKAGES,'installer-*')
 
-use/cleanup/x11: use/cleanup
+use/cleanup/x11: use/cleanup use/cleanup/libs
 	@$(call add,CLEANUP_PACKAGES,libICE libSM libxcb 'libX*')
 
 # as some alterator modules are installed into stage3 (the destination
