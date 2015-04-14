111ec1d044a5856733d5709f103579683b2782ad
Michael Shigorin <mike@altlinux.org>
2015-04-14 19:58:01 +0300

    stage2: make use of finer-grained module lists
    This is sort of laying the ground for the future dismantling
    of 10-stage2 (which was sub.in/stage1/modules just recently);
    things look like tagged lists might become due some day, e.g.
    "net+usb" or "scsi+raid" -- time will tell.

--- features.in/stage2/config.mk
+++ features.in/stage2/config.mk
@@ -5,11 +5,19 @@ use/stage2: sub/stage1
 	@$(call add,STAGE1_MODLISTS,$$(FEATURES))
 	@$(call xport,STAGE1_KCONFIG)
 
-use/stage2/kms:
+# building blocks for propagator's module cove
+use/stage2/ata use/stage2/drm use/stage2/fs use/stage2/hid use/stage2/md \
+	use/stage2/mmc use/stage2/net use/stage2/net-cifs use/stage2/net-nfs \
+	use/stage2/pcmcia use/stage2/rtc use/stage2/scsi use/stage2/usb \
+	use/stage2/virtio: \
+	use/stage2/%: use/stage2
+	@$(call add,STAGE1_MODLISTS,stage2-$*)
+
+use/stage2/kms: use/stage2/drm
 	@$(call add,STAGE1_KMODULES_REGEXP,drm.*)
 
 # install mount.cifs to stage1
-use/stage2/cifs-install:
+use/stage2/cifs-install: use/stage2/net-cifs
 	@$(call add,STAGE1_PACKAGES,cifs-utils)
 
 # eth0 instead of enp0s3
