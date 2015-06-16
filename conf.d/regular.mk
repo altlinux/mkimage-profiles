f02fc7ac1ea13dcb23ae304bb7a8c2aa454e0e96
Michael Shigorin <mike@altlinux.org>
2015-06-16 14:29:08 +0300

    regular.mk: change xfce-sysv to un-def
    Let's provide some kernel versatility as well
    since there are two distinct Xfce-based images.

--- conf.d/regular.mk
+++ conf.d/regular.mk
@@ -119,6 +119,7 @@ distro/regular-xfce: distro/.regular-gtk \
 
 distro/regular-xfce-sysv: distro/.regular-sysv-gtk use/x11/xfce \
 	use/fonts/ttf/redhat
+	@$(call set,KFLAVOURS,un-def)
 	@$(call add,LIVE_PACKAGES,xfce4-mixer pm-utils bc elinks mpg123)
 
 distro/regular-lxde: distro/.regular-gtk use/x11/lxde use/fonts/infinality \
