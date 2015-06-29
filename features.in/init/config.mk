30d38381a3d9318c53e96ce97d001da70cd7902e
Michael Shigorin <mike@altlinux.org>
2015-06-29 17:19:57 +0300

    init: rework de-systemd-ization
    It's the very same problem that must be solved within mkimage:
    some package lists get expanded early and some late thus having
    no chance to influence apt's choices of alternatives made early
    (in fact, too early).
    
    Until that, here's another kludge...
    
    PS: turns out that ^systemd- is not "drop ^systemd" but rather:
        systemd-analyze
        systemd-coredump
        systemd-journal-gateway
        systemd-networkd
        systemd-sysvinit
        -- thus one /really/ wants something else.

--- features.in/init/config.mk
+++ features.in/init/config.mk
@@ -4,13 +4,15 @@
 use/init:
 	@$(call add_feature)
 	@$(call add,THE_LISTS,$$(INIT_TYPE))
+	@$(call add,RESCUE_LISTS,$$(INIT_TYPE))
 
 # THE_LISTS is too late when BASE_PACKAGES have pulled in
 # the wrong syslogd-daemon provider already
 use/init/sysv: use/init
 	@$(call set,INIT_TYPE,sysvinit)
 	@$(call add,BASE_PACKAGES,syslogd)
-	@$(call add,BASE_PACKAGES_REGEXP,^systemd- ^systemd-services-)
+	@$(call add,BASE_PACKAGES_REGEXP,^systemd-services-)
+	@$(call add,BASE_PACKAGES_REGEXP,^systemd-networkd-)
 
 use/init/sysv/polkit: use/init/sysv
 	@$(call add,THE_PACKAGES,polkit-sysvinit)
