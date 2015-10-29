fb1781a4c96a2110589347b171c2f90b40eea95d
Michael Shigorin <mike@altlinux.org>
2015-10-29 17:57:44 +0300

    regular.mk: added wmxkbru to wmaker
    This one has been suggested by YYY:
    https://forum.altlinux.org/index.php?topic=35623.msg268496#msg268496

--- conf.d/regular.mk
+++ conf.d/regular.mk
@@ -101,7 +101,7 @@ mixin/regular-wmaker: use/efi/refind use/syslinux/ui/gfxboot \
 # wdm can't do autologin so add standalone one for livecd
 distro/regular-wmaker: distro/.regular-sysv \
 	mixin/regular-wmaker use/live/autologin use/browser/seamonkey/i18n
-	@$(call add,LIVE_PACKAGES,wdm)
+	@$(call add,LIVE_PACKAGES,wdm wmxkbru)
 
 # gdm2.20 can reboot/halt with both sysvinit and systemd, and is slim
 mixin/regular-gnustep: use/x11/gnustep use/x11/gdm2.20 use/mediacheck \
