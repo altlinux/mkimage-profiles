2271b034457790b4541d19c14432060bee4a2a6c
Alexey Shabalin <shaba@altlinux.org>
2015-12-23 10:03:39 +0300

    add use/net-eth/networkd

--- features.in/net-eth/config.mk
+++ features.in/net-eth/config.mk
@@ -8,8 +8,16 @@ use/net-eth: use/net/etcnet
 	@$(call xport,NET_ETH)
 	@$(call add,BASE_PACKAGES,udev-rule-generator-net)
 
+use/net-eth/networkd: use/net/networkd
+	@$(call add_feature)
+	@$(call xport,NET_ETH)
+	@$(call add,BASE_PACKAGES,udev-rule-generator-net)
+
 # typical boilerplate
 use/net-eth/dhcp: use/net-eth use/net/dhcp
+	@$(call add,NET_ETH,eth0:dhcp)
+
+use/net-eth/networkd-dhcp: use/net-eth/networkd
 	@$(call add,NET_ETH,eth0:dhcp)
 
 # use e.g. eth0:static:10.0.0.2/24:10.0.0.1 for predefined static configuration
