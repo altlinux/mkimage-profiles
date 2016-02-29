dda5cc71a17c944e1bfbf35dd2cf907f12de8676
Michael Shigorin <mike@altlinux.org>
2016-02-29 21:39:11 +0300

    education.mk: speak Russian by default, care for VMs
    The former is part of requirements for this distribution,
    and the latter is part of expectations.

--- conf.d/education.mk
+++ conf.d/education.mk
@@ -2,7 +2,9 @@
 
 ifeq (distro,$(IMAGE_CLASS))
 
-distro/education-junior: distro/.installer use/slinux/full
+distro/education-junior: distro/.installer use/slinux/full \
+	use/l10n/default/ru_RU +vmguest
+	@$(call set,INSTALLER,junior)
 	@$(call set,BRANDING,school-junior)
 	@$(call set,META_VOL_SET,Education Junior)
 	@$(call add,THE_LISTS,education/desktop)
