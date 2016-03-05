b2f8702774906de7cd5a2aeea8d11b9e2e1aac08
Denis Pynkin <dans@altlinux.org>
2016-03-05 21:28:49 +0300

    regular.mk: added regular server with LXD

--- conf.d/regular.mk
+++ conf.d/regular.mk
@@ -290,6 +290,17 @@ distro/regular-server-samba4: distro/.regular-server-managed
 	@$(call add,THE_PACKAGES,alterator-dhcp)
 	@$(call add,DEFAULT_SERVICES_DISABLE,smbd nmbd winbind)
 
+distro/regular-server-lxd: distro/.regular-bare \
+	+efi +power \
+	use/live/base use/live/rw use/live/repo/online use/live/textinstall \
+	use/lxc/lxd use/tty/S0 \
+	use/init/systemd/multiuser
+	@$(call set,KFLAVOURS,un-def)
+	@$(call add,DEFAULT_SERVICES_ENABLE,sshd)
+	@$(call add,DEFAULT_SERVICES_ENABLE,lxd lxc-net lxcfs cgmanager)
+	@$(call add,DEFAULT_SERVICES_ENABLE,getty@tty1 getty@ttyS0)
+	@$(call add,DEFAULT_SERVICES_ENABLE,livecd-net-eth)
+
 distro/regular-engineering: distro/regular-lxde use/live/ru
 	@$(call add,THE_LISTS,$(call tags,engineering desktop))
 	@$(call add,THE_LISTS,$(call tags,desktop sane))
