813a84d11cd9fdddfa4d1dcf841638589ef6d131
Michael Shigorin <mike@altlinux.org>
2016-04-13 14:16:23 +0300

    docs: fix thinko
    This single-byte error has led to premature variable
    substitution thus non-existant "docs-" package being
    requested; ouch.

--- features.in/docs/config.mk
+++ features.in/docs/config.mk
@@ -13,7 +13,7 @@ use/docs/indexhtml: use/docs use/branding
 
 use/docs/manual: use/docs
 	@$(call xport,DOCS)
-	@$(call add,THE_PACKAGES,docs-$(DOCS))
+	@$(call add,THE_PACKAGES,docs-$$(DOCS))
 
 use/docs/license: use/docs use/branding
 	@$(call add,THE_BRANDING,notes)
