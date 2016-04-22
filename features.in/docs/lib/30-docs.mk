3c65c498175fba2da051e1b0819e50755303dcb8
Michael Shigorin <mike@altlinux.org>
2016-04-22 14:50:00 +0300

    docs: don't pull branding bits in unconditionally
    ...just do it when actually requested for the main image part.

--- features.in/docs/lib/30-docs.mk
+++ features.in/docs/lib/30-docs.mk
@@ -5,8 +5,10 @@ DOT_BASE += docs-$(DOCS)
 CHROOT_PACKAGES += docs-$(DOCS)
 endif
 
-ifneq (,$(BRANDING))
+ifneq (,$(findstring notes,$(THE_BRANDING) $(INSTALL2_BRANDING)))
 CHROOT_PACKAGES_REGEXP += $(call branding,notes)
 endif
 
+ifneq (,$(findstring indexhtml,$(THE_BRANDING)))
 CHROOT_PACKAGES_REGEXP += $(call branding,indexhtml)
+endif
