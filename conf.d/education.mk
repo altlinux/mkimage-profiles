59b8da8c618e17b775016c3e3d0ec3e98fd3ccbb
Andrey Cherepanov <cas@altlinux.org>
2016-07-15 18:06:04 +0300

    education.mk: ALT Education 8.0

--- conf.d/education.mk
+++ conf.d/education.mk
@@ -2,11 +2,11 @@
 
 ifeq (distro,$(IMAGE_CLASS))
 
-distro/education-junior: distro/.installer use/slinux/full \
+distro/education: distro/.installer use/slinux/full \
 	use/l10n/default/ru_RU +vmguest
 	@$(call set,INSTALLER,junior)
-	@$(call set,BRANDING,school-junior)
-	@$(call set,META_VOL_SET,Education Junior)
+	@$(call set,BRANDING,alt-education)
+	@$(call set,META_VOL_SET,ALT Education 8.0)
 	@$(call add,THE_LISTS,education/desktop)
 
 endif
