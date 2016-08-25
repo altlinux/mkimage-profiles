20b0b23fa29f550738dc0522e8d2bd638f5a94cd
Denis Pynkin <dans@altlinux.org>
2016-08-25 14:30:39 +0300

    regular.mk, lxc, lxd: updated regular-server-lxd
    Use lxd-bridge for networking.

--- features.in/lxc/config.mk
+++ features.in/lxc/config.mk
@@ -7,5 +7,6 @@ use/lxc/lxd: use/lxc
 	@$(call add,GROUPS,netadmin lxd)
 	@$(call add,LIVE_LISTS,lxd)
 	@$(call add,LIVE_LISTS,openssh)
+	@$(call add,LIVE_PACKAGES,su)
 	@$(call add,LIVE_PACKAGES,livecd-net-eth)
 	@$(call add,LIVE_PACKAGES,udev-rule-generator-net)
