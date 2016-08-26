e04c7a4ade5614cd2150cfa888406e538edba721
Andrey Cherepanov <cas@altlinux.org>
2016-08-26 16:08:29 +0300

    education.mk: add EFI support

--- conf.d/education.mk
+++ conf.d/education.mk
@@ -5,7 +5,7 @@ ifeq (distro,$(IMAGE_CLASS))
 distro/education: distro/.installer use/slinux/full \
 	use/browser/firefox/esr \
 	use/memtest \
-	use/l10n/default/ru_RU +vmguest
+	use/l10n/default/ru_RU +vmguest +efi
 	@$(call set,INSTALLER,junior)
 	@$(call set,BRANDING,alt-education)
 	@$(call set,META_VOL_SET,ALT Education 8.0)
