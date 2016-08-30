7a58ffd355a215d92b85bdf083607011bf77e74a
Andrey Cherepanov <cas@altlinux.org>
2016-08-30 13:37:28 +0300

    education.mk: add vim-console and Live support

--- conf.d/education.mk
+++ conf.d/education.mk
@@ -5,6 +5,7 @@ ifeq (distro,$(IMAGE_CLASS))
 distro/education: distro/.installer use/slinux/full \
 	use/browser/firefox/esr \
 	use/memtest \
+	use/live/install use/live/suspend use/live/x11 use/live/repo \
 	use/l10n/default/ru_RU +vmguest +efi
 	@$(call set,INSTALLER,junior)
 	@$(call set,BRANDING,alt-education)
@@ -15,10 +16,10 @@ distro/education: distro/.installer use/slinux/full \
 	@$(call add,THE_GROUPS,education/kde5)
 	@$(call add,THE_GROUPS,education/server-apps-edu)
 	@$(call add,INSTALL2_PACKAGES,xorg-drv-libinput)
-	@$(call add,THE_PACKAGES,usbutils pv syslinux lftp links2 openssh-server xinput xorg-drv-libinput)
+	@$(call add,THE_PACKAGES,usbutils pv syslinux lftp links2 openssh-server xinput xorg-drv-libinput vim-console)
 	@$(call add,THE_KMODULES,lsadrv)
 	@$(call add,BASE_PACKAGES,os-prober)
-	@$(call add,THE_PACKAGES,disable-usb-autosuspend)
+	@$(call add,THE_PACKAGES,xorg-drv-vmware xorg-drv-vmmouse disable-usb-autosuspend)
 	@$(call add,THE_PACKAGES,bluez pulseaudio-bluez)
 
 endif
