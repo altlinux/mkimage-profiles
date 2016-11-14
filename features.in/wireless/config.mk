af67c76e0d25244f30c1df80be20aec427171093
Michael Shigorin <mike@altlinux.org>
2016-11-14 12:53:51 +0300

    wireless: dedup via pkglist
    The two package sets seemed *too* identical when
    I finally needed to add another package there...

--- features.in/wireless/config.mk
+++ features.in/wireless/config.mk
@@ -3,7 +3,7 @@
 use/wireless:
 	@$(call add_feature)
 	@$(call add,THE_KMODULES,bcmwl rt3070 rtl8192 staging)
-	@$(call add,THE_PACKAGES,wireless-tools rfkill crda iw)
+	@$(call add,THE_LISTS,tools/wireless)
 
 use/wireless/full: use/wireless use/kernel/wireless; @:
-	@$(call add,RESCUE_PACKAGES,wireless-tools rfkill crda iw)
+	@$(call add,RESCUE_LISTS,tools/wireless)
