bca134201504ca62abc6779591b4749bf30f0548
Michael Shigorin <mike@altlinux.org>
2017-03-31 20:53:19 +0300

    syslinux, tty: better serial support
    A one-liner for syslinux and a simple installer feature
    change a lot in this regard!

--- features.in/tty/config.mk
+++ features.in/tty/config.mk
@@ -2,8 +2,10 @@ use/tty:
 	@$(call add_feature)
 	@$(call xport,TTY_DEV)
 	@$(call xport,TTY_RATE)
+	@$(call add,BASE_PACKAGES,installer-feature-serial-stage3)
 
 use/tty/S0: use/tty
 	@$(call add,THE_PACKAGES,agetty)
 	@$(call add,TTY_DEV,ttyS0)
 	@$(call set,TTY_RATE,115200)
+	@$(call add,SYSLINUX_CFG,ttyS0)
