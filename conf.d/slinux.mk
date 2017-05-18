b0595fcdefffac7221cf71b175362fed1443063c
Mikhail Efremov <sem@altlinux.org>
2017-05-18 20:41:09 +0300

    slinux: Drop steam
    It can be broken and no one tested it.
    See https://bugzilla.altlinux.org/show_bug.cgi?id=32063

--- conf.d/slinux.mk
+++ conf.d/slinux.mk
@@ -9,7 +9,7 @@ distro/slinux-live: distro/.livecd-install use/slinux/base
 
 distro/slinux: distro/.installer use/slinux/full use/rescue/base
 	@$(call set,INSTALLER,simply-linux)
-	@$(call add,MAIN_GROUPS,slinux/dropbox slinux/emulators-full slinux/games-base slinux/games slinux/games-full slinux/games-full-steam slinux/graphics-base slinux/graphics-full-blender slinux/graphics slinux/graphics-full-inkscape slinux/graphics-full-shotwell slinux/graphics-full-synfigstudio slinux/multimedia-full-audacity slinux/multimedia-base slinux/multimedia-full-cheese slinux/multimedia slinux/multimedia-full-pitivi slinux/multimedia-full-sound-juicer slinux/network-base)
+	@$(call add,MAIN_GROUPS,slinux/dropbox slinux/emulators-full slinux/games-base slinux/games slinux/games-full slinux/graphics-base slinux/graphics-full-blender slinux/graphics slinux/graphics-full-inkscape slinux/graphics-full-shotwell slinux/graphics-full-synfigstudio slinux/multimedia-full-audacity slinux/multimedia-base slinux/multimedia-full-cheese slinux/multimedia slinux/multimedia-full-pitivi slinux/multimedia-full-sound-juicer slinux/network-base)
 	@$(call set,META_VOL_SET,Simply Linux)
 	@$(call set,META_VOL_ID,Simply Linux/$(ARCH))
 
