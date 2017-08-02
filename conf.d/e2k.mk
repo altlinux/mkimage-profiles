c903e3b0cecbdb0c6dd8db4983d5fac7b0556c08
Michael Shigorin <mike@altlinux.org>
2017-08-02 19:25:28 +0300

    e2k.mk: initial suite
    These images have been tested on an Elbrus-401 PC.

---
+++ conf.d/e2k.mk
@@ -0,0 +1,12 @@
+ifeq (vm,$(IMAGE_CLASS))
+
+vm/live-e2k: vm/bare use/e2k use/deflogin/live
+	@$(call add,THE_PACKAGES,openssh)
+	@$(call add,THE_LISTS,$(call tags,ve tools))
+
+vm/rescue-e2k: vm/live-e2k use/tty/S0 use/net-eth/dhcp
+	@$(call add,THE_PACKAGES,agetty gpm fdisk parted smartmontools pv sshfs)
+	@$(call add,THE_LISTS,$(call tags,server && (network || extra)))
+	@$(call add,DEFAULT_SERVICES_DISABLE,gpm smartd)
+
+endif
