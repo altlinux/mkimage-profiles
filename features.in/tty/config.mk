009e3a4d13174ccc30abb15b9bdca2d3d58ea855
Michael Shigorin <mike@altlinux.org>
2017-08-02 18:49:46 +0300

    tty: restrict installer features to distros
    ...as vm images don't need extra processing.

--- features.in/tty/config.mk
+++ features.in/tty/config.mk
@@ -2,7 +2,9 @@ use/tty:
 	@$(call add_feature)
 	@$(call xport,TTY_DEV)
 	@$(call xport,TTY_RATE)
+ifeq (distro,$(IMAGE_CLASS))
 	@$(call add,BASE_PACKAGES,installer-feature-serial-stage3)
+endif
 
 use/tty/S0: use/tty
 	@$(call add,THE_PACKAGES,agetty)
