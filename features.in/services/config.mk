5174ec5dffaeafbdcdce13597c1d7d36a6bd81af
Michael Shigorin <mike@altlinux.org>
2017-08-02 21:14:42 +0300

    services: lvm2-* disable target
    These are creeping throughout m-p and aren't really needed
    by default as these should be enabled upon configuration;
    let's provide a single switch off point.

--- features.in/services/config.mk
+++ features.in/services/config.mk
@@ -4,3 +4,8 @@ use/services: sub/rootfs
 	@$(call xport,DEFAULT_SERVICES_DISABLE)
 	@$(call xport,SERVICES_ENABLE)
 	@$(call xport,SERVICES_DISABLE)
+
+use/services/lvm2-disable: use/services
+	@$(call add,DEFAULT_SERVICES_DISABLE,lvm2-lvmetad)
+	@$(call add,DEFAULT_SERVICES_DISABLE,lvm2-lvmpolld)
+	@$(call add,DEFAULT_SERVICES_DISABLE,lvm2-monitor)
