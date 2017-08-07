0d6fe3500c0a1a7e16022ae8e2b154bc94d02242
Michael Shigorin <mike@altlinux.org>
2017-08-07 17:48:36 +0300

    e2k: official x11 support
    The early builds used to rely upon a non-committed
    rootfs/files/etc/X11/xorg.conf within this feature
    which was a bit annoying and would have screwed an
    Elbrus system based on any other GPU.
    
    So let's provide some flexibility by packaging it.

--- features.in/e2k/config.mk
+++ features.in/e2k/config.mk
@@ -1,3 +1,7 @@
 use/e2k:
 	@$(call add_feature)
 	@$(call set,KFLAVOURS,mcst-def)
+
+use/e2k/x11: use/e2k use/x11
+	@$(call add,THE_PACKAGES,xorg-conf-e401-radeon)
+	@$(call add,THE_PACKAGES,xorg-drv-ati)
