fae0bb94e72a0aa169353a99ca048d7578f7c6bd
Michael Shigorin <mike@altlinux.org>
2017-08-07 17:58:26 +0300

    e2k.mk: vm/e2k-xfce
    Let's build an Xfce based image, got anything needed (well, almost:
    xorg-drv-libinput isn't there and no one is crying over that here).
    
    And let's change "e2k" suffix to be prefix while at that.

--- conf.d/e2k.mk
+++ conf.d/e2k.mk
@@ -1,12 +1,16 @@
 ifeq (vm,$(IMAGE_CLASS))
 
-vm/live-e2k: vm/bare use/e2k use/deflogin/live
+vm/e2k-live: vm/bare use/e2k use/deflogin/live use/control/sudo-su
 	@$(call add,THE_PACKAGES,openssh)
 	@$(call add,THE_LISTS,$(call tags,ve tools))
 
-vm/rescue-e2k: vm/live-e2k use/tty/S0 use/net-eth/dhcp use/services/lvm2-disable
+vm/e2k-rescue: vm/e2k-live use/tty/S0 use/net-eth/dhcp use/services/lvm2-disable
 	@$(call add,THE_PACKAGES,agetty gpm fdisk parted smartmontools pv sshfs)
 	@$(call add,THE_LISTS,$(call tags,server && (network || extra)))
 	@$(call add,DEFAULT_SERVICES_DISABLE,gpm mdadm smartd)
+
+vm/e2k-xfce: vm/e2k-rescue use/e2k/x11 use/x11/xfce \
+	use/l10n/default/ru_RU use/fonts/otf/adobe use/fonts/otf/mozilla
+	@$(call add,THE_PACKAGES,xinit xterm firefox mc)
 
 endif
