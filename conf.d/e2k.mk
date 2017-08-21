16da13ff70c383ad0753c80f139f6a5c94fe085c
Michael Shigorin <mike@altlinux.org>
2017-08-21 21:58:44 +0300

    e2k.mk: add vm/e2k-builder
    ...and somewhat improve vm/e2k-xfce.

--- conf.d/e2k.mk
+++ conf.d/e2k.mk
@@ -9,8 +9,12 @@ vm/e2k-rescue: vm/e2k-live use/tty/S0 use/net-eth/dhcp use/services/lvm2-disable
 	@$(call add,THE_LISTS,$(call tags,server && (network || extra)))
 	@$(call add,DEFAULT_SERVICES_DISABLE,gpm mdadm smartd)
 
-vm/e2k-xfce: vm/e2k-rescue use/e2k/x11 use/x11/xfce \
+vm/e2k-xfce: vm/e2k-rescue use/e2k/sound use/e2k/x11 use/x11/xfce \
+	use/browser/firefox use/browser/firefox/esr use/browser/firefox/i18n \
 	use/l10n/default/ru_RU use/fonts/otf/adobe use/fonts/otf/mozilla
-	@$(call add,THE_PACKAGES,xinit xterm firefox mc)
+	@$(call add,THE_PACKAGES,xinit xterm mc)
+	@$(call add,THE_PACKAGES,fonts-bitmap-terminus pnmixer)
+
+vm/e2k-builder: vm/e2k-rescue use/dev/builder/base; @:
 
 endif
