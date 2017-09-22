f32fd9a3b0e854a2029ceca5172568221427cee1
Michael Shigorin <mike@altlinux.org>
2017-09-22 17:57:36 +0300

    firmware: add firmware-rt* to use/firmware/wireless
    ...for firmware-rt61pci; thanks to an anonymous bugreporter.
    
    See-also: http://www.opennet.ru/openforum/vsluhforumID3/112310.html#177

--- features.in/firmware/config.mk
+++ features.in/firmware/config.mk
@@ -31,6 +31,7 @@ use/firmware/wireless: use/firmware
 	@$(call add,THE_PACKAGES_REGEXP,firmware-prism.*)
 	@$(call add,THE_PACKAGES_REGEXP,firmware-ipw.*)
 	@$(call add,THE_PACKAGES_REGEXP,firmware-zd.*)
+	@$(call add,THE_PACKAGES_REGEXP,firmware-rt.*)
 
 use/firmware/laptop: use/firmware/cpu
 	@$(call add,KMODULES,acpi_call)
