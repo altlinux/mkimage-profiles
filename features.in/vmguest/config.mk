a9b1f055014954d403af6274d7bd1ed1d68e2c98
Alexey Shabalin <shaba@altlinux.org>
2017-10-23 13:43:40 +0300

    add use/vmguest/kvm/x11

--- features.in/vmguest/config.mk
+++ features.in/vmguest/config.mk
@@ -6,9 +6,9 @@ ifeq (,$(filter-out i586 x86_64,$(ARCH)))
 use/vmguest:
 	@$(call add_feature)
 
-use/vmguest/base: use/vmguest/vbox use/vmguest/vmware; @:
+use/vmguest/base: use/vmguest/vbox use/vmguest/vmware use/vmguest/kvm; @:
 use/vmguest/complete: use/vmguest/base \
-	use/vmguest/vbox/x11 use/vmguest/vmware/x11 use/vmguest/kvm; @:
+	use/vmguest/vbox/x11 use/vmguest/vmware/x11 use/vmguest/kvm/x11; @:
 
 use/vmguest/vbox: use/vmguest
 	@$(call add,THE_KMODULES,virtualbox-addition vboxguest)
@@ -20,7 +20,10 @@ use/vmguest/vbox/x11: use/vmguest/vbox
 # NB: only reasonable for X11-bearing images
 # see also use/install2/kvm
 use/vmguest/kvm: use/vmguest
-	@$(call add,THE_PACKAGES,spice-vdagent qemu-guest-agent)
+	@$(call add,THE_PACKAGES,qemu-guest-agent)
+
+use/vmguest/kvm/x11: use/vmguest/kvm
+	@$(call add,THE_PACKAGES,spice-vdagent xorg-drv-qxl)
 
 # see also use/install2/vmware
 use/vmguest/vmware:
