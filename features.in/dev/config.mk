32105cfd9abee145564183cc7af1a17e6d37008a
Michael Shigorin <mike@altlinux.org>
2018-02-19 13:20:10 +0300

    dev, pkg.in: introduce dev/builder pkggroup
    This looks like missing in server starterkit for me;
    builder one can be installed by hand if one knows how
    to do that but let's better be explicit about that.

--- features.in/dev/config.mk
+++ features.in/dev/config.mk
@@ -3,11 +3,10 @@ use/dev: use/control
 	@$(call add,THE_PACKAGES,git-core hasher gear)
 	@$(call add,CONTROL,pam_mktemp:enabled)
 
-use/dev/repo: use/dev use/repo/main
+# use/dev intentionally missing
+use/dev/repo: use/repo/main
 	@$(call add,THE_PACKAGES,apt-repo)
-	@$(call add,MAIN_PACKAGES,rpm-build basesystem)
-	@$(call add,MAIN_PACKAGES,fakeroot sisyphus_check)
-	@$(call add,MAIN_PACKAGES,file make-initrd make-initrd-propagator)
+	@$(call add,MAIN_LISTS,$(call tags,main builder))
 
 use/dev/mkimage: use/dev
 	@$(call add,THE_PACKAGES,mkimage shadow-change su)
@@ -19,9 +18,12 @@ use/dev/builder/live: use/dev/builder/base
 	@$(call add,LIVE_LISTS,$(call tags,live builder))
 	@$(call add,LIVE_PACKAGES,livecd-qemu-arch qemu-user-binfmt_misc)
 
-use/dev/builder/full: use/dev/builder/live use/dev/repo
+use/dev/builder/full: use/dev use/dev/builder/live use/dev/repo
 	@$(call set,KFLAVOURS,$(BIGRAM))
 	@$(call add,THE_LISTS,$(call tags,server extra))
 	@$(call add,MAIN_LISTS,$(call tags,live builder))
 	@$(call add,MAIN_PACKAGES,syslinux pciids memtest86+ xorriso)
 	@$(call add,LIVE_PACKAGES,sudo perl-Gear-Remotes)
+
+use/dev/groups/builder: use/dev/repo
+	@$(call add,MAIN_GROUPS,dev/builder)
