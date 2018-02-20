0b251d070338ce14eadf230082a8234bac846264
Michael Shigorin <mike@altlinux.org>
2018-02-20 18:44:53 +0300

    regular.mk: drop xsane from mate
    The problem being worked around by this is:
    anything in the lists that Requires: webclient
    results in rekonq being pulled into the image
    (after mate-default requires firefox no more).
    
    The proper fix is to force *_PACKAGES, *_LISTS
    and *_REGEXP to be processed in a _single_
    transaction for each destination so that
    early mis-expansion of virtual packages
    doesn't occur when _installing_ those.
    
    This commit should be reverted then.
    
    See-also: https://bugzilla.altlinux.org/30806

--- conf.d/regular.mk
+++ conf.d/regular.mk
@@ -140,7 +140,6 @@ distro/regular-xmonad: distro/.regular-gtk use/x11/xmonad
 distro/regular-mate: distro/.regular-gtk +nm \
 	use/x11/mate use/fonts/ttf/google use/domain-client
 	@$(call add,LIVE_LISTS,$(call tags,mobile mate))
-	@$(call add,LIVE_LISTS,$(call tags,desktop sane))
 	@$(call add,LIVE_LISTS,$(call tags,base smartcard))
 
 distro/regular-mate-sysv: distro/.regular-sysv-gtk use/x11/mate +nm
