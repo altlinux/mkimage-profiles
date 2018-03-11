0ec1ecb7175b888e9caf901a918f83714329d40a
Michael Shigorin <mike@altlinux.org>
2018-03-11 20:19:59 +0300

    p8.mk: piggyback ibus-unicode
    ...into cinnamon and lxde flavours; it got dropped out from
    regular builds but this file controls p8-based starterkits
    specifically!
    
    PS: looks like unikey can get configured but either fails
        to actually work or I forgot how to use it already...

--- conf.d/p8.mk
+++ conf.d/p8.mk
@@ -5,15 +5,19 @@ mixin/p8: use/browser/firefox/esr
 	@$(call set,IMAGE_FLAVOUR,$(subst alt-p8-,,$(IMAGE_NAME)))
 	@$(call set,META_VOL_ID,ALT p8 $$(IMAGE_FLAVOUR)/$(ARCH))
 
+# missing in sisyphus but not in p8
+mixin/p8/unikey:
+	@$(call add,THE_PACKAGES,ibus-unikey)
+
 ifeq (distro,$(IMAGE_CLASS))
 
-distro/alt-p8-cinnamon: distro/regular-cinnamon mixin/p8; @:
+distro/alt-p8-cinnamon: distro/regular-cinnamon mixin/p8 mixin/p8/unikey; @:
 distro/alt-p8-icewm: distro/regular-icewm mixin/p8; @:
 distro/alt-p8-gnome3: distro/regular-gnome3 mixin/p8; @:
 distro/alt-p8-gnustep: distro/regular-gnustep mixin/p8; @:
 distro/alt-p8-kde4: distro/regular-kde4 mixin/p8; @:
 distro/alt-p8-kde5: distro/regular-kde5 mixin/p8; @:
-distro/alt-p8-lxde: distro/regular-lxde mixin/p8; @:
+distro/alt-p8-lxde: distro/regular-lxde mixin/p8 mixin/p8/unikey; @:
 distro/alt-p8-lxde-sysv: distro/regular-lxde-sysv mixin/p8; @:
 distro/alt-p8-lxqt: distro/regular-lxqt mixin/p8; @:
 distro/alt-p8-lxqt-sysv: distro/regular-lxqt-sysv mixin/p8; @:
