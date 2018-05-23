bc37f8840761d480f90c36efd8c71e7e91a3b95d
Michael Shigorin <mike@altlinux.org>
2018-05-23 11:54:11 +0300

    e2k: cleanup comments
    Forgot to do that before merging, sorry.
    
    (xorg-drv-ati cares for dependencies
    but we still prefer modesetting driver
    in the installers to make them versatile)

--- features.in/e2k/config.mk
+++ features.in/e2k/config.mk
@@ -9,7 +9,7 @@ use/e2k/x11: use/e2k use/x11
 
 use/e2k/x11/401 use/e2k/x11/801: use/e2k/x11/%: use/e2k/x11
 	@$(call add,THE_PACKAGES,xorg-conf-e$*-radeon)
-	@$(call add,THE_PACKAGES,xorg-drv-ati)	### why not just radeon?
+	@$(call add,THE_PACKAGES,xorg-drv-ati)
 
 use/e2k/x11/101: use/e2k/x11
 	@$(call add,THE_PACKAGES,xorg-conf-e101-modesetting)
@@ -23,7 +23,6 @@ use/e2k/install2/desktop: use/e2k/install2
 
 use/e2k/install2/401 use/e2k/install2/801 use/e2k/install2/101: \
 	use/e2k/install2/%: use/e2k/install2/desktop
-	@#$(call add,INSTALL2_PACKAGES,xorg-conf-e$*-radeon)
 	@$(call add,INSTALL2_PACKAGES,xorg-conf-e$*-modesetting)
 	@$(call add,INSTALL2_CLEANUP_PACKAGES,llvm)
 
