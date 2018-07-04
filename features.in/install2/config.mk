40cd1c580f95e16bb26d27717f95842e4ce7daed
Michael Shigorin <mike@altlinux.org>
2018-07-04 15:18:51 +0300

    install2: use/install2/fat target
    This adds mkfs.fat and fsck.fat utilities to installer
    so it's able to create/mount this "filesystem" as well.

--- features.in/install2/config.mk
+++ features.in/install2/config.mk
@@ -86,6 +86,9 @@ use/install2/jfs:
 use/install2/reiserfs:
 	@$(call add,SYSTEM_PACKAGES,reiserfsprogs)
 
+use/install2/fat:
+	@$(call add,SYSTEM_PACKAGES,dosfstools)
+
 # prepare bootloader for software suspend (see also live)
 use/install2/suspend:
 	@$(call add,INSTALL2_PACKAGES,installer-feature-desktop-suspend-stage2)
