ba437ba8281b12148c577f6378ed2b52d880139e
Michael Shigorin <mike@altlinux.org>
2018-07-20 16:53:15 +0300

    basealt.mk: factor out mixin-basealt.mk
    It's the common part that should be more or less architecture
    independent (given enough packages in the corresponding repo)
    that's been moved into a mixin; archdep bits kept in basealt.mk
    (albeit rewritten).
    
    Note that this approach should help building ALT Workstation
    for both new arches (like aarch64 or mipsel) and new targets
    (like vm) altogether.

--- conf.d/basealt.mk
+++ conf.d/basealt.mk
@@ -1,76 +1,41 @@
 ifeq (distro,$(IMAGE_CLASS))
 
-distro/alt-workstation: workstation_groups = $(addprefix workstation/,\
-	10-office 20-networking 30-multimedia 40-virtualization 50-publishing \
-	3rdparty agents blender clamav cloud-clients emulators freecad \
-	ganttproject gnome-peer-to-peer graphics-editing gtk-dictionary \
-	kvm libreoffice mate-usershare pidgin raccess \
-	scanning scribus smartcard sound-editing thunderbird virtualbox \
-	vlc voip-clients freeipa-client)
+ifeq (,$(filter-out i586 x86_64,$(ARCH)))
+distro/alt-workstation: workstation_groups_x86 = $(addprefix workstation/,\
+	3rdparty blender clamav cloud-clients freecad gnome-peer-to-peer \
+	gtk-dictionary kvm smartcard virtualbox voip-clients)
 
-distro/alt-workstation: distro/.base use/luks  \
-	+installer +power +systemd +pulse +vmguest +wireless +efi \
-	use/kernel/net use/docs/license \
-	use/memtest use/bootloader/grub \
-	use/install2/fs use/install2/vnc use/install2/repo \
-	use/install2/suspend use/x11/xorg use/sound use/xdg-user-dirs \
-	mixin/desktop-installer \
-	use/efi/refind use/efi/memtest86 use/efi/shell use/rescue/base \
-	use/branding/complete \
-	use/fonts/install2 use/install2/fs \
-	use/fonts/otf/adobe use/fonts/otf/mozilla \
-	use/fonts/ttf/google/extra use/fonts/ttf/redhat use/fonts/ttf/ubuntu \
-	use/l10n/default/ru_RU \
-	use/control use/services \
-	use/live/install use/live/suspend use/live/x11 use/live/repo \
-	use/live/rw \
-	use/x11/lightdm/gtk use/docs/manual use/x11/gtk/nm +nm \
-	use/fonts/ttf/google use/domain-client/full \
-	use/browser/firefox use/browser/firefox/esr \
-	use/x11/amdgpu
-	@$(call set,GLOBAL_LIVE_NO_CLEANUPDB,true)
-	@$(call set,BRANDING,alt-workstation)
-	@$(call add,THE_BRANDING,mate-settings)
-	@$(call add,STAGE1_MODLISTS,stage2-mmc)
-	@$(call set,INSTALLER,altlinux-desktop)
-	@$(call add,INSTALL2_PACKAGES,alterator-notes)
-	@$(call add,INSTALL2_PACKAGES,volumes-profile-regular)
+distro/alt-workstation: distro/.base +power +vmguest +wireless +efi \
+	mixin/desktop-installer mixin/alt-workstation \
+        use/memtest use/rescue/base use/bootloader/grub use/luks \
+        use/efi/refind use/efi/memtest86 use/efi/shell \
+	use/install2/suspend use/live/suspend \
+        use/live/install use/live/x11 use/live/repo use/live/rw \
+	use/branding/complete use/docs/license \
+        use/domain-client/full use/x11/amdgpu
 	@$(call add,INSTALL2_PACKAGES,open-iscsi)
 	@$(call add,INSTALL2_PACKAGES,xorg-conf-synaptics)
-	@$(call add,INSTALL2_PACKAGES,fdisk)
-	@$(call add,COMMON_PACKAGES,vim-console)
 	@$(call add,MAIN_PACKAGES,solaar)
-	@$(call add,MAIN_GROUPS,$(workstation_groups))
+	@$(call add,MAIN_GROUPS,$(workstation_groups_x86))
 	@$(call add,MAIN_LISTS,workstation/extras)
 	@$(call add,MAIN_LISTS,$(call tags,xorg vaapi))
-	@$(call add,BASE_LISTS,workstation/base.pkgs)
-	@$(call add,BASE_LISTS,$(call tags,desktop cups))
-	@$(call add,LIVE_LISTS,workstation/live.pkgs)
-	@$(call add,LIVE_LISTS,$(call tags,desktop sane))
-	@$(call add,LIVE_LISTS,$(call tags,desktop office))
-	@$(call add,THE_LISTS,workstation/mate)
-	@$(call add,THE_LISTS,workstation/the.pkgs)
-	@$(call add,THE_LISTS,$(call tags,regular desktop))
-	@$(call add,THE_LISTS,$(call tags,base regular))
-	@$(call add,THE_LISTS,$(call tags,base l10n))
-	@$(call add,THE_LISTS,$(call tags,base desktop))
 	@$(call add,THE_LISTS,$(call tags,archive extra))
 	@$(call add,THE_LISTS,$(call tags,mobile mate))
-	@$(call add,THE_KMODULES,staging)
 	@$(call add,BASE_KMODULES,kvm virtualbox)
 	@$(call add,CLEANUP_BASE_PACKAGES,acpid-events-power)
-	@$(call add,CLEANUP_PACKAGES,xterm)
-	@$(call add,EFI_BOOTARGS,lang=ru_RU)
 	@$(call add,RESCUE_BOOTARGS,nomodeset vga=0)
-	@$(call add,CONTROL,xdg-user-dirs:enabled)
-	@$(call add,SERVICES_ENABLE,sshd)
-	@$(call add,SERVICES_ENABLE,cups smb nmb httpd2 bluetoothd libvirtd)
-	@$(call add,DEFAULT_SERVICES_ENABLE,fstrim.timer)
-	@$(call add,DEFAULT_SERVICES_DISABLE,powertop bridge gpm)
-	@$(call set,META_PUBLISHER,BaseALT Ltd)
-	@$(call set,META_VOL_SET,ALT)
-	@$(call set,META_VOL_ID,ALT Workstation)
-	@$(call set,META_APP_ID,$(DISTRO_VERSION)/$(ARCH))
-	@$(call set,DOCS,alt-workstation)
+	@$(call add,EFI_BOOTARGS,lang=ru_RU)
+endif
+
+ifeq (,$(filter-out e2k%,$(ARCH)))
+distro/.alt-workstation: distro/.e2k-installer mixin/alt-workstation
+	@$(call add,THE_PACKAGES,setup-mate-terminal)
+	@$(call add,THE_PACKAGES,setup-mate-nocomposite)
+	@$(call add,THE_PACKAGES,firmware-linux)
+
+distro/alt-workstation-101: distro/.alt-workstation use/e2k/101; @:
+distro/alt-workstation-401: distro/.alt-workstation use/e2k/401; @:
+distro/alt-workstation-801: distro/.alt-workstation use/e2k/801; @:
+endif
 
 endif
---
+++ conf.d/mixin-basealt.mk
@@ -0,0 +1,52 @@
+# shared across all supported arches, can be complemented per arch
+
+mixin/alt-workstation: workstation_groups = $(addprefix workstation/,\
+	10-office 20-networking 30-multimedia 40-virtualization 50-publishing \
+	agents emulators ganttproject graphics-editing libreoffice \
+	mate-usershare pidgin raccess scanning scribus sound-editing \
+	thunderbird vlc freeipa-client)
+
+mixin/alt-workstation: +installer +systemd +pulse +nm \
+	use/kernel/net use/l10n/default/ru_RU \
+	use/x11/xorg use/x11/lightdm/gtk use/x11/gtk/nm \
+	use/install2/fs use/install2/vnc use/install2/repo \
+	use/fonts/install2 \
+	use/fonts/otf/adobe use/fonts/otf/mozilla \
+	use/fonts/ttf/google use/fonts/ttf/google/extra \
+	use/fonts/ttf/redhat use/fonts/ttf/ubuntu \
+	use/control use/services \
+	use/sound use/xdg-user-dirs use/docs/manual \
+	use/browser/firefox use/browser/firefox/esr
+	@$(call set,BRANDING,alt-workstation)
+	@$(call add,THE_BRANDING,mate-settings)
+	@$(call add,STAGE1_MODLISTS,stage2-mmc)
+	@$(call set,INSTALLER,altlinux-desktop)
+	@$(call add,INSTALL2_PACKAGES,alterator-notes)
+	@$(call add,INSTALL2_PACKAGES,volumes-profile-regular)
+	@$(call add,INSTALL2_PACKAGES,fdisk)
+	@$(call add,COMMON_PACKAGES,vim-console)
+	@$(call add,MAIN_GROUPS,$(workstation_groups))
+	@$(call add,BASE_LISTS,workstation/base.pkgs)
+	@$(call add,BASE_LISTS,$(call tags,desktop cups))
+	@$(call add,LIVE_LISTS,workstation/live.pkgs)
+	@$(call add,LIVE_LISTS,$(call tags,desktop sane))
+	@$(call add,LIVE_LISTS,$(call tags,desktop office))
+	@$(call add,THE_LISTS,workstation/mate)
+	@$(call add,THE_LISTS,workstation/the.pkgs)
+	@$(call add,THE_LISTS,$(call tags,regular desktop))
+	@$(call add,THE_LISTS,$(call tags,base regular))
+	@$(call add,THE_LISTS,$(call tags,base l10n))
+	@$(call add,THE_LISTS,$(call tags,base desktop))
+	@$(call add,THE_KMODULES,staging)
+	@$(call add,CLEANUP_PACKAGES,xterm)
+	@$(call set,GLOBAL_LIVE_NO_CLEANUPDB,true)
+	@$(call add,CONTROL,xdg-user-dirs:enabled)
+	@$(call add,SERVICES_ENABLE,sshd)
+	@$(call add,SERVICES_ENABLE,cups smb nmb httpd2 bluetoothd libvirtd)
+	@$(call add,DEFAULT_SERVICES_ENABLE,fstrim.timer)
+	@$(call add,DEFAULT_SERVICES_DISABLE,powertop bridge gpm)
+	@$(call set,META_PUBLISHER,BaseALT Ltd)
+	@$(call set,META_VOL_SET,ALT)
+	@$(call set,META_VOL_ID,ALT Workstation)
+	@$(call set,META_APP_ID,$(DISTRO_VERSION)/$(ARCH))
+	@$(call set,DOCS,alt-workstation)
