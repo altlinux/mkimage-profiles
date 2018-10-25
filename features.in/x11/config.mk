18733c176a053fc4e291e34f732c397e0a0ba0e3
Anton Midyukov <antohami@altlinux.org>
2018-10-25 12:45:19 +0700

    new target use/x11/lxdm
    lxdm - lightweight display manager for LXDE

--- features.in/x11/config.mk
+++ features.in/x11/config.mk
@@ -93,6 +93,9 @@ use/x11/gdm2.20: use/x11-autostart
 use/x11/sddm: use/x11-autostart
 	@$(call add,THE_PACKAGES,sddm)
 
+use/x11/lxdm: use/x11-autostart
+	@$(call add,THE_PACKAGES,lxde-lxdm)
+
 use/x11/icewm: use/x11
 	@$(call add,THE_LISTS,$(call tags,icewm desktop))
 
