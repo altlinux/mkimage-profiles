7ef157e2b12a542922289c31b95797e12b54b54b
Anton Midyukov <antohami@altlinux.org>
2019-02-12 21:03:38 +0700

    use/ntp/chrony: initial target
    chrony synchronizes time when connecting to a network using
    NetworkManager. This feature is great for computers without
    hardware clock, for example Raspberry Pi.

--- features.in/ntp/config.mk
+++ features.in/ntp/config.mk
@@ -1,9 +1,18 @@
 use/ntp: use/services use/pkgpriorities
 	@$(call add_feature)
-	@$(call add,THE_PACKAGES,openntpd)
-	@$(call add,PINNED_PACKAGES,openntpd)
-	@$(call add,DEFAULT_SERVICES_ENABLE,ntpd)
+	@$(call add,THE_PACKAGES,$$(THE_NTPD))
+	@$(call add,PINNED_PACKAGES,$$(THE_NTPD))
+	@$(call add,DEFAULT_SERVICES_ENABLE,$$(THE_NTPD_SERVICE))
+	@$(call try,THE_PACKAGES,openntpd)
+	@$(call try,PINNED_PACKAGES,openntpd)
+	@$(call try,DEFAULT_SERVICES_ENABLE,ntpd)
 
 use/ntp/client: use/ntp
+	@$(call set,THE_NTPD,openntpd)
+	@$(call set,THE_NTPD_SERVICE,ntpd)
 	@$(call add,NTP_SERVERS,pool.ntp.org)
 	@$(call xport,NTP_SERVERS)
+
+use/ntp/chrony: use/ntp
+	@$(call set,THE_NTPD,chrony)
+	@$(call set,THE_NTPD_SERVICE,chronyd)
