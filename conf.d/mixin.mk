c6e716b3fc8fc72dbba03aecb30f301cd833a5b4
Anton Midyukov <antohami@altlinux.org>
2019-02-14 21:38:13 +0700

    mixin/regular-xfce: drop xscreensaver
    xfce4-full now provides its own xfce4-screensaver

--- conf.d/mixin.mk
+++ conf.d/mixin.mk
@@ -80,7 +80,6 @@ mixin/regular-gnustep: use/x11/gnustep use/x11/gdm2.20 use/mediacheck \
 
 mixin/regular-xfce: use/x11/xfce use/x11/gtk/nm +nm \
 	use/fonts/ttf/redhat use/fonts/ttf/google/extra
-	@$(call add,THE_PACKAGES,xscreensaver-frontend)
 
 mixin/regular-xfce-sysv: use/init/sysv/polkit use/deflogin/sysv/nm \
 	use/x11/lightdm/gtk \
