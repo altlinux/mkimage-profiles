c602ba7904839142cee295c4901adbd26b2f57e8
Mikhail Efremov <sem@altlinux.org>
2019-03-11 18:41:32 +0300

    slinux: Drop installer-distro-desktop

--- features.in/slinux/config.mk
+++ features.in/slinux/config.mk
@@ -20,7 +20,6 @@ use/slinux/base: use/isohybrid use/slinux use/x11/xorg use/x11/lightdm/gtk +puls
 	@$(call add,THE_LISTS,slinux/misc-base)
 	@$(call add,THE_LISTS,slinux/xfce-base)
 	@$(call add,THE_LISTS,$(call tags,base l10n))
-	@$(call add,STAGE2_PACKAGES,installer-distro-desktop)
 	@$(call add,STAGE2_PACKAGES,xorg-conf-libinput-touchpad)
 	@$(call add,THE_KMODULES,staging)
 	@$(call add,STAGE1_MODLISTS,stage2-mmc)
