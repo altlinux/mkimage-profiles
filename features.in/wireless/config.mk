5fb451ec9b282967a71bba38a10cb9106259a2db
Anton Midyukov <antohami@altlinux.org>
2019-04-06 00:03:25 +0700

    wireless: add rtl8821ce

--- features.in/wireless/config.mk
+++ features.in/wireless/config.mk
@@ -2,7 +2,7 @@
 
 use/wireless:
 	@$(call add_feature)
-	@$(call add,THE_KMODULES,bcmwl rt3070 rtl8192 rtl8723de staging)
+	@$(call add,THE_KMODULES,bcmwl rt3070 rtl8192 rtl8723de rtl8821ce staging)
 	@$(call add,THE_LISTS,tools/wireless)
 
 use/wireless/full: use/wireless use/kernel/wireless; @:
