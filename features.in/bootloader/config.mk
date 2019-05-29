572aa46ddbc211a79785fc9c4f863cb1237285d1
Anton Midyukov <antohami@altlinux.org>
2019-05-29 23:07:56 +0700

    bootloader: os-prober appears to be generally available

--- features.in/bootloader/config.mk
+++ features.in/bootloader/config.mk
@@ -19,6 +19,4 @@ use/bootloader/live: use/bootloader
 	@$(call add,LIVE_PACKAGES,alterator-$$(BASE_BOOTLOADER))
 
 use/bootloader/os-prober: use/bootloader
-ifeq (,$(filter-out i586 x86_64 aarch64,$(ARCH)))
 	@$(call add,BASE_PACKAGES,os-prober)
-endif
