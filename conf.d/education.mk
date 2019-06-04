d603a36e6435b9de35ec33a3679653b6a55f6c8d
Anton Midyukov <antohami@altlinux.org>
2019-06-05 01:46:35 +0700

    education.mk: initial build for aarch64

--- conf.d/education.mk
+++ conf.d/education.mk
@@ -1,38 +1,49 @@
 # ALT Education
 
+mixin/education: use/kernel use/firmware \
+	+systemd \
+	use/services \
+	use/ntp/chrony \
+	use/x11/xorg use/x11/lightdm/gtk +pulse \
+	+nm use/x11/gtk/nm \
+	use/xdg-user-dirs/deep
+	@$(call set,BRANDING,alt-education)
+	@$(call add,THE_BRANDING,indexhtml)
+	@$(call add,THE_BRANDING,menu xfce-settings system-settings)
+	@$(call add,THE_LISTS,slinux/xfce-base)
+	@$(call add,THE_LISTS,slinux/misc-base)
+	@$(call add,THE_LISTS,gnome-p2p)
+	@$(call add,THE_LISTS,$(call tags,base l10n))
+	@$(call add,BASE_LISTS,education/base)
+	@$(call add,BASE_LISTS,workstation/3rdparty)
+	@$(call add,THE_LISTS,education/misc)
+	@$(call add,THE_PACKAGES,usbutils pv lftp links2 openssh-server xinput xorg-drv-libinput)
+	@$(call add,THE_PACKAGES,net-tools fdisk gdisk parted partclone)
+	@$(call add,THE_PACKAGES,disable-usb-autosuspend)
+	@$(call add,THE_LISTS,$(call tags,base regular))
+	@$(call add,SERVICES_ENABLE,bluetoothd sshd bind crond alteratord cups ahttpd)
+
 ifeq (distro,$(IMAGE_CLASS))
 
-distro/education: distro/alt-education
-distro/alt-education: distro/.installer \
-	+systemd \
+distro/education: distro/alt-education; @:
+distro/alt-education: distro/.installer mixin/education \
 	use/memtest \
-	use/services \
+	use/branding/complete \
 	use/live/install use/live/suspend \
 	use/live/repo use/live/x11 use/live/rw \
 	use/install2/vnc use/install2/full \
 	use/l10n/default/ru_RU +vmguest \
 	+efi use/efi/refind use/efi/shell \
-	use/ntp/chrony \
-	use/isohybrid use/x11/xorg use/x11/lightdm/gtk +pulse use/luks \
-	+plymouth +nm use/x11/gtk/nm +wireless \
-	use/xdg-user-dirs/deep use/install2/fonts \
-	use/branding/complete
+	use/isohybrid use/luks \
+	+plymouth +wireless \
+	use/install2/fonts
 	@$(call set,INSTALLER,junior)
-	@$(call set,BRANDING,alt-education)
-	@$(call add,THE_BRANDING,menu xfce-settings system-settings)
 	@$(call set,META_APP_ID,$(DISTRO_VERSION)/$(ARCH))
 	@$(call set,META_VOL_SET,ALT Education 8.2)
 	@$(call set,META_PUBLISHER,BaseALT Ltd)
 	@$(call add,INSTALL2_PACKAGES,disable-usb-autosuspend)
 	@$(call add,INSTALL2_PACKAGES,installer-feature-samba-usershares-stage2)
-	@$(call add,THE_LISTS,slinux/xfce-base)
-	@$(call add,THE_LISTS,slinux/misc-base)
-	@$(call add,THE_LISTS,gnome-p2p)
-	@$(call add,THE_LISTS,$(call tags,base l10n))
 	@$(call add,MAIN_LISTS,slinux/not-install-full)
-	@$(call add,BASE_LISTS,education/base)
-	@$(call add,BASE_LISTS,workstation/3rdparty)
-	@$(call add,THE_LISTS,education/misc)
 	@$(call add,MAIN_GROUPS,education/01_preschool)
 	@$(call add,MAIN_GROUPS,education/02_gradeschool)
 	@$(call add,MAIN_GROUPS,education/03_highschool)
@@ -47,18 +58,16 @@ distro/alt-education: distro/.installer \
 	@$(call add,THE_KMODULES,lsadrv bbswitch)
 	@$(call add,THE_KMODULES,staging)
 	@$(call add,MAIN_KMODULES,bbswitch)
-	@$(call add,THE_PACKAGES,usbutils pv syslinux lftp links2 openssh-server xinput xorg-drv-libinput)
-	@$(call add,THE_PACKAGES,net-tools fdisk gdisk parted partclone)
-	@$(call add,THE_PACKAGES,xorg-drv-vmware xorg-drv-vmmouse disable-usb-autosuspend)
+	@$(call add,THE_PACKAGES,xorg-drv-vmware xorg-drv-vmmouse)
 	@$(call add,THE_PACKAGES,bluez pulseaudio-bluez)
 	@$(call add,THE_PACKAGES,alterator-fbi)
+	@$(call add,THE_PACKAGES,syslinux)
 	@$(call add,BASE_PACKAGES,os-prober)
 	@$(call add,BASE_PACKAGES,guest-account)
 	@$(call add,BASE_PACKAGES,nextcloud-client)
 	@$(call add,MAIN_PACKAGES,iperf3 owamp-server)
 	@$(call add,MAIN_PACKAGES,stellarium)
 	@$(call add,MAIN_PACKAGES,lmms)
-	@$(call add,THE_LISTS,$(call tags,base regular))
 	@$(call add,STAGE1_MODLISTS,stage2-mmc)
 	@$(call set,GLOBAL_LIVE_NO_CLEANUPDB,true)
 	@$(call add,LIVE_PACKAGES,livecd-timezone)
@@ -66,5 +75,15 @@ distro/alt-education: distro/.installer \
 	@$(call add,STAGE2_PACKAGES,xorg-conf-libinput-touchpad)
 	@$(call add,STAGE2_PACKAGES,chrony)
 	@$(call add,STAGE1_MODLISTS,stage2-mmc)
-	@$(call add,SERVICES_ENABLE,bluetoothd sshd bind crond alteratord cups ahttpd)
+endif
+
+ifeq (vm,$(IMAGE_CLASS))
+ifeq (,$(filter-out aarch64 armh,$(ARCH)))
+
+vm/education: vm/alt-education; @:
+vm/alt-education: vm/systemd use/repo use/x11/armsoc \
+	use/oem use/bootloader/uboot mixin/education
+	@$(call add,DEFAULT_SERVICES_DISABLE,multipathd)
+
+endif
 endif
