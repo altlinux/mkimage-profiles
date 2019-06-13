dc2101a01d74761710253d78d3440045b9a564b6
Anton Midyukov <antohami@altlinux.org>
2019-06-13 20:15:41 +0700

    uboot: setup serial tty for qemu

--- features.in/uboot/config.mk
+++ features.in/uboot/config.mk
@@ -1,3 +1,13 @@
-use/uboot: use/kernel
+ifeq (,$(filter-out qcow2 qcow2c,$(IMAGE_TYPE)))
+ifeq (,$(filter-out aarch64 armh,$(ARCH)))
+UBOOT_TTY := use/tty/AMA0
+else
+UBOOT_TTY := /use/tty/S0
+endif
+endif
+
+use/uboot: use/kernel/initrd-setup $(UBOOT_TTY)
 	@$(call add_feature)
 	@$(call add,THE_LISTS,singleboard-tools)
+	@$(call try,TTY_DEV,tty1)
+	@$(call xport,TTY_DEV)
