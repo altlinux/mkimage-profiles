d2937227ce8824aa7d3e042ddd804368c2a2b817
Anton Midyukov <antohami@altlinux.org>
2019-07-03 01:59:32 +0700

    syslinux: X86 only

--- features.in/syslinux/config.mk
+++ features.in/syslinux/config.mk
@@ -1,5 +1,6 @@
 # default is plain text prompt
 # NB: might be usbflash-ready hybrid iso
+ifeq (,$(filter-out i586 x86_64,$(ARCH)))
 use/syslinux: sub/stage1 $(ISOHYBRID:%=use/isohybrid)
 	@$(call add_feature)
 	@$(call add,STAGE1_PACKAGES,syslinux)
@@ -7,14 +8,19 @@ use/syslinux: sub/stage1 $(ISOHYBRID:%=use/isohybrid)
 	@$(call try,BOOTVGA,normal)
 	@$(call set,RELNAME,ALT ($(IMAGE_NAME)))
 	@$(call set,IMAGE_PACKTYPE,boot)
+else
+use/syslinux: ; @:
+endif
 
 # UI is overwritten
 use/syslinux/ui/%: use/syslinux
 	@$(call set,SYSLINUX_UI,$*)
+ifeq (,$(filter-out i586 x86_64,$(ARCH)))
 	@if [ "$*" == gfxboot ]; then \
 		$(call add,STAGE1_PACKAGES,gfxboot); \
 		$(call add,STAGE1_BRANDING,bootloader); \
 	fi
+endif
 
 # modules and config snippets just add up
 use/syslinux/%.com use/syslinux/%.c32: use/syslinux
