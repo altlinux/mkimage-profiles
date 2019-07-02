fddbb12ab48898894ece45df01f9578e2e5ec585
Anton Midyukov <antohami@altlinux.org>
2019-07-03 01:55:13 +0700

    memtest: x86-only

--- features.in/memtest/config.mk
+++ features.in/memtest/config.mk
@@ -1,6 +1,9 @@
+ifeq (,$(filter-out i586 x86_64,$(ARCH)))
 use/memtest: use/syslinux
 	@$(call add_feature)
 	@$(call add,SYSTEM_PACKAGES,memtest86+)
 	@$(call add,SYSLINUX_CFG,memtest)
-
+else
+use/memtest: ; @:
+endif
 # see also use/efi/memtest86
