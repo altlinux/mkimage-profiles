353a354800e33ffab1e9c43c5bb4bdee3a2c8e8e
Gleb Fotengauer-Malinovskiy <glebfm@altlinux.org>
2019-07-09 13:40:27 +0300

    Untie plymouth feature from gfxboot

--- features.in/plymouth/config.mk
+++ features.in/plymouth/config.mk
@@ -14,7 +14,7 @@ use/plymouth:
 # looks like there's no sense to pull in progressbars
 # while leaving the very basic text bootloader screen
 use/plymouth/stage2: use/plymouth use/branding \
-	use/syslinux/ui/gfxboot use/stage2/kms
+	use/stage2/kms
 	@$(call add,STAGE1_PACKAGES_REGEXP,make-initrd-plymouth)
 	@$(call add,STAGE1_BRANDING,bootsplash)
 	@$(call add,STAGE2_BRANDING,bootsplash)
