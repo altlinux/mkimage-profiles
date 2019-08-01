b150f9097b1e7ac6373a89b22c9590d143b78f87
Anton Midyukov <antohami@altlinux.org>
2019-08-02 01:24:06 +0700

    armh-mcom02: use xorg-drv-fbturbo
    xorg-drv-fbturbo is recommended by ELVIS and supports working
    with the proprietary mali driver.

--- features.in/armh-mcom02/config.mk
+++ features.in/armh-mcom02/config.mk
@@ -4,4 +4,7 @@ use/armh-mcom02: use/uboot use/tty/S0
 	@$(call add_feature)
 	@$(call set,KFLAVOURS,mcom02)
 	@$(call add,THE_PACKAGES,u-boot-mcom02-firmware-tools u-boot-mcom02)
+
+use/armh-mcom02/x11: use/armh-mcom02
+	@$(call add,THE_PACKAGES,xorg-drv-fbturbo)
 endif
