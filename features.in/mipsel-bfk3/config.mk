f9d5a19ffc3b9fe04023c01b7548cc7fae2c25dc
Anton Midyukov <antohami@altlinux.org>
2019-09-02 19:04:20 +0700

    mipsel-bfk3: add use/mipsel-bfk3/x11

--- features.in/mipsel-bfk3/config.mk
+++ features.in/mipsel-bfk3/config.mk
@@ -6,3 +6,5 @@ use/mipsel-bfk3: use/kernel
 	@$(call add,NET_ETH,eth1:dhcp)
 	@$(call add,NET_ETH,eth2:dhcp)
 	@$(call add,THE_PACKAGES,fbset-modes-sm750-bfk3)
+
+use/mipsel-bfk3/x11: use/mipsel-bfk3 use/x11/radeon; @:
