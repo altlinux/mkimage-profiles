4900dab9c464bfbe2f7e67987ab73a7c156dbb09
Mikhail Efremov <sem@altlinux.org>
2019-09-06 18:36:56 +0300

    slinux.mk, slinux: Update iso metainfo
    See thread
    https://lists.altlinux.org/pipermail/devel-distro/2019-July/001669.html
    for details.

--- conf.d/slinux.mk
+++ conf.d/slinux.mk
@@ -4,13 +4,12 @@ ifeq (distro,$(IMAGE_CLASS))
 
 distro/slinux-live: distro/.livecd-install use/slinux/base
 	@$(call add,THE_LISTS,slinux/live-install)
-	@$(call set,META_VOL_SET,Simply Linux live)
-	@$(call set,META_VOL_ID,Simply Linux live/$(ARCH))
+	@$(call set,META_VOL_ID,Simply Linux live $(DISTRO_VERSION) $(ARCH))
+	@$(call set,META_APP_ID,Simply Linux live $(DISTRO_VERSION) $(ARCH) $(shell date +%F))
 
 distro/slinux: distro/.installer use/slinux/full use/rescue/base
 	@$(call set,INSTALLER,simply-linux)
 	@$(call add,MAIN_GROUPS,slinux/dropbox slinux/emulators-full slinux/games-base slinux/games slinux/games-full slinux/graphics-base slinux/graphics-full-blender slinux/graphics slinux/graphics-full-inkscape slinux/graphics-full-shotwell slinux/graphics-full-synfigstudio slinux/multimedia-full-audacity slinux/multimedia-base slinux/multimedia-full-cheese slinux/multimedia slinux/multimedia-full-pitivi slinux/multimedia-full-sound-juicer slinux/comm-base)
-	@$(call set,META_VOL_SET,Simply Linux)
-	@$(call set,META_VOL_ID,Simply Linux/$(ARCH))
-
+	@$(call set,META_VOL_ID,Simply Linux $(DISTRO_VERSION) $(ARCH))
+	@$(call set,META_APP_ID,Simply Linux $(DISTRO_VERSION) $(ARCH) $(shell date +%F))
 endif
--- features.in/slinux/config.mk
+++ features.in/slinux/config.mk
@@ -3,7 +3,7 @@ use/slinux: use/x11
 	@$(call set,BRANDING,simply-linux)
 	@$(call add,THE_BRANDING,menu xfce-settings system-settings)
 	@$(call set,META_PUBLISHER,BaseALT Ltd)
-	@$(call set,META_APP_ID,$(DISTRO_VERSION)/$(ARCH))
+	@$(call set,META_VOL_SET,ALT)
 
 use/slinux/base: use/isohybrid use/slinux use/x11/xorg use/x11/lightdm/gtk +pulse use/luks \
 	+plymouth +nm use/x11/gtk/nm use/memtest +systemd +wireless +vmguest use/l10n/default/ru_RU \
