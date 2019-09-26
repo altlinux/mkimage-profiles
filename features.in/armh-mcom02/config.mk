853c5e275d84d02ae544d58d4e45d58310c8430a
Anton Midyukov <antohami@altlinux.org>
2019-09-26 15:32:07 +0700

    aarch64-tegra, armh-mcom02: use/bootloader/u-boot
    It's required to set BASE_BOOTLOADER variable,
    otherwise grub will get in *too*.

--- features.in/armh-mcom02/config.mk
+++ features.in/armh-mcom02/config.mk
@@ -1,6 +1,6 @@
 
 ifeq (,$(filter-out armh,$(ARCH)))
-use/armh-mcom02: use/uboot use/tty/S0
+use/armh-mcom02: use/bootloader/uboot use/tty/S0
 	@$(call add_feature)
 	@$(call set,KFLAVOURS,mcom02)
 	@$(call add,THE_PACKAGES,u-boot-mcom02-firmware-tools u-boot-mcom02)
