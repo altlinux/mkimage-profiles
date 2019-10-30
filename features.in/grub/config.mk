300ee8a83f8edaee005c1720c216f8ecf1b29506
Alexey Shabalin <shaba@altlinux.org>
2019-10-30 20:36:22 +0300

    add grub feature

---
+++ features.in/grub/config.mk
@@ -0,0 +1,39 @@
+# default is plain text prompt
+# NB: might be usbflash-ready hybrid iso
+
+# on ppc64le syslinux feature is used only to generate config
+#ifeq (,$(filter-out i586 x86_64 ppc64le aarch64,$(ARCH)))
+ifeq (,$(filter-out i586 x86_64,$(ARCH)))
+
+use/grub: sub/stage1 $(ISOHYBRID:%=use/isohybrid)
+	@$(call add_feature)
+ifeq (,$(filter-out i586 x86_64,$(ARCH)))
+	@$(call add,STAGE1_PACKAGES,grub-efi shim-signed)
+	@$(call try,BOOTVGA,normal)
+endif
+	@$(call set,RELNAME,ALT ($(IMAGE_NAME)))
+	@$(call set,IMAGE_PACKTYPE,boot)
+else
+
+use/grub: ; @:
+
+endif
+
+ifeq (,$(filter-out i586 x86_64,$(ARCH)))
+# UI is overwritten
+use/grub/ui/%: use/grub
+	@$(call set,GRUB_UI,$*)
+	@if [ "$*" == gfxboot ]; then \
+		$(call add,STAGE1_PACKAGES,gfxboot); \
+		$(call add,STAGE1_BRANDING,bootloader); \
+	fi
+else
+use/grub/ui/%: use/grub; @:
+endif
+
+use/grub/%.cfg: use/grub
+	@$(call add,GRUB_CFG,$*)
+
+
+use/grub/timeout/%: use/grub
+	@$(call set,GRUB_TIMEOUT,$*)
---
+++ features.in/grub/generate.mk
@@ -0,0 +1,100 @@
+ifdef BUILDDIR
+
+# in deciseconds
+DEFAULT_TIMEOUT = 90
+
+# prepare data for grub installation;
+# see also stage1/scripts.d/01-grub
+
+include $(BUILDDIR)/distcfg.mk
+
+ifndef BOOTLOADER
+$(error grub feature enabled but BOOTLOADER undefined)
+endif
+
+ifndef GRUB_DIRECT
+# SUBPROFILES are considered GRUB_CFG too
+# (note these can appear like stage2@live);
+# 01defaults.cfg is included indefinitely
+GRUB_CFG := $(GRUB_CFG) $(SUBPROFILE_DIRS) defaults
+endif
+
+DSTDIR  := $(BUILDDIR)/stage1/files/EFI/BOOT/.in
+DSTCFGS := $(DSTDIR)/*.cfg
+
+# we can do GRUB_{CFG,MODULES,FILES}
+# CFG have only cfg snippet
+cfg = $(wildcard cfg.in/??$(1).cfg)
+
+# NB: list position determined by file numbering (*.cfg sorting)
+#
+# config snippets are copied into generated profile where they can
+# be also tested against grub modules (some can be unavailable);
+# we can't do tests right now since that implies host grub being
+# identical to build system one which might be not the case...
+#
+# have to piggyback parameters as we're running in host system yet,
+# and files involved will appear inside instrumental chroot
+#
+# arguments get evaluated before recipe body execution thus prep
+
+all: debug timeout
+	@### proper text branding should be implemented
+	@echo $(GRUB_FILES) > $(DSTDIR)/grub.list
+	@sed -i \
+		-e 's,@mkimage-profiles@,$(IMAGE_NAME),' \
+		-e 's,@relname@,$(RELNAME),' \
+		$(DSTCFGS)
+
+# integerity check
+timeout: distro
+	@if [ "$(GRUB_TIMEOUT)" -ge 0 ] 2>/dev/null; then \
+		TIMEOUT="$(GRUB_TIMEOUT)"; \
+	else \
+		TIMEOUT="$(DEFAULT_TIMEOUT)"; \
+	fi; \
+	sed -i "s,@timeout@,$$TIMEOUT," $(DSTCFGS)
+
+distro: bootargs
+	@if [ -n "$(META_VOL_SET)" ]; then \
+		DISTRO="$(META_VOL_SET)"; \
+	else \
+		DISTRO="ALT"; \
+	fi; \
+	sed -i "s,@distro@,$$DISTRO," $(DSTCFGS)
+
+# pass over additional parameters, if any
+bootargs: clean
+	@if [ -n "$(STAGE2_BOOTARGS)" ]; then \
+		sed -i "s,@bootargs@,$(STAGE2_BOOTARGS)," $(DSTCFGS); \
+	fi; \
+	sed -i "s,@bootargs@,," $(DSTCFGS)
+	@if [ -n "$(RESCUE_BOOTARGS)" ]; then \
+		sed -i "s,@rescue_bootargs@,$(RESCUE_BOOTARGS)," $(DSTCFGS); \
+	fi; \
+	sed -i "s,@rescue_bootargs@,," $(DSTCFGS)
+	@if [ -n "$(BOOTVGA)" ]; then \
+		sed -i "s,@bootvga@,$(BOOTVGA)," $(DSTCFGS); \
+	fi; \
+	sed -i "s,@bootvga@,,;s,vga= ,," $(DSTCFGS)
+
+clean: copy
+	@if [ "$(GRUB_UI)" = gfxboot ]; then \
+		sed -i "s/\^//;/menu label /d" $(DSTCFGS); \
+	fi
+
+copy: prep
+	@cp -pLt $(DSTDIR) -- $(sort \
+		$(foreach C,$(GRUB_CFG),$(call cfg,$(C))))
+
+prep:
+	@mkdir -p $(DSTDIR)
+
+debug:
+	@if [ -n "$(DEBUG)" ]; then \
+		echo "** BOOTLOADER: $(BOOTLOADER)"; \
+		echo "** GRUB_CFG: $(GRUB_CFG)"; \
+		echo "** GRUB_FILES: $(GRUB_FILES)"; \
+	fi
+
+endif
