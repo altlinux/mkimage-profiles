7fbd12af5c80f108ffd4969b4076f2086d151a7c
Anton Midyukov <antohami@altlinux.org>
2019-11-20 22:54:17 +0700

    aarch64-rpi4: add RPi-audioswitch
    Raspberry Pi 3/4 simply TUI switch between HDMI and 3,5mm
    headphone jack audio output.

--- features.in/aarch64-rpi4/config.mk
+++ features.in/aarch64-rpi4/config.mk
@@ -4,4 +4,6 @@ use/aarch64-rpi4: use/bootloader/uboot
 	@$(call add_feature)
 	@$(call set,KFLAVOURS,rpi-def rpi-un)
 	@$(call add,THE_PACKAGES,u-boot-tools)
+	@$(call add,THE_PACKAGES,RPi-audioswitch)
+
 endif
