0cd5e2ea3fe678c95cb72a504a54c989e9353a32
Anton Midyukov <antohami@altlinux.org>
2019-11-25 18:55:52 +0700

    aarch64-rpi4: add rpi4-boot-switch
    Switch of boot mode for Raspberry Pi 4
    In Alt, it is customary to load the kernel, dtb and initrd
    using u-boot and configuration in extlinux.conf.
    Firmware Raspberry Pi 4 loads and modifies dtb
    according to config.txt configuration.
    U-boot doesn't.
    The scripts in this package allow you to switch boot modes:
    Firmware loads the kernel, dtb, and initrd. U-boot is not used.
    Firmware loads dtb. U-boot loads the kernel and initrd.
    U-boot loads the kernel, dtb, and initrd.

--- features.in/aarch64-rpi4/config.mk
+++ features.in/aarch64-rpi4/config.mk
@@ -5,5 +5,6 @@ use/aarch64-rpi4: use/bootloader/uboot
 	@$(call set,KFLAVOURS,rpi-def rpi-un)
 	@$(call add,THE_PACKAGES,u-boot-tools)
 	@$(call add,THE_PACKAGES,RPi-audioswitch)
+	@$(call add,THE_PACKAGES,rpi4-boot-switch)
 
 endif
