cbd45bbdef589a1e1a09f52133f30ab61eda5efc
Anton Midyukov <antohami@altlinux.org>
2019-11-25 18:29:12 +0700

    mixin.mk: alteratord service on for all regular desktop

--- conf.d/mixin.mk
+++ conf.d/mixin.mk
@@ -59,6 +59,7 @@ mixin/regular-desktop: use/x11/xorg +alsa use/xdg-user-dirs \
 	@$(call add,DEFAULT_SERVICES_DISABLE,gssd idmapd krb5kdc rpcbind)
 	@$(call add,DEFAULT_SERVICES_ENABLE,bluetoothd)
 	@$(call add,DEFAULT_SERVICES_ENABLE,cups)
+	@$(call add,DEFAULT_SERVICES_ENABLE,alteratord)
 
 mixin/desktop-extra:
 	@$(call add,BASE_LISTS,$(call tags,(archive || base) && extra))
