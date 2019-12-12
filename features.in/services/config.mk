183d85fa9f1416d3715e286528f6591875256593
Mikhail Efremov <sem@altlinux.org>
2019-12-12 17:44:18 +0300

    features/services: Add SYSTEMD_SERVICES_{ENABLE,DISABLE}
    Support systemd-spicific services, e.g. *.socket amd .service units.

--- features.in/services/config.mk
+++ features.in/services/config.mk
@@ -4,6 +4,8 @@ use/services: sub/rootfs
 	@$(call xport,DEFAULT_SERVICES_DISABLE)
 	@$(call xport,SERVICES_ENABLE)
 	@$(call xport,SERVICES_DISABLE)
+	@$(call xport,SYSTEMD_SERVICES_ENABLE)
+	@$(call xport,SYSTEMD_SERVICES_DISABLE)
 
 use/services/lvm2-disable: use/services
 	@$(call add,DEFAULT_SERVICES_DISABLE,lvm2-lvmetad)
