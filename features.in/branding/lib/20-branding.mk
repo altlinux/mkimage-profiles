e6bb78a5690b36a11ec0c6fc1a35fd1bba723b5c
Anton Midyukov <antohami@altlinux.org>
2019-12-19 23:09:45 +0700

    branding: ImageMagick-tools for x86_64 only (refind needed)
    See-also: commit e4bb17279d35bfec36a7e6ca6f0435276b6af01f

--- features.in/branding/lib/20-branding.mk
+++ features.in/branding/lib/20-branding.mk
@@ -2,5 +2,8 @@
 
 DOT_BASE += $(call branding,$(THE_BRANDING))
 
+# needed for refind only
+ifeq (,$(filter-out x86_64,$(ARCH)))
 CHROOT_PACKAGES_REGEXP += $(call branding,bootloader)
 CHROOT_PACKAGES += ImageMagick-tools
+endif
