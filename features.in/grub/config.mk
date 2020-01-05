b6010f5bd4ed1be8d9575ef1e4426bad9f095bcf
Anton Midyukov <antohami@altlinux.org>
2020-01-05 21:51:30 +0700

    grub: available for i586, x86_64, aarch64, ppc64le

--- features.in/grub/config.mk
+++ features.in/grub/config.mk
@@ -1,9 +1,7 @@
 # default is plain text prompt
 # NB: might be usbflash-ready hybrid iso
 
-# on ppc64le syslinux feature is used only to generate config
-#ifeq (,$(filter-out i586 x86_64 ppc64le aarch64,$(ARCH)))
-ifeq (,$(filter-out i586 x86_64,$(ARCH)))
+ifeq (,$(filter-out i586 x86_64 ppc64le aarch64,$(ARCH)))
 
 use/grub: sub/stage1 $(ISOHYBRID:%=use/isohybrid)
 	@$(call add_feature)
