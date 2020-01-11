9804e0b216422d87ec51f54f201524ec793512ee
Anton Midyukov <antohami@altlinux.org>
2020-01-12 00:22:06 +0700

    hdt: for i586 and x86_64 only

--- features.in/hdt/config.mk
+++ features.in/hdt/config.mk
@@ -1,5 +1,9 @@
 # optimized out use/syslinux due to use/memtest
+ifeq (,$(filter-out i586 x86_64,$(ARCH)))
 use/hdt: use/memtest
 	@$(call add_feature)
 	@$(call add,STAGE1_PACKAGES,pciids)
 	@$(call add,SYSLINUX_MODULES,hdt)
+else
+use/hdt: ; @:
+endif
