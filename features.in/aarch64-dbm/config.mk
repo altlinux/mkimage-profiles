8bc377c7121d9702797998af86c544cf14eed5eb
Dmitriy Terekhin <jqt4@altlinux.org>
2020-01-21 18:08:08 +0400

    aarch64-dbm: added 93-grub2-param-dbm
    To load a Linux kernel onto a DBM board using GRUB2,
    you need specific parameters on the kernel command line
    and an explicit definition the dtb file to load.
    This script creates the required settings in the file
    /etc/sysconfig/grub2

--- features.in/aarch64-dbm/config.mk
+++ features.in/aarch64-dbm/config.mk
@@ -1,6 +1,6 @@
-
 ifeq (,$(filter-out aarch64,$(ARCH)))
-use/aarch64-dbm: use/efi/grub
+use/aarch64-dbm: use/efi/grub use/tty/S0
 	@$(call add_feature)
 	@$(call set,KFLAVOURS,bfkm)
+	@$(call add,BASE_BOOTARGS,video=HDMI-A-1:D fbcon=map:0)
 endif
