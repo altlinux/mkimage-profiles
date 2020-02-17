d6b13bb323595cc8a8e62d3a17ca2da805f8ee48
Anton Midyukov <antohami@altlinux.org>
2020-02-17 19:14:24 +0700

    mixin.mk: added pandoc into regular-xfce-sysv
    Suggested by Speccyfighter: https://forum.altlinux.org/index.php?topic=36177.msg346516#msg346516

--- conf.d/mixin.mk
+++ conf.d/mixin.mk
@@ -106,6 +106,7 @@ mixin/regular-xfce-sysv: mixin/xfce-base \
 	@$(call add,THE_PACKAGES,nload)
 	@$(call add,THE_PACKAGES,NetworkManager-tui)
 	@$(call add,THE_PACKAGES,apt-conf-ignore-systemd)
+	@$(call add,THE_PACKAGES,pandoc)
 
 mixin/regular-lxde: use/x11/lxde use/im +nm-gtk
 	@$(call add,THE_LISTS,$(call tags,desktop gvfs))
