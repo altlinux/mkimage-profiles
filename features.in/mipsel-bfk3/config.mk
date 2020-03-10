9620de4b703be43ad502f546f4f032f384fb31e5
Ivan A. Melnikov <iv@altlinux.org>
2020-03-10 17:17:26 +0400

    mipsel-bfk3: Specify DTB name in config.mk
    mipsel-bfk3 feature often serves as basis for special
    build for various other BE-T1000-based devices. Having
    DTB name in config.mk simplifies creating such derivatives.

--- features.in/mipsel-bfk3/config.mk
+++ features.in/mipsel-bfk3/config.mk
@@ -1,6 +1,8 @@
 use/mipsel-bfk3: use/kernel use/tty/S0 use/net-eth/dhcp/timeout/20
 	@$(call add_feature)
 	@$(call set,KFLAVOURS,bfk3-def)
+	@$(call set,DTB_NAME,baikal_bfk2)
+	@$(call xport,DTB_NAME)
 	@$(call set,VM_FSTYPE,ext2)
 	@$(call add,NET_ETH,eth0:dhcp)
 	@$(call add,NET_ETH,eth1:dhcp)
