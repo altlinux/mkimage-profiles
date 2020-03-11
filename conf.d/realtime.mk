4321717572bff515094d5723c2c02091c250d041
Anton Midyukov <antohami@altlinux.org>
2020-03-11 23:18:29 +0700

    realtime.mk: added cleanup packages for virtual machines

--- conf.d/realtime.mk
+++ conf.d/realtime.mk
@@ -1,7 +1,12 @@
-mixin/cnc-rt: +nm-gtk
+mixin/cnc-rt: use/cleanup +nm-gtk
 	@$(call set,KFLAVOURS,rt)
 	@$(call add,THE_LISTS,realtime/tests)
 	@$(call add,THE_LISTS,engineering/cnc)
+	@$(call add,CLEANUP_PACKAGES,virtualbox-guest-common)
+	@$(call add,CLEANUP_PACKAGES,open-vm-tools)
+	@$(call add,CLEANUP_PACKAGES,xorg-drv-vboxvideo xorg-drv-qxl)
+	@$(call add,CLEANUP_PACKAGES,spice-vdagent qemu-guest-agent)
+	@$(call add,CLEANUP_PACKAGES,xorg-drv-vmware xorg-drv-vmmouse open-vm-tools-desktop)
 
 ifeq (distro,$(IMAGE_CLASS))
 distro/cnc-rt: distro/regular-lxde mixin/cnc-rt
