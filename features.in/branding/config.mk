df6ce49a9d7c381410b74f11abc23d1c55b57b69
Anton Midyukov <antohami@altlinux.org>
2020-03-18 22:51:54 +0700

    branding: gfxboot and plymouth for all arches
    Partially this is a return of the commit 35db75378ee51155053342c93a10168c9809179e.
    For branding packages, regexp is used. Therefore, it makes no sense to limit them
    to architectures. use/plymouth and use/syslinux was fenced separately.

--- features.in/branding/config.mk
+++ features.in/branding/config.mk
@@ -7,21 +7,13 @@ use/branding/notes: use/branding
 	@$(call add,THE_BRANDING,notes)
 
 # NB: not every distro might have all the branding of its own
-ifeq (,$(filter-out i586 x86_64,$(ARCH)))
-use/branding/full: use/branding/notes use/syslinux/ui/gfxboot
+use/branding/full: use/branding/notes use/syslinux/ui/gfxboot \
+	use/grub/ui/gfxboot
 	@$(call add,THE_BRANDING,alterator bootloader bootsplash graphics)
 	@$(call add,THE_BRANDING,indexhtml slideshow)
 
 use/branding/complete: use/branding/full use/plymouth/full
 	@$(call add,INSTALL2_BRANDING,notes slideshow)
-else
-use/branding/full: use/branding/notes
-	@$(call add,THE_BRANDING,alterator graphics)
-	@$(call add,THE_BRANDING,indexhtml slideshow)
-
-use/branding/complete: use/branding/full
-	@$(call add,INSTALL2_BRANDING,notes slideshow)
-endif
 
 # http://altlinux.org/branding/slideshow
 use/branding/slideshow/once: use/branding
