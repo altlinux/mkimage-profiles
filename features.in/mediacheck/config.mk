1ada94366ad4d4df5eb39e6a0ed186508aed0589
Anton Midyukov <antohami@altlinux.org>
2020-05-20 14:35:55 +0700

    mediacheck: allowed for distro targets only

--- features.in/mediacheck/config.mk
+++ features.in/mediacheck/config.mk
@@ -1,3 +1,7 @@
+ifeq (distro,$(IMAGE_CLASS))
 use/mediacheck: use/stage2 sub/stage2@mediacheck use/syslinux/mediacheck.cfg
 	@$(call add_feature)
 	@$(call add,POSTPROCESS_TARGETS,90mediacheck)
+else
+use/mediacheck: ; @:
+endif
