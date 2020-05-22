5137e5ad9c908ece07175074dbb5e3b7205b9f90
Anton Midyukov <antohami@altlinux.org>
2020-05-22 18:24:34 +0700

    Rename aarch64-rpi4 feature to arm-rpi4
    The feature фкь-кзш4 is available for armh now.

--- features.in/arm-rpi4/config.mk
+++ features.in/arm-rpi4/config.mk
@@ -1,6 +1,5 @@
-
-ifeq (,$(filter-out aarch64,$(ARCH)))
-use/aarch64-rpi4: use/bootloader/uboot
+ifeq (,$(filter-out aarch64 armh,$(ARCH)))
+use/arm-rpi4: use/bootloader/uboot
 	@$(call add_feature)
 	@$(call set,KFLAVOURS,rpi-def rpi-un)
 	@$(call add,THE_PACKAGES,u-boot-tools)
