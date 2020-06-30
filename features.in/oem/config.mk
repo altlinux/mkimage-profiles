df0875fe22e1579b81c22936bab0934d5cd220e7
Anton Midyukov <antohami@altlinux.org>
2020-06-30 16:17:18 +0700

    oem: Added use/oem/vnc

--- features.in/oem/config.mk
+++ features.in/oem/config.mk
@@ -1,5 +1,12 @@
 use/oem: use/services use/branding
 	@$(call add_feature)
-	@$(call add,DEFAULT_SERVICES_ENABLE,messagebus alteratord setup)
+	@$(call add,DEFAULT_SERVICES_ENABLE,messagebus alteratord)
 	@$(call add,THE_PACKAGES,alterator-setup alterator-notes)
 	@$(call add,THE_BRANDING,alterator notes)
+	@$(call try,OEM_TARGET,setup)
+	@$(call add,DEFAULT_SERVICES_ENABLE,$$(OEM_TARGET))
+	@$(call xport,OEM_TARGET)
+
+use/oem/vnc: use/oem use/x11-vnc
+	@$(call add,THE_PACKAGES,alterator-setup-x11vnc)
+	@$(call set,OEM_TARGET,setup-vnc)
