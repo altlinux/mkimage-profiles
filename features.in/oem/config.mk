2d53a9af94a4ab9ca955c1d55529c59ef187b024
Anton Midyukov <antohami@altlinux.org>
2020-07-03 04:56:33 +0700

    net-eth, oem: use/oem/vnc for disable NetworkManager control eth0
    Network interface managed by NetworkManager is not activated when
    alterator-setup is launched.

--- features.in/oem/config.mk
+++ features.in/oem/config.mk
@@ -7,6 +7,8 @@ use/oem: use/services use/branding
 	@$(call add,DEFAULT_SERVICES_ENABLE,$$(OEM_TARGET))
 	@$(call xport,OEM_TARGET)
 
-use/oem/vnc: use/oem use/x11-vnc
+use/oem/vnc: use/oem use/x11-vnc use/net-eth/dhcp
 	@$(call add,THE_PACKAGES,alterator-setup-x11vnc)
 	@$(call set,OEM_TARGET,setup-vnc)
+	@$(call set,NMCTL,no)
+	@$(call xport,NMCTL)
