8db77d64628720a1b092555f494440cb38b70b1c
Michael Shigorin <mike@altlinux.org>
2020-08-21 21:26:49 +0300

    browsers: rewrote with per-arch browser lists
    There are arches with many browsers available
    and there are those with elinks; let's provide
    relevant targets to use at the very least.

--- features.in/browser/config.mk
+++ features.in/browser/config.mk
@@ -3,16 +3,26 @@ use/browser:
 	@$(call add,THE_PACKAGES,$$(THE_BROWSER))
 	@$(call try,THE_BROWSER,elinks)	# X11-less fallback
 
+# amend as neccessary; firefox is treated separately due to its flavours
+BROWSERS_i586 = chromium seamonkey netsurf epiphany falkon otter-browser \
+		elinks links2
+BROWSERS_x86_64 := $(BROWSERS_i586)
+BROWSERS_ppc64el = netsurf epiphany falkon otter-browser elinks links2
+BROWSERS_aarch64 = chromium netsurf epiphany falkon otter-browser elinks links2
+BROWSERS_armh = netsurf epiphany falkon otter-browser elinks links2
+BROWSERS_mipsel = chromium seamonkey netsurf epiphany falkon otter-browser \
+		  elinks links2
+BROWSERS_riscv64 = netsurf elinks
+BROWSERS_e2k = netsurf elinks links2
+BROWSERS_e2kv4 := $(BROWSERS_e2k)
+BROWSERS := $(BROWSERS_$(ARCH))
+
+$(addprefix use/browser/,$(BROWSERS)): use/browser/%: use/browser
+	@$(call set,THE_BROWSER,$*)
+
 # support both firefox and firefox-esr
 use/browser/firefox: use/browser
 	@$(call set,THE_BROWSER,firefox$$(FX_FLAVOUR))
-
-use/browser/seamonkey use/browser/netsurf \
-	use/browser/chromium use/browser/epiphany \
-	use/browser/falkon use/browser/otter-browser \
-	use/browser/elinks use/browser/links2: \
-	use/browser/%: use/browser
-	@$(call set,THE_BROWSER,$*)
 
 # the complete lack of dependencies is intentional
 use/browser/firefox/esr:
