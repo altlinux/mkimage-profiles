1cf05b639c23c404f307602d35e59337fb23987f
Michael Shigorin <mike@altlinux.org>
2020-08-21 20:21:53 +0300

    bootloader: just skip on e2k
    There's no need for bootloader on Elbrus platform
    since firmware boots Linux just fine; note that
    the whole feature is betted omitted since its
    script checks for the case when the feature
    is requested but not configured properly.

--- features.in/bootloader/config.mk
+++ features.in/bootloader/config.mk
@@ -9,6 +9,7 @@
 GRUB_ARCHES := i586 x86_64 aarch64 ppc64le
 
 use/bootloader: use/pkgpriorities
+ifneq (,$(filter-out e2k%,$(ARCH)))
 	@$(call add_feature)
 	@$(call try,BASE_BOOTLOADER,grub)
 	@$(call xport,BASE_BOOTLOADER)
@@ -18,6 +19,7 @@ ifeq (distro,$(IMAGE_CLASS))
 	@$(call add,BASE_PACKAGES,installer-feature-bootloader-$$(BASE_BOOTLOADER))
 	@$(call add,PINNED_PACKAGES,alterator-$$(BASE_BOOTLOADER))
 	@$(call add,PINNED_PACKAGES,installer-bootloader-$$(BASE_BOOTLOADER)-stage2)
+endif
 endif
 
 ifeq (,$(filter-out i586 x86_64,$(ARCH)))
@@ -39,8 +41,10 @@ endif
 use/bootloader/uboot: use/bootloader use/uboot
 	@$(call set,BASE_BOOTLOADER,uboot)
 
-use/bootloader/live: use/bootloader
+use/bootloader/live: use/bootloader; @:
+ifneq (,$(filter-out e2k%,$(ARCH)))
 	@$(call add,LIVE_PACKAGES,alterator-$$(BASE_BOOTLOADER))
+endif
 
 use/bootloader/os-prober: use/bootloader
 	@$(call add,BASE_PACKAGES,os-prober)
