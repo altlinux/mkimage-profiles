73ec7f0e9a1eeca79fc8f5cd368d9c8b93ae6c19
Anton Midyukov <antohami@altlinux.org>
2020-08-21 20:23:48 +0700

    oem: Added rootfs-installer-features
    Run installer features during install via rootfs.

--- features.in/oem/config.mk
+++ features.in/oem/config.mk
@@ -2,6 +2,7 @@ use/oem: use/services use/branding
 	@$(call add_feature)
 	@$(call add,DEFAULT_SERVICES_ENABLE,messagebus alteratord)
 	@$(call add,THE_PACKAGES,alterator-setup alterator-notes)
+	@$(call add,THE_PACKAGES,rootfs-installer-features)
 	@$(call add,THE_BRANDING,alterator notes)
 	@$(call try,OEM_TARGET,setup)
 	@$(call add,DEFAULT_SERVICES_ENABLE,$$(OEM_TARGET))
