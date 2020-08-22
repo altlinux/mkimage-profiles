5285c0a8327ef2171836ec3f22c435b5de1b022f
Michael Shigorin <mike@altlinux.org>
2020-08-22 10:24:56 +0300

    basealt.mk: generalize distro/alt-workstation
    The point is that it's common between the rest of the arches,
    no need to keep making my old mistake of growing configuration
    trees based on very different "roots" (.e2k-installer this time
    which was good for early experiments but not for production).
    
    Also get rid of long-unused distro/alt-workstation-?01 targets,
    unified kernel made those obsolete.
    
    And last but not least: *drop* that setup-libgl-dri3-disable,
    makes Radeon RX lag (half-recent AMD GPUs seem to lack 2D part
    rather altogether, it's emulated through 3D).

--- conf.d/basealt.mk
+++ conf.d/basealt.mk
@@ -1,10 +1,12 @@
 ifeq (distro,$(IMAGE_CLASS))
 
-ifeq (,$(filter-out i586 x86_64,$(ARCH)))
 distro/alt-workstation: workstation_groups_x86 = $(addprefix workstation/,\
 	3rdparty blender clamav cloud-clients freecad \
 	gtk-dictionary kvm smartcard voip-clients)
-endif
+
+distro/alt-workstation: workstation_groups_e2k = $(addprefix workstation/,\
+	z-autologin e801-1seat e801-2seat e801-3seat e801-6seat \
+	e101-mga2 e101-modesetting)
 
 distro/alt-workstation: distro/.base +vmguest +wireless +efi \
 	mixin/desktop-installer mixin/alt-workstation \
@@ -23,37 +25,26 @@ ifeq (,$(filter-out x86_64,$(ARCH)))
 	@$(call add,MAIN_GROUPS,workstation/virtualbox)
 	@$(call add,BASE_KMODULES,kvm virtualbox)
 endif
+
+ifeq (,$(filter-out e2k%,$(ARCH)))
+	@$(call add,THE_PACKAGES,python-module-serial)
+	@$(call add,THE_PACKAGES,setup-mate-nocomposite)
+	@$(call add,THE_PACKAGES,xorg-drv-smi)	# use/x11/smi
+ifeq (,$(filter-out e2k,$(ARCH)))
+	@$(call add,THE_PACKAGES,setup-alsa-elbrus-401)	# use/e2k/sound/401
+	@$(call set,META_VOL_ID,ALT Workstation for Elbrus 401-PC)
+endif	# e2k
+ifeq (,$(filter-out e2kv4,$(ARCH)))
+	@$(call set,META_VOL_ID,ALT Workstation for Elbrus 801/101-PC)
+	@$(call add,INSTALL2_PACKAGES,installer-feature-e2k-801-multiseat)
+	@$(call add,MAIN_GROUPS,$(workstation_groups_e2k))
+	@$(call add,THE_PACKAGES,test-audio)
+endif	# e2kv4
+endif	# e2k%
 	@$(call add,RESCUE_BOOTARGS,nomodeset vga=0)
 	@$(call add,EFI_BOOTARGS,lang=ru_RU)
 
-ifeq (,$(filter-out e2k%,$(ARCH)))
-distro/.alt-workstation-base: distro/.e2k-installer mixin/alt-workstation
-	@$(call add,THE_PACKAGES,setup-mate-terminal)
-	@$(call add,THE_PACKAGES,setup-mate-nocomposite)
-	@$(call add,THE_PACKAGES,setup-libgl-dri3-disable)
-	@$(call add,THE_PACKAGES,setup-pulseaudio-plain-module-detect)
-	@$(call add,THE_PACKAGES,firmware-linux)
-	@$(call add,THE_BRANDING,graphics)
-	@$(call set,META_APP_ID,ALT Workstation/$(ARCH))
-
-distro/.alt-workstation: distro/.alt-workstation-base use/x11/lightdm/gtk; @:
-
-distro/alt-workstation-101: distro/.alt-workstation use/e2k/101
-	@$(call set,META_VOL_ID,ALT Workstation 101)
-	@$(call add,MAIN_GROUPS,workstation/e101-modesetting)
-	@$(call add,MAIN_GROUPS,workstation/e101-mga2)
-
-distro/alt-workstation-401: distro/.alt-workstation use/e2k/401; @:
-	@$(call set,META_VOL_ID,ALT Workstation 401)
-
-# avoid lightdm; NB: mate-settings pulls it in (hence the dummy)
-distro/alt-workstation-801: distro/.alt-workstation-base use/e2k/install2/801
-	@$(call set,META_VOL_ID,ALT Workstation 801)
-	@$(call add,MAIN_GROUPS,workstation/e801-1seat workstation/e801-2seat)
-	@$(call add,INSTALL2_PACKAGES,installer-feature-e2k-801-dualseat)
-endif
-
-endif
+endif	# distro
 
 ifeq (vm,$(IMAGE_CLASS))
 ifeq (,$(filter-out aarch64 armh,$(ARCH)))
