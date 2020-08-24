33436b6b1bf144c1912197c78d7ca53bc3589342
Michael Shigorin <mike@altlinux.org>
2020-08-24 12:19:07 +0300

    alt-server.mk: generalize distro/alt-server
    This commit includes summary of the work done within RELENG-alt-server
    branch, and goes over the existing bits regarding architecture support.
    
    It was somewhat hard to keep separate so I finally chose to lump these
    parts together.

--- conf.d/alt-server.mk
+++ conf.d/alt-server.mk
@@ -1,38 +1,47 @@
 ifeq (distro,$(IMAGE_CLASS))
 
 ifeq (,$(filter-out i586 x86_64 ppc64le aarch64,$(ARCH)))
-distro/alt-server: server_groups_x86 = $(addprefix centaurus/,\
-        emulators freenx-server \
-	ipmi netinst sogo 80-desktop mate office pidgin vlc xorg)
+distro/alt-server: server_groups_primary = $(addprefix centaurus/,\
+        ipmi netinst sogo)
 
 ifeq (,$(filter-out x86_64 ppc64le aarch64,$(ARCH)))
-distro/alt-server: server_groups_x86_64 = $(addprefix centaurus/,\
-       freeipa-server v12n-server)
-distro/alt-server: use/efi/refind use/memtest +efi
+distro/alt-server: server_groups_virtipa = $(addprefix centaurus/,\
+	v12n-server freeipa-server)
 endif
 endif
 
-ifeq (,$(filter-out ppc64le aarch64,$(ARCH)))
-distro/.alt-server-vnc: use/install2/vnc/listen; @:
-else
-distro/.alt-server-vnc: ; @:
+ifeq (,$(filter-out i586 x86_64 ppc64le aarch64 e2k%,$(ARCH)))
+distro/alt-server: server_groups_desktop = $(addprefix centaurus/,\
+        80-desktop emulators freenx-server mate office pidgin vlc xorg)
 endif
 
-ifneq (,$(filter-out e2k%,$(ARCH)))
+ifeq (,$(filter-out x86_64 i586,$(ARCH)))
+distro/alt-server:: use/memtest
+endif
+
+ifeq (,$(filter-out x86_64 aarch64,$(ARCH)))
+distro/alt-server:: use/efi/refind +efi
+endif
+
+ifeq (,$(filter-out ppc64le aarch64 e2k%,$(ARCH)))
+distro/alt-server:: use/install2/vnc/listen; @:
+endif
 
 # FIXME: generalize vm-profile
-distro/alt-server: distro/.base distro/.alt-server-vnc \
-	mixin/alt-server use/vmguest/base \
+distro/alt-server:: distro/.base mixin/alt-server use/vmguest/base \
 	use/bootloader/grub use/rescue/base use/stage2/kms\
 	use/stage2/fs use/stage2/hid use/stage2/md \
 	use/stage2/mmc use/stage2/net use/stage2/net-nfs \
 	use/stage2/rtc use/stage2/sbc use/stage2/scsi use/stage2/usb \
 	use/docs/license
-	@$(call add,MAIN_GROUPS,$(server_groups_x86))
-	@$(call add,MAIN_GROUPS,$(server_groups_x86_64))
+	@$(call add,MAIN_GROUPS,$(server_groups_primary))
+	@$(call add,MAIN_GROUPS,$(server_groups_virtipa))
+	@$(call add,MAIN_GROUPS,$(server_groups_desktop))
 	@$(call add,MAIN_LISTS,centaurus/disk-dvd)
 	@$(call add,MAIN_LISTS,centaurus/disk-server-light)
+ifneq (,$(filter-out e2k%,$(ARCH)))
 	@$(call add,INSTALL2_PACKAGES,installer-feature-desktop-suspend-stage2)
+endif
 ifeq (,$(filter-out i586 x86_64,$(ARCH)))
 	@$(call add,INSTALL2_PACKAGES,installer-feature-setup-plymouth)
 endif
@@ -43,12 +52,6 @@ endif
 	@$(call add,CLEANUP_BASE_PACKAGES,acpid-events-power)
 	@$(call add,RESCUE_BOOTARGS,nomodeset vga=0)
 	@$(call add,EFI_BOOTARGS,lang=ru_RU)
-distro/alt-server: monitoring = $(addprefix server-v/, 90-monitoring \
-	zabbix-agent telegraf prometheus-node_exporter monit collectd nagios-nrpe)
-
-else
-
-distro/alt-server: distro/.e2k-installer mixin/alt-server
 ifeq (,$(filter-out e2k,$(ARCH)))
 	@$(call set,META_APP_ID,ALT Server for Elbrus-4.4)
 endif
@@ -56,6 +59,7 @@ ifeq (,$(filter-out e2kv4,$(ARCH)))
 	@$(call set,META_APP_ID,ALT Server for Elbrus-80x)
 endif
 
-endif # e2k
+distro/alt-server:: monitoring = $(addprefix server-v/, 90-monitoring \
+	zabbix-agent telegraf prometheus-node_exporter monit collectd nagios-nrpe)
 
 endif
