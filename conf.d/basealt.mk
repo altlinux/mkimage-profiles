4d3f64818d4a2af8584f03637bc1474c4a4e012e
Michael Shigorin <mike@altlinux.org>
2020-08-25 22:01:44 +0300

    basealt.mk: generalize e2k machine support
    Finalize the move to shared multiseat support on e2k
    following education.mk; should have been done properly
    in the first place but I couldn't manage that for 9.0,
    let's at least fix this now.

--- conf.d/basealt.mk
+++ conf.d/basealt.mk
@@ -4,10 +4,6 @@ distro/alt-workstation: workstation_groups_x86 = $(addprefix workstation/,\
 	3rdparty blender clamav cloud-clients freecad \
 	gtk-dictionary kvm smartcard voip-clients)
 
-distro/alt-workstation: workstation_groups_e2k = $(addprefix workstation/,\
-	z-autologin e801-1seat e801-2seat e801-3seat e801-6seat \
-	e101-mga2 e101-modesetting)
-
 distro/alt-workstation: distro/.base +vmguest +wireless +efi \
 	mixin/desktop-installer mixin/alt-workstation \
 	mixin/alt-workstation-install \
@@ -17,7 +13,8 @@ distro/alt-workstation: distro/.base +vmguest +wireless +efi \
         use/live/install use/live/x11 use/live/repo use/live/rw \
         use/vmguest/kvm/x11 use/stage2/kms \
 	use/branding/complete use/docs/license \
-        use/domain-client/full use/x11/amdgpu use/x11/lightdm/gtk
+        use/domain-client/full use/x11/amdgpu use/x11/lightdm/gtk \
+	use/e2k/multiseat/801/full use/e2k/x11/101 use/e2k/sound/401
 ifeq (,$(filter-out i586 x86_64,$(ARCH)))
 	@$(call add,MAIN_GROUPS,$(workstation_groups_x86))
 endif
@@ -25,7 +22,6 @@ ifeq (,$(filter-out x86_64,$(ARCH)))
 	@$(call add,MAIN_GROUPS,workstation/virtualbox)
 	@$(call add,BASE_KMODULES,kvm virtualbox)
 endif
-
 ifeq (,$(filter-out e2k%,$(ARCH)))
 	@$(call add,THE_PACKAGES,python-module-serial)
 	@$(call add,THE_PACKAGES,setup-mate-nocomposite)
@@ -33,15 +29,10 @@ ifeq (,$(filter-out e2k%,$(ARCH)))
 	@$(call add,MAIN_GROUPS,workstation/alterator-web)
 	@$(call add,SERVICES_ENABLE,ahttpd)	# in case it gets installed
 ifeq (,$(filter-out e2k,$(ARCH)))
-	@$(call add,THE_PACKAGES,setup-alsa-elbrus-401)	# use/e2k/sound/401
 	@$(call set,META_VOL_ID,ALT Workstation for Elbrus 401-PC)
 endif	# e2k
 ifeq (,$(filter-out e2kv4,$(ARCH)))
 	@$(call set,META_VOL_ID,ALT Workstation for Elbrus 801/101-PC)
-	@$(call add,INSTALL2_PACKAGES,installer-feature-e2k-801-multiseat)
-	@$(call add,MAIN_GROUPS,$(workstation_groups_e2k))
-	@$(call add,MAIN_GROUPS,workstation/90-e801 workstation/91-e101)
-	@$(call add,THE_PACKAGES,test-audio)
 endif	# e2kv4
 endif	# e2k%
 	@$(call add,RESCUE_BOOTARGS,nomodeset vga=0)
