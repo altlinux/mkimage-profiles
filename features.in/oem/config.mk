b91fe5a29fead15619055a8ccf70a6be83afd852
Anton Midyukov <antohami@altlinux.org>
2020-10-01 21:01:29 +0700

    oem: Added use/oem/no-cleanup
    This target disable cleanup install packages after finishing
    alterator-setup.

--- features.in/oem/config.mk
+++ features.in/oem/config.mk
@@ -7,9 +7,13 @@ use/oem: use/services use/branding
 	@$(call try,OEM_TARGET,setup)
 	@$(call add,DEFAULT_SERVICES_ENABLE,$$(OEM_TARGET))
 	@$(call xport,OEM_TARGET)
+	@$(call xport,OEM_NO_CLEANUP)
 
 use/oem/vnc: use/oem use/x11-vnc use/net-eth/dhcp
 	@$(call add,THE_PACKAGES,alterator-setup-x11vnc)
 	@$(call set,OEM_TARGET,setup-vnc)
 	@$(call set,NMCTL,no)
 	@$(call xport,NMCTL)
+
+use/oem/no-cleanup: use/oem
+	@$(call set,OEM_NO_CLEANUP,yes)
