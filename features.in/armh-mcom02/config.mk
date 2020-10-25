b9b345554ed4b91353a5b1c31e919f4f78c6cec0
Anton Midyukov <antohami@altlinux.org>
2020-10-25 19:58:00 +0700

    armh-mcom02: Added resize root partition at first start

--- features.in/armh-mcom02/config.mk
+++ features.in/armh-mcom02/config.mk
@@ -5,6 +5,7 @@ use/armh-mcom02: use/bootloader/uboot use/tty/S0 use/no-sleep
 	@$(call set,KFLAVOURS,mcom02)
 	@$(call add,THE_PACKAGES,u-boot-mcom02-firmware-tools u-boot-mcom02)
 	@$(call add,THE_PACKAGES,extlinux-fdtdir-cleanup-filetrigger)
+	@$(call add,THE_PACKAGES,rpi4-resize-rootpart)
 
 use/armh-mcom02/x11: use/armh-mcom02
 	@$(call add,THE_PACKAGES,xorg-drv-fbturbo)
