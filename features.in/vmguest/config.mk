cb31cc4ebd5fb490fafac03ca581c5baf1ae9d44
Anton Midyukov <antohami@altlinux.org>
2020-10-25 23:16:27 +0700

    vmguest: Fix and simplified for all architectures

--- features.in/vmguest/config.mk
+++ features.in/vmguest/config.mk
@@ -1,9 +1,15 @@
 # various VM guest modules/tools
-ifeq (,$(filter-out i586 x86_64 aarch64 armh ppc64le,$(ARCH)))
++vmguest: use/vmguest/complete; @:
 
 use/vmguest:
 	@$(call add_feature)
 
+use/vmguest/bare: use/vmguest/vbox use/vmguest/kvm; @:
+use/vmguest/base: use/vmguest/bare use/vmguest/vmware; @:
+use/vmguest/complete: use/vmguest/base \
+	use/vmguest/vbox/x11 use/vmguest/vmware/x11 use/vmguest/kvm/x11; @:
+
+ifeq (,$(filter-out i586 x86_64 aarch64 armh ppc64le,$(ARCH)))
 # NB: only reasonable for X11-bearing images
 # see also use/install2/kvm
 use/vmguest/kvm: use/vmguest
@@ -11,14 +17,11 @@ use/vmguest/kvm: use/vmguest
 
 use/vmguest/kvm/x11: use/vmguest/kvm
 	@$(call add,THE_PACKAGES,spice-vdagent xorg-drv-qxl)
+else
+use/vmguest/kvm use/vmguest/kvm/x11: ; @:
+endif
 
 ifeq (,$(filter-out i586 x86_64,$(ARCH)))
-
-use/vmguest/bare: use/vmguest/vbox use/vmguest/kvm; @:
-use/vmguest/base: use/vmguest/bare use/vmguest/vmware; @:
-use/vmguest/complete: use/vmguest/base \
-	use/vmguest/vbox/x11 use/vmguest/vmware/x11 use/vmguest/kvm/x11; @:
-
 use/vmguest/vbox: use/vmguest
 	@$(call add,THE_KMODULES,virtualbox-addition virtualbox-addition-guest)
 
@@ -35,30 +38,7 @@ use/vmguest/vmware:
 use/vmguest/vmware/x11: use/vmguest/vmware
 	@$(call add,THE_PACKAGES,xorg-drv-vmware xorg-drv-vmmouse)
 	@$(call add,THE_PACKAGES,open-vm-tools-desktop)
-
 else
-
-# non-x86
-use/vmguest/bare: use/vmguest/kvm; @:
-use/vmguest/base: use/vmguest/bare; @:
-use/vmguest/complete: use/vmguest/base use/vmguest/kvm/x11; @:
-
-use/vmguest/base: use/vmguest/kvm
+use/vmguest/vbox use/vmguest/vbox/x11 \
+use/vmguest/vmware use/vmguest/vmware/x11: ; @:
 endif
-
-else
-
-# kvm-unsupported guest arch
-use/vmguest: ; @:
-use/vmguest/bare: ; @:
-use/vmguest/base: ; @:
-use/vmguest/complete: ; @:
-
-ifeq (,$(filter-out e2k%,$(ARCH)))
-# no kvm before e2kv6 either
-use/vmguest/kvm use/vmguest/kvm/x11: ; @:
-endif
-
-endif
-
-+vmguest: use/vmguest/complete; @:
