3f2e559b77310d35a16183da51b723a5aa1c7a38
Anton Midyukov <antohami@altlinux.org>
2020-10-26 14:39:59 +0700

    fonts: Fix typo (logical error)

--- features.in/fonts/config.mk
+++ features.in/fonts/config.mk
@@ -9,11 +9,11 @@ use/fonts:
 # standalone target to specify non-bitmap installer fonts
 use/fonts/install2:
 	@$(call try,INSTALL2_FONTS,fonts-ttf-google-croscore-arimo)
-	@$(call add,INSTALL2_PACKAGES,$$(SYSTEM_FONTS))
+	@$(call add,INSTALL2_PACKAGES,$$(INSTALL2_FONTS))
 
 use/fonts/system:
 	@$(call try,SYSTEM_FONTS,fonts-ttf-google-croscore-arimo)
-	@$(call add,SYSTEM_PACKAGES,$$(INSTALL2_FONTS))
+	@$(call add,SYSTEM_PACKAGES,$$(SYSTEM_FONTS))
 
 # just stating that kernels and font habits are pretty individual
 use/fonts/zerg: use/fonts
