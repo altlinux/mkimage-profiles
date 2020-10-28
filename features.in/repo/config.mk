d1c947de31227ce27c6aaff49e62c160462b7c31
Anton Midyukov <antohami@altlinux.org>
2020-10-29 00:02:00 +0700

    repo: use/repo/main for vm targets
    use/repo/main for vm targets will now create and enable the main
    repository in /ALTLinux on rootfs.

--- features.in/repo/config.mk
+++ features.in/repo/config.mk
@@ -1,8 +1,15 @@
 use/repo:
 	@$(call add_feature)
 	@$(call add,STAGE1_PACKAGES,gnupg)
+	@$(call xport,ROOTFS_MAIN)
 
-use/repo/main: sub/main use/repo; @:
+use/repo/main:: sub/main use/repo; @:
+
+ifeq (vm,$(IMAGE_CLASS))
+use/repo/main::
+	@$(call set,ROOTFS_MAIN,1)
+	@$(call add,THE_PACKAGES,apt-repo-tools)
+endif
 
 use/repo/addon: use/repo/main
 	@$(call set,MAIN_SUFFIX,addon)
