bdef2e48b078f32020cff69e9e3b05c938dc0b69
Anton Midyukov <antohami@altlinux.org>
2020-11-01 23:22:47 +0700

    wireless: Update kernel modules for wi-fi

--- features.in/wireless/config.mk
+++ features.in/wireless/config.mk
@@ -2,7 +2,9 @@
 
 use/wireless:
 	@$(call add_feature)
-	@$(call add,THE_KMODULES,bcmwl rt3070 rtl8192 rtl8723de rtl8821ce rtl8812au staging)
+	@$(call add,THE_KMODULES,bcmwl staging)
+	@$(call add,THE_KMODULES,rtl8188fu rtl8192eu rtl8723de rtl8812au)
+	@$(call add,THE_KMODULES,rtl8821ce rtl8821cu rtl88x2bu rtl8723bu)
 	@$(call add,THE_LISTS,tools/wireless)
 
 use/wireless/full: use/wireless use/kernel/wireless; @:
