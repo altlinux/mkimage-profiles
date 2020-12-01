f1de2584c2b2408144c05f7a916dd007370253ec
Anton Midyukov <antohami@altlinux.org>
2020-12-01 23:13:50 +0700

    armh-skit: auto-resize root patition enable

--- features.in/armh-skit/config.mk
+++ features.in/armh-skit/config.mk
@@ -1,5 +1,5 @@
 ifeq (,$(filter-out armh,$(ARCH)))
-use/armh-skit: use/uboot use/tty/S0 use/no-sleep
+use/armh-skit: use/uboot use/tty/S0 use/no-sleep use/auto-resize
 	@$(call add_feature)
 	@$(call set,KFLAVOURS,skit)
 	@$(call add,THE_PACKAGES,extlinux-fdtdir-cleanup-filetrigger)
