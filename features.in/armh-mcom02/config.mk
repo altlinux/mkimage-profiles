dd0a009edde3a89849c35d17f4c5f8b700220a04
Anton Midyukov <antohami@altlinux.org>
2020-12-01 20:14:23 +0700

    armh-mcom02: Fix auto-resize root patition

--- features.in/armh-mcom02/config.mk
+++ features.in/armh-mcom02/config.mk
@@ -1,11 +1,10 @@
 
 ifeq (,$(filter-out armh,$(ARCH)))
-use/armh-mcom02: use/uboot use/tty/S0 use/no-sleep
+use/armh-mcom02: use/uboot use/tty/S0 use/no-sleep use/auto-resize
 	@$(call add_feature)
 	@$(call set,KFLAVOURS,mcom02)
 	@$(call add,THE_PACKAGES,u-boot-mcom02-firmware-tools u-boot-mcom02)
 	@$(call add,THE_PACKAGES,extlinux-fdtdir-cleanup-filetrigger)
-	@$(call add,THE_PACKAGES,rpi4-resize-rootpart)
 
 use/armh-mcom02/x11: use/armh-mcom02
 	@$(call add,THE_PACKAGES,xorg-drv-fbturbo)
