9db2390d31619700642f5346d73301e864574a67
Ivan A. Melnikov <iv@altlinux.org>
2021-02-03 15:56:34 +0400

    mipsel-bfk3: Switch to 5.4 kernel
    There, DTB is named differently.

--- features.in/mipsel-bfk3/config.mk
+++ features.in/mipsel-bfk3/config.mk
@@ -1,7 +1,7 @@
 use/mipsel-bfk3: use/kernel use/tty/S0 use/net-eth/dhcp/timeout/20
 	@$(call add_feature)
 	@$(call set,KFLAVOURS,bfk3-def)
-	@$(call set,DTB_NAME,baikal_bfk2)
+	@$(call set,DTB_NAME,baikal/baikal_bfk3.dtb)
 	@$(call xport,DTB_NAME)
 	@$(call set,VM_FSTYPE,ext2)
 	@$(call add,NET_ETH,eth0:dhcp)
