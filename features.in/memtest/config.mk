1e2a05d9bc05d6f3b0ee3c6d3ea6036e821e16ac
Anton Midyukov <antohami@altlinux.org>
2021-02-10 00:17:33 +0700

    grub, memtest: Add memtest for grub-pc

--- features.in/memtest/config.mk
+++ features.in/memtest/config.mk
@@ -1,8 +1,9 @@
 ifeq (,$(filter-out i586 x86_64,$(ARCH)))
-use/memtest: use/syslinux
+use/memtest: use/syslinux use/grub
 	@$(call add_feature)
 	@$(call add,SYSTEM_PACKAGES,memtest86+)
 	@$(call add,SYSLINUX_CFG,memtest)
+	@$(call add,GRUB_CFG,memtest_bios)
 else
 use/memtest: ; @:
 endif
