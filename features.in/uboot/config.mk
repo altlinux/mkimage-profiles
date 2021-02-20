cb6d5e5449de66008b0d31d56d5b51ca25611bfd
Anton Midyukov <antohami@altlinux.org>
2021-02-20 17:48:05 +0700

    uboot: Not add 'cma=192M' to UBOOT_BOOTARGS
    Booting to Raspberry Pi 4 is not possible with these parameter.

--- features.in/uboot/config.mk
+++ features.in/uboot/config.mk
@@ -14,9 +14,6 @@ ifneq (,$(filter-out i586 x86_64,$(ARCH)))
 use/uboot: use/kernel/initrd-setup $(UBOOT_TTY)
 	@$(call add_feature)
 	@$(call add,THE_LISTS,singleboard-tools)
-ifeq (,$(filter-out aarch64 armh,$(ARCH)))
-	@$(call add,UBOOT_BOOTARGS,cma=192M)
-endif
 ifeq (,$(filter-out riscv64,$(ARCH)))
 	@$(call add,UBOOT_BOOTARGS,earlyprintk debug no_alt_virt_keyboard)
 endif
