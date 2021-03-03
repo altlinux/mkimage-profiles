a4db4bf624fd76c7856784da429a484cf4580a12
Anton Midyukov <antohami@altlinux.org>
2021-03-04 00:54:33 +0700

    realtime.mk: Refactoring, drop live with session support
    live with session support needed efi=runtime for cmdline kernel.
    This parameter makes it unsuitable for real-time.

--- conf.d/realtime.mk
+++ conf.d/realtime.mk
@@ -1,20 +1,17 @@
-mixin/cnc-rt: use/cleanup +nm-gtk
+mixin/cnc-rt: use/l10n +nm-gtk +systemd +systemd-optimal +plymouth \
+	mixin/regular-lxde use/x11/lightdm/gtk
 	@$(call set,KFLAVOURS,rt)
 	@$(call add,THE_LISTS,realtime/tests)
 	@$(call add,THE_LISTS,engineering/cnc)
 	@$(call add,THE_LISTS,engineering/misc)
-	@$(call add,CLEANUP_PACKAGES,virtualbox-guest-common)
-	@$(call add,CLEANUP_PACKAGES,open-vm-tools)
-	@$(call add,CLEANUP_PACKAGES,xorg-drv-vboxvideo xorg-drv-qxl)
-	@$(call add,CLEANUP_PACKAGES,spice-vdagent qemu-guest-agent)
-	@$(call add,CLEANUP_PACKAGES,xorg-drv-vmware xorg-drv-vmmouse open-vm-tools-desktop)
 
 ifeq (distro,$(IMAGE_CLASS))
-distro/cnc-rt: distro/regular-lxde mixin/cnc-rt
+distro/cnc-rt: distro/.regular-x11 mixin/cnc-rt \
+	use/live/install
 	@$(call add,EFI_BOOTARGS,efi=runtime)
 endif
 
 ifeq (vm,$(IMAGE_CLASS))
-vm/cnc-rt: vm/regular-lxde mixin/vm-archdep mixin/cnc-rt; @:
+vm/cnc-rt: vm/systemd mixin/regular-vm-x11 mixin/vm-archdep mixin/cnc-rt; @:
 vm/cnc-rt-efi: vm/cnc-rt +efi; @:
 endif
