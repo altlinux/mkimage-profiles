fd1e972ceb457e3b139723023ab6428ed6166e1c
Anton Midyukov <antohami@altlinux.org>
2021-03-10 02:33:12 +0700

    armh-mcom02: Don't overwrite extlinux.conf, set screen resolution 1366x768
    - No need to do double work.
    - With screen resolution '1920x1080' there are errors getting EDID when
    using fbturbo videodriver. There is a cyclical shift of the screen
    image to the right.

--- features.in/armh-mcom02/config.mk
+++ features.in/armh-mcom02/config.mk
@@ -8,6 +8,7 @@ use/armh-mcom02: use/uboot use/tty/S0 use/no-sleep use/auto-resize
 
 use/armh-mcom02/x11: use/armh-mcom02
 	@$(call add,THE_PACKAGES,xorg-drv-fbturbo)
+	@$(call add,UBOOT_BOOTARGS,video=HDMI:1366x768)
 
 use/armh-mcom02/mali: use/armh-mcom02/x11
 	@$(call add,THE_KMODULES,mali)
