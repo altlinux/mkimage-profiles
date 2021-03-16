724d904cae49e73987665d958acaa67cfcbb0a39
Anton Midyukov <antohami@altlinux.org>
2021-03-16 20:59:11 +0700

    grub: restrict graphics mode to architectures i586, x86_64, aarch64
    Not needed for other architectures.
    See-also: use/branding/full

--- features.in/grub/config.mk
+++ features.in/grub/config.mk
@@ -13,10 +13,12 @@ endif
 # UI is overwritten
 use/grub/ui/%: use/grub
 	@$(call set,GRUB_UI,$*)
+ifeq (,$(filter-out i586 x86_64 aarch64,$(ARCH)))
 	@if [ "$*" == gfxboot ]; then \
 		$(call add,STAGE1_BRANDING,bootloader); \
 		$(call add,STAGE1_PACKAGES,grub-common); \
 	fi
+endif
 
 use/grub/%.cfg: use/grub
 	@$(call add,GRUB_CFG,$*)
