0f8e976fdce6a1b8ab9f0dd90c268fdd7691c326
Dmitriy Terekhin <jqt4@altlinux.org>
2021-03-25 20:46:42 +0400

    uuid-iso: create UUID for ISO image
    By default, when building an image, xorriso generates
    an image UUID of the form YYYY-MM-DD-hh-mm-ss-cc
    from the current time.
    If the xorriso command line has a parameter
    -volume_date uuid YYYYMMDDhhmmsscc
    then the UUID of the image is generated from it.
    This feature reads the current time and creates variables:
    UUID_ISO containing YYYY-MM-DD-hh-mm-ss-cc
    UUID_ISO_SHRT containing YYYYMMDDhhmmsscc
    Also add 60-cdrom_id.rules to initrd
    This allows to identify CD/DVD by UUID
    and use to boot the installer
    method:disk,uuid:YYYY-MM-DD-hh-mm-ss-cc

---
+++ features.in/uuid-iso/config.mk
@@ -0,0 +1,8 @@
+UUID_ISO = $(shell date -u +%Y-%m-%d-%H-%M-%S-00)
+UUID_ISO_SHRT = $(shell echo $(UUID_ISO) | sed 's/-//g')
+
+use/uuid-iso:
+	@$(call add_feature)
+	@$(call set,UUID_ISO_SHRT,$(UUID_ISO_SHRT))
+	@$(call set,UUID_ISO,$(UUID_ISO))
+	@$(call xport,UUID_ISO)
