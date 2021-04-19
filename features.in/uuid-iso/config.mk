f3d8ad8f54926403e2896301377d073abfa4bcf9
Dmitriy Terekhin <jqt4@altlinux.org>
2021-04-19 17:53:11 +0400

    uuid-iso: prevent "lazy evaluation" of time for UUID

--- features.in/uuid-iso/config.mk
+++ features.in/uuid-iso/config.mk
@@ -1,6 +1,6 @@
 ifeq (,$(filter-out i586 x86_64 aarch64,$(ARCH)))
-UUID_ISO = $(shell date -u +%Y-%m-%d-%H-%M-%S-00)
-UUID_ISO_SHRT = $(shell echo $(UUID_ISO) | sed 's/-//g')
+UUID_ISO := $(shell date -u +%Y-%m-%d-%H-%M-%S-00)
+UUID_ISO_SHRT := $(shell echo $(UUID_ISO) | sed 's/-//g')
 
 use/uuid-iso:
 	@$(call add_feature)
