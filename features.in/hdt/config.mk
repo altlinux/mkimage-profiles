2b6c37afcd8e7827078f53a5610a906dfbc91cc9
Anton Midyukov <antohami@altlinux.org>
2021-04-22 18:44:52 +0700

    grub, hdt: Add hdt support for grub-pc (memdisk + floppy image)
    Note: syslinux --install needed /proc, so:
    @$(call set,GLOBAL_HSH_PROC,1)
    
    See-also: mkimage/doc/README.ru

--- features.in/hdt/config.mk
+++ features.in/hdt/config.mk
@@ -1,9 +1,12 @@
 # optimized out use/syslinux due to use/memtest
 ifeq (,$(filter-out i586 x86_64,$(ARCH)))
-use/hdt: use/memtest
+use/hdt: use/syslinux use/memtest
 	@$(call add_feature)
 	@$(call add,STAGE1_PACKAGES,pciids)
+	@$(call add,STAGE1_PACKAGES,dosfstools)
 	@$(call add,SYSLINUX_MODULES,hdt)
+	@$(call add,GRUB_CFG,hdt_bios)
+	@$(call set,GLOBAL_HSH_PROC,1)
 else
 use/hdt: ; @:
 endif
