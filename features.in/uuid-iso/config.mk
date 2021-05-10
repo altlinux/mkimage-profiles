de18e39e61e539f9eddeb0594b4cd39e284aadd6
Anton Midyukov <antohami@altlinux.org>
2021-05-10 22:35:38 +0700

    uuid-iso: Allowed for all platform with grub support
    Since we are now using fuuid, it doesn't depend on mkimage version.
    But it depends on the propagator version.
    Needed propagator >= 20210329-alt1
    See-also: commit 52e90c264dc1b5d0aa300d7cfb669c934fc4a3c6

--- features.in/uuid-iso/config.mk
+++ features.in/uuid-iso/config.mk
@@ -1,14 +1,9 @@
-ifeq (,$(filter-out i586 x86_64 aarch64,$(ARCH)))
 UUID_ISO := $(shell date -u +%Y-%m-%d-%H-%M-%S-00)
 UUID_ISO_SHRT := $(shell echo $(UUID_ISO) | sed 's/-//g')
 
 use/uuid-iso:
 	@$(call add_feature)
-	@$(call set,MKI_VER_MINIMAL,0.2.41)
+	@$(call set,MKI_VER_OPTIMAL,0.2.41)
 	@$(call set,UUID_ISO_SHRT,$(UUID_ISO_SHRT))
 	@$(call set,UUID_ISO,$(UUID_ISO))
 	@$(call xport,UUID_ISO)
-
-else
-use/uuid-iso: ; @:
-endif
