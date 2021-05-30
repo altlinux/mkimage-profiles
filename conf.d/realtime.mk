f3c0d17726c47fde78447c398285e4cc5baf9be4
Anton Midyukov <antohami@altlinux.org>
2021-05-30 22:49:00 +0700

    realtime.mk: rename to regulars config

--- conf.d/realtime.mk
+++ conf.d/realtime.mk
@@ -1,17 +1,17 @@
 mixin/cnc-rt: use/l10n +nm-gtk +systemd +systemd-optimal +plymouth \
-	mixin/regular-lxde use/x11/lightdm/gtk
+	mixin/regular-lxde use/x11/lightdm/gtk $(STARTERKIT)
 	@$(call set,KFLAVOURS,rt)
 	@$(call add,THE_LISTS,realtime/tests)
 	@$(call add,THE_LISTS,engineering/cnc)
 	@$(call add,THE_LISTS,engineering/misc)
 
 ifeq (distro,$(IMAGE_CLASS))
-distro/cnc-rt: distro/.regular-x11 mixin/cnc-rt \
+distro/regular-cnc-rt: distro/.regular-x11 mixin/cnc-rt \
 	use/live/install
 	@$(call add,EFI_BOOTARGS,efi=runtime)
 endif
 
 ifeq (vm,$(IMAGE_CLASS))
-vm/cnc-rt: vm/systemd mixin/regular-vm-x11 mixin/vm-archdep mixin/cnc-rt; @:
-vm/cnc-rt-efi: vm/cnc-rt +efi; @:
+vm/regular-cnc-rt: vm/systemd mixin/regular-vm-x11 mixin/vm-archdep mixin/cnc-rt; @:
+vm/regular-cnc-rt-efi: vm/cnc-rt +efi; @:
 endif
