e7c889e7ac19f3800aa51e7a22653e34aa3129ac
Mikhail Efremov <sem@altlinux.org>
2021-06-24 15:57:24 +0300

    workstation: Add Samba group policy managing tools
    From server profile.
    And dropped alterator-gpupdate from the.pkgs list:
    it in the new admc list now.

--- conf.d/mixin-basealt.mk
+++ conf.d/mixin-basealt.mk
@@ -4,7 +4,7 @@ mixin/alt-workstation-install: workstation_groups = $(addprefix workstation/,\
 	10-office 20-networking 30-multimedia 40-virtualization 50-publishing \
 	agents emulators ganttproject gnome-peer-to-peer graphics-editing \
 	libreoffice mate-usershare pidgin raccess scanning scribus \
-	sound-editing thunderbird vlc freeipa-client)
+	sound-editing thunderbird vlc freeipa-client admc)
 
 mixin/alt-workstation: +systemd +systemd-optimal +pulse +nm +power \
 	use/kernel/latest \
