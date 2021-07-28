af7ece34a3ee159d71a32352c67061ae556588c1
Anton Midyukov <antohami@altlinux.org>
2021-07-28 18:26:31 +0700

    arm-rpi4: not use specyphic features for RPi4 in use/arm-rpi4
    use/arm-rpi4 is now used to improve Raspberry Pi support in
    universal rootfs with u-boot.

--- features.in/arm-rpi4/config.mk
+++ features.in/arm-rpi4/config.mk
@@ -10,18 +10,18 @@ endif
 	@$(call add,BASE_KMODULES,staging)
 	@$(call add,THE_LISTS,uboot)
 	@$(call add,THE_PACKAGES,u-boot-tools)
-	@$(call add,THE_PACKAGES,rpi4-boot-switch)
-	@$(call add,THE_PACKAGES,rpi4-boot-nouboot-filetrigger)
-	@$(call add,THE_PACKAGES,rpi4-boot-uboot-filetrigger)
 	@$(call add,THE_PACKAGES,firmware-bcm4345)
-	@$(call add,THE_PACKAGES,pi-bluetooth)
-	@$(call add,DEFAULT_SERVICES_DISABLE,systemd-networkd-wait-online)
 
 use/arm-rpi4/x11: use/arm-rpi4
+	@$(call add,THE_PACKAGES,pi-bluetooth)
 	@$(call add,DEFAULT_SERVICES_ENABLE,bluetoothd hciuart)
+	@$(call add,DEFAULT_SERVICES_DISABLE,systemd-networkd-wait-online)
 
 use/arm-rpi4/kernel: use/arm-rpi4
 	@$(call set,RPI_NOUBOOT,yes)
+	@$(call add,THE_PACKAGES,rpi4-boot-switch)
+	@$(call add,THE_PACKAGES,rpi4-boot-nouboot-filetrigger)
+	@$(call add,THE_PACKAGES,rpi4-boot-uboot-filetrigger)
 	@$(call xport,RPI_NOUBOOT)
 ifeq (aarch64,$(ARCH))
 	@$(call set,KFLAVOURS,rpi-un rpi-def)
