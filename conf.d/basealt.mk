491b07e398a0272506df52ce38681f8f33d0222d
Michael Shigorin <mike@altlinux.org>
2021-08-06 16:59:00 +0300

    basealt.mk: add alterator-secsetup on e2k too
    It's duplicating commit e8694a6df4fc48b5f8fa5f5e9b387647b3ee1fca
    in sem@'s tree -- I don't pull workstation/extras into e2k images
    due to too many of those being simply unavailable for now; still
    let the black@'s wish for this AltHa control knob be honoured.

--- conf.d/basealt.mk
+++ conf.d/basealt.mk
@@ -32,6 +32,7 @@ ifeq (,$(filter-out e2k%,$(ARCH)))
 	@$(call add,THE_PACKAGES,setup-mate-nocomposite)
 	@$(call add,THE_PACKAGES,xorg-drv-smi)	# use/x11/smi
 	@$(call add,THE_PACKAGES,flashrom)
+	@$(call add,MAIN_PACKAGES,alterator-secsetup)
 	@$(call add,MAIN_GROUPS,workstation/alterator-web)
 	@$(call add,SERVICES_ENABLE,ahttpd)	# in case it gets installed
 ifeq (,$(filter-out e2k,$(ARCH)))
