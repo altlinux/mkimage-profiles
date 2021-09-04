24217832d32ad4006641fc520894361986dd04f2
Anton Midyukov <antohami@altlinux.org>
2021-09-04 20:17:11 +0700

    initrd-bootchain: disable bc_debug

--- features.in/initrd-bootchain/config.mk
+++ features.in/initrd-bootchain/config.mk
@@ -2,6 +2,6 @@ use/initrd-bootchain: use/uuid-iso
 	@$(call add_feature)
 	@$(call set,STAGE1_INITRD,initrd-pipeline)
 	@$(call set,STAGE1_PACKAGES,make-initrd-bootchain)
-	@$(call set,STAGE1_INITRD_TYPEARGS,$(shell echo "root=bootchain bootchain=fg,altboot bc_debug automatic"))
+	@$(call set,STAGE1_INITRD_TYPEARGS,$(shell echo "root=bootchain bootchain=fg,altboot automatic"))
 	@$(call set,STAGE1_INITRD_BOOTMETHOD,$(shell echo "method:cdrom,uuid:$(UUID_ISO)"))
 	@$(call set,STAGE1_INITRD_STAGE2_OPTION,stagename)
