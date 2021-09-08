63df7d7f844cece2bfc748191a4937849ae341f2
Anton Midyukov <antohami@altlinux.org>
2021-09-08 13:00:07 +0700

    vmguest: fix build with use/vmguest/kvm/x11 for BRANCH=p9

--- features.in/vmguest/config.mk
+++ features.in/vmguest/config.mk
@@ -17,7 +17,9 @@ use/vmguest/kvm: use/vmguest
 
 use/vmguest/kvm/x11: use/vmguest/kvm
 	@$(call add,THE_PACKAGES,spice-vdagent xorg-drv-qxl xorg-drv-spiceqxl)
+ifneq (p9,$(BRANCH))
 	@$(call add,THE_PACKAGES,xorg-dri-virtio)
+endif
 else
 use/vmguest/kvm use/vmguest/kvm/x11: ; @:
 endif
