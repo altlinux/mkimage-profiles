c1b349cf30aeed961928e4b99c06a9915cc5e68c
Anton Midyukov <antohami@altlinux.org>
2021-09-09 21:18:07 +0700

    initrd-bootchain: allow overriding variables in bootchain config

--- features.in/initrd-bootchain/generate.mk
+++ features.in/initrd-bootchain/generate.mk
@@ -1,4 +1,4 @@
-# set up livecd browser redirection page
+# set up initrd-bootchain config
 
 ifdef BUILDDIR
 
@@ -7,16 +7,28 @@ include $(BUILDDIR)/distcfg.mk
 BOOTCHAIN_CFG := $(BUILDDIR)/stage1/files/bootchain
 
 all: debug
-	@if [ -n "$(META_VOL_ID)" ]; then \
+	@[ -s "$(BOOTCHAIN_CFG)" ] || exit 1; \
+	if [ -n "$(META_VOL_ID)" ]; then \
 		DISTRO="$(META_VOL_ID)"; \
 	else \
 		DISTRO="$(RELNAME)"; \
 	fi; \
-	if [ -s "$(BOOTCHAIN_CFG)" ]; then \
-		sed -i \
-			-e "s,@distro@,$$DISTRO," \
-			$(BOOTCHAIN_CFG); \
-	fi
+	sed -i -e "s,@distro@,$$DISTRO," \
+		-e "s,@bc_fgvt_activate@,$(BOOTCHAIN_BC_FGVT_ACTIVATE)," \
+		-e "s,@waitdev_timeout@,$(BOOTCHAIN_WAITDEV_TIMEOUT)," \
+		-e "s,@oem_welcome_text@,$(BOOTCHAIN_OEM_WELCOME_TEXT)," \
+		-e "s,@oem_cdroot@,$(BOOTCHAIN_OEM_CDROOT)," \
+		-e "s,@oem_default_stage2@,$(BOOTCHAIN_OEM_DEFAULT_STAGE2)," \
+		-e "s,@oem_live_storage@,$(BOOTCHAIN_OEM_LIVE_STORAGE)," \
+		-e "s,@oem_bad_storage@,$(BOOTCHAIN_OEM_BAD_STORAGE)," \
+		-e "s,@oem_setup_storage@,$(BOOTCHAIN_OEM_SETUP_STORAGE)," \
+		-e "s,@oem_images_base@,$(BOOTCHAIN_OEM_IMAGES_BASE)," \
+		-e "s,@oem_overlays_dir@,$(BOOTCHAIN_OEM_OVERLAYS_DIR)," \
+		-e "s,@oem_url_netinst@,$(BOOTCHAIN_OEM_URL_NETINST)," \
+		-e "s,@oem_srv_netinst@,$(BOOTCHAIN_OEM_SRV_NETINST)," \
+		-e "s,@oem_nfs_netinst@,$(BOOTCHAIN_OEM_NFS_NETINST)," \
+		-e "s,@oem_cifs_netinst@,$(BOOTCHAIN_OEM_CIFS_NETINST)," \
+		$(BOOTCHAIN_CFG)
 
 debug:
 	@if [ -n "$(DEBUG)" ]; then \
