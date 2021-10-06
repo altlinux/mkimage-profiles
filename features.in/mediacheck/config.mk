9c4bf34eb8720bd6465a84acfbc94b50c3397b0b
Anton Midyukov <antohami@altlinux.org>
2021-10-06 20:39:13 +0700

    mediacheck: add missing use/grub/mediacheck.cfg target

--- features.in/mediacheck/config.mk
+++ features.in/mediacheck/config.mk
@@ -1,5 +1,6 @@
 ifeq (distro,$(IMAGE_CLASS))
-use/mediacheck: use/stage2 sub/stage2@mediacheck use/syslinux/mediacheck.cfg
+use/mediacheck: use/stage2 sub/stage2@mediacheck \
+	use/grub/mediacheck.cfg use/syslinux/mediacheck.cfg
 	@$(call add_feature)
 	@$(call add,POSTPROCESS_TARGETS,90mediacheck)
 else
