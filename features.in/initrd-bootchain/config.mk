8fdafec8b7c20517385b7f6a5b6c9c9239c7fbf2
Anton Midyukov <antohami@altlinux.org>
2021-10-14 09:02:26 +0700

    initrd-bootchain: fix typo
    This error from initial version in which the feature has been
    named as a pipeline.

--- features.in/initrd-bootchain/config.mk
+++ features.in/initrd-bootchain/config.mk
@@ -1,6 +1,6 @@
 use/initrd-bootchain: use/uuid-iso
 	@$(call add_feature)
-	@$(call set,STAGE1_INITRD,initrd-pipeline)
+	@$(call set,STAGE1_INITRD,initrd-bootchain)
 	@$(call set,STAGE1_PACKAGES,make-initrd-bootchain)
 	@$(call set,STAGE1_INITRD_TYPEARGS,$(shell echo "root=bootchain bootchain=fg,altboot automatic"))
 	@$(call set,STAGE1_INITRD_BOOTMETHOD,$(shell echo "method:cdrom,uuid:$(UUID_ISO)"))
