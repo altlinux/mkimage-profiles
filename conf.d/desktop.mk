49e92b36babba76738242a7deb72ad20a35c0be5
Anton Midyukov <antohami@altlinux.org>
2021-12-10 02:44:57 +0700

    desktop.mk: hide broken targets

--- conf.d/desktop.mk
+++ conf.d/desktop.mk
@@ -8,7 +8,7 @@ distro/.desktop-mini: distro/.desktop-base use/x11/xdm; @:
 
 distro/.desktop-network: distro/.desktop-mini mixin/desktop-installer; @:
 
-distro/icewm: distro/.desktop-network use/lowmem use/install2/fs +icewm; @:
-distro/ltsp-icewm: distro/icewm +ltsp; @:
+#distro/icewm: distro/.desktop-network use/lowmem use/install2/fs +icewm; @:
+#distro/ltsp-icewm: distro/icewm +ltsp; @:
 
 endif
