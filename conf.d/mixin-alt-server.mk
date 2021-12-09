e1aae5d44497df6effc01aec6032127172098a12
Anton V. Boyarshinov <boyarsh@altlinux.org>
2021-12-09 10:54:00 +0300

    mixin-alt-server.mk, centaurus: add a group of pve-backup-server

--- conf.d/mixin-alt-server.mk
+++ conf.d/mixin-alt-server.mk
@@ -18,6 +18,7 @@ mixin/alt-server: +installer +systemd \
 ifeq (,$(filter-out i586 x86_64 aarch64,$(ARCH)))
 	@$(call add,THE_BRANDING,bootloader bootsplash)
 	@$(call set,KFLAVOURS,std-def)
+	@$(call add,MAIN_GROUPS,centaurus/pve-backup-server)
 endif
 	@$(call add,BASE_LISTS,centaurus/base)
 	@$(call add,BASE_LISTS,centaurus/base-server)
