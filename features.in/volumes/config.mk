6abea584bca25c33d7672ab4186871e6d247c5a5
Anton Midyukov <antohami@altlinux.org>
2021-12-23 22:34:45 +0700

    volumes: make a feature available only for distro targets

--- features.in/volumes/config.mk
+++ features.in/volumes/config.mk
@@ -1,3 +1,4 @@
+ifeq (distro,$(IMAGE_CLASS))
 use/volumes:
 	@$(call add_feature)
 	@$(call add,INSTALL2_PACKAGES,$$(STAGE2_VOLUMES_PROFILE))
@@ -5,3 +6,8 @@ use/volumes:
 
 use/volumes/%: use/volumes
 	@$(call set,STAGE2_VOLUMES_PROFILE,volumes-profile-$*)
+
+else
+use/volumes: ; @:
+use/volumes/%: ; @:
+endif
