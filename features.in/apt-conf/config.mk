7844b5bc5c4dc54b95243d5fced24c0454e91dd8
Anton Midyukov <antohami@altlinux.org>
2021-12-27 23:56:28 +0700

    apt-conf: replace IMAGE_INIT_LIST to PINNED_PACKAGES
    No need to add packages to IMAGE_INIT_LIST. Use pkgpriorities.
    See also commit 8551e71028f639601376fb779f5ff63218f3d3b0

--- features.in/apt-conf/config.mk
+++ features.in/apt-conf/config.mk
@@ -1,7 +1,7 @@
 use/apt-conf:
 	@$(call add_feature)
 	@$(call add,THE_PACKAGES,apt-conf-$$(THE_APT_CONF))
-	@$(call add,IMAGE_INIT_LIST,apt-conf-$$(THE_APT_CONF))
+	@$(call add,PINNED_PACKAGES,apt-conf-$$(THE_APT_CONF))
 	@$(call try,THE_APT_CONF,sisyphus)
 
 use/apt-conf/branch: use/apt-conf
