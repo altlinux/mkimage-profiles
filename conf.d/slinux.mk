a64d6493f081feb45ad402017400343a0a6b05f7
Anton Midyukov <antohami@altlinux.org>
2022-01-21 17:55:04 +0700

    slinux.mk: enable online repo for live

--- conf.d/slinux.mk
+++ conf.d/slinux.mk
@@ -2,12 +2,12 @@
 
 ifeq (distro,$(IMAGE_CLASS))
 
-distro/slinux-live: distro/.livecd-install use/slinux/base
+distro/slinux-live: distro/.livecd-install use/slinux/base use/live/repo
 	@$(call add,THE_LISTS,slinux/live-install)
 	@$(call set,META_VOL_ID,Simply Linux live $(DISTRO_VERSION) $(ARCH))
 	@$(call set,META_APP_ID,Simply Linux live $(DISTRO_VERSION) $(ARCH) $(shell date +%F))
 
-distro/slinux: distro/.installer use/slinux/full use/rescue/base
+distro/slinux: distro/.installer use/slinux/full use/rescue/base use/live/repo
 	@$(call set,INSTALLER,simply-linux)
 	@$(call add,MAIN_GROUPS,slinux/emulators-full slinux/games-base slinux/games slinux/games-full slinux/graphics-base slinux/graphics slinux/graphics-full-inkscape slinux/multimedia-full-audacity slinux/multimedia-base slinux/multimedia-full-cheese slinux/multimedia slinux/multimedia-full-shotcut slinux/multimedia-full-sound-juicer slinux/multimedia-full-obs-studio slinux/net-base)
 ifneq (,$(filter-out e2k%,$(ARCH)))
