3dfe3f932814833f91705fa804c354697674cff0
Anton Midyukov <antohami@altlinux.org>
2022-02-02 15:27:09 +0700

    repo: rename variable ROOTFS_MAIN to REPO_MAIN
    This variable blocks the enable of online repositories and activates
    the enable of RPMS.main repository in iso or rootfs.
    The old name was misleading.

--- features.in/repo/config.mk
+++ features.in/repo/config.mk
@@ -1,15 +1,15 @@
 use/repo:
 	@$(call add_feature)
 	@$(call add,STAGE1_PACKAGES,gnupg)
-	@$(call try,REPO,rsync/alt)
-	@$(call xport,ROOTFS_MAIN)
+	@$(call try,REPO,http/alt)
+	@$(call xport,REPO_MAIN)
 	@$(call xport,REPO)
 
 use/repo/main:: sub/main use/repo; @:
 
 ifeq (vm,$(IMAGE_CLASS))
 use/repo/main::
-	@$(call set,ROOTFS_MAIN,1)
+	@$(call set,REPO_MAIN,1)
 	@$(call add,THE_PACKAGES,apt-repo-tools)
 endif
 
