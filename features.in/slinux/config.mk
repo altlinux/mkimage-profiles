eb0c51f6efc26d0b6c4d4c176a3ea931b7840922
Anton Midyukov <antohami@altlinux.org>
2022-02-24 21:21:21 +0700

    slinux: add use/stage2/ata

--- features.in/slinux/config.mk
+++ features.in/slinux/config.mk
@@ -97,7 +97,7 @@ endif
 use/slinux/base: use/isohybrid use/luks \
 	+plymouth use/memtest +vmguest \
 	+efi \
-	use/stage2/fs use/stage2/hid use/stage2/md \
+	use/stage2/ata use/stage2/fs use/stage2/hid use/stage2/md \
 	use/stage2/mmc use/stage2/net use/stage2/net-nfs use/stage2/cifs \
 	use/stage2/rtc use/stage2/sbc use/stage2/scsi use/stage2/usb \
 	use/live/x11 use/live/rw use/install2/fonts \
