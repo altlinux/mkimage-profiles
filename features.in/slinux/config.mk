f488e3ecae3f7717fbe86847ba0a8b4074360e27
Mikhail Efremov <sem@altlinux.org>
2022-03-22 13:52:17 +0300

    slinux: Use un-def kernel
    For newest hardware support.
    This reverts commit 3f1b521acaf97293e875cafe47b6977db4947189.

--- features.in/slinux/config.mk
+++ features.in/slinux/config.mk
@@ -94,7 +94,7 @@ ifeq (,$(filter-out e2k%,$(ARCH)))
 	@$(call add,THE_LISTS,$(call tags,xscreensaver && (base || desktop)))
 endif
 ifeq (,$(filter-out armh aarch64 i586 x86_64,$(ARCH)))
-	@$(call set,KFLAVOURS,std-def)
+	@$(call set,KFLAVOURS,un-def)
 endif
 
 use/slinux/base: use/isohybrid use/luks \
