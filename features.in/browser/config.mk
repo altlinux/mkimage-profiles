f10ff87d2236ea786f937ebac01a537675cd3b7f
Anton Midyukov <antohami@altlinux.org>
2022-06-28 15:06:25 +0700

    browser: chromium available for x86_64, aarch64 only

--- features.in/browser/config.mk
+++ features.in/browser/config.mk
@@ -4,13 +4,13 @@ use/browser:
 	@$(call try,THE_BROWSER,elinks)	# X11-less fallback
 
 # amend as neccessary; firefox is treated separately due to its flavours
-BROWSERS_i586 = chromium seamonkey netsurf epiphany falkon otter-browser \
+BROWSERS_i586 = seamonkey netsurf epiphany falkon otter-browser \
 		elinks links2
-BROWSERS_x86_64 := $(BROWSERS_i586)
+BROWSERS_x86_64 := $(BROWSERS_i586) chromium
 BROWSERS_ppc64el = netsurf epiphany falkon otter-browser elinks links2
 BROWSERS_aarch64 = chromium netsurf epiphany falkon otter-browser elinks links2
 BROWSERS_armh = netsurf epiphany falkon otter-browser elinks links2
-BROWSERS_mipsel = chromium seamonkey netsurf epiphany falkon otter-browser \
+BROWSERS_mipsel = seamonkey netsurf epiphany falkon otter-browser \
 		  elinks links2
 BROWSERS_riscv64 = epiphany midori netsurf elinks
 BROWSERS_e2k = netsurf elinks links2
@@ -20,7 +20,7 @@ BROWSERS := $(BROWSERS_$(ARCH))
 $(addprefix use/browser/,$(BROWSERS)): use/browser/%: use/browser
 	@$(call set,THE_BROWSER,$*)
 
-ifneq (,$(filter-out x86_64 i586 aarch64 mipsel,$(ARCH)))
+ifneq (,$(filter-out x86_64 aarch64,$(ARCH)))
 use/browser/chromium: use/browser/firefox use/browser/firefox/esr; @:
 endif
 
