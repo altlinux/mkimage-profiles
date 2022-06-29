a8352aca3c6ec5f1045ac684df68be8528f7dda5
Anton Midyukov <antohami@altlinux.org>
2022-06-29 22:32:36 +0700

    init: add startup to use/init
    startup contains configs needed all.
    e.g. /etc/sysconfig/i18n needed for /root/.i18n from package rootfiles-alt.
    See also https://bugzilla.altlinux.org/43107

--- features.in/init/config.mk
+++ features.in/init/config.mk
@@ -7,6 +7,7 @@ use/init: use/pkgpriorities
 	@$(call add_feature)
 	@$(call add,THE_LISTS,$$(INIT_TYPE))
 	@$(call add,PINNED_PACKAGES,$$(INIT_TYPE))
+	@$(call add,THE_PACKAGES,startup) # contains configs needed all
 
 # THE_LISTS is too late when BASE_PACKAGES have pulled in
 # the wrong syslogd-daemon provider already
