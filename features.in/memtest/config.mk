2731db39a4d8a1023a14083207f9a5ded8321f1a
Anton Midyukov <antohami@altlinux.org>
2022-10-25 21:44:12 +0700

    memtest, grub: add EFI support
    Note: memtest86 is not signed.

--- features.in/memtest/config.mk
+++ features.in/memtest/config.mk
@@ -3,7 +3,7 @@ use/memtest: use/syslinux use/grub
 	@$(call add_feature)
 	@$(call add,SYSTEM_PACKAGES,memtest86+)
 	@$(call add,SYSLINUX_CFG,memtest)
-	@$(call add,GRUB_CFG,memtest_bios)
+	@$(call add,GRUB_CFG,memtest)
 else
 use/memtest: ; @:
 endif
