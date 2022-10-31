438a9b77ab77e9587e6cadda5cfc226f06718755
Anton Midyukov <antohami@altlinux.org>
2022-10-31 22:54:32 +0700

    live.mk: add use/memtest to distro/grub

--- conf.d/live.mk
+++ conf.d/live.mk
@@ -11,7 +11,7 @@ distro/syslinux: distro/.boot \
 endif
 
 ifeq (,$(filter-out i586 x86_64 aarch64 ppc64le riscv64,$(ARCH)))
-distro/grub: distro/.boot use/grub use/hdt +efi \
+distro/grub: distro/.boot use/grub use/hdt use/memtest +efi \
 	use/grub/localboot_bios.cfg use/grub/sdab_bios.cfg; @:
 ifeq (,$(filter-out i586 x86_64,$(ARCH)))
 	@$(call set,BOOTLOADER,grubpcboot)
