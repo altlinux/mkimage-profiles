59b1ee06ebc85d5a2b6a41f0c10077c0c9b6fcff
Anton Midyukov <antohami@altlinux.org>
2022-11-25 21:20:53 +0700

    Add STDOUT option to output log to stdout

--- lib/log.mk
+++ lib/log.mk
@@ -24,8 +24,13 @@ GLOBAL_VERBOSE ?= $(DEBUG)
 ifeq (2,$(DEBUG))
 SHELL += -x
 endif
+ifeq (1,$(STDOUT))
+LOG = 2>&1 | tee -a $(BUILDLOG)
+LOG_STDERR = $(LOG)
+else
 LOG = >>$(BUILDLOG) 2>&1
 LOG_STDERR = 2> >(tee -a $(BUILDLOG) >&2) 1>>$(BUILDLOG)
+endif
 else
 MAKE += -s
 LOG = 2>>$(BUILDLOG) >/dev/null
