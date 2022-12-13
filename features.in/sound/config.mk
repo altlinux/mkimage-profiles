959d58e6e594ec50aec4ee6cfc7829201806ceb5
Anton Midyukov <antohami@altlinux.org>
2022-12-14 01:07:51 +0700

    sound: use wireplumber instead pipewire-media-session for pipewire in Sisyphus
    Request by aris@

--- features.in/sound/config.mk
+++ features.in/sound/config.mk
@@ -19,3 +19,9 @@ use/sound/pulse: use/sound
 use/sound/pipewire: use/sound
 	@$(call set,THE_SOUND,sound/pipewire)
 	@$(call add,DEFAULT_SYSTEMD_USER_SERVICES_ENABLE,pipewire pipewire-pulse pipewire-media-session)
+ifeq (sisyphus,$(BRANCH))
+	@$(call add,THE_PACKAGES,wireplumber)
+	@$(call add,DEFAULT_SYSTEMD_USER_SERVICES_ENABLE,wireplumber)
+else
+	@$(call add,DEFAULT_SYSTEMD_USER_SERVICES_ENABLE,pipewire-media-session)
+endif
