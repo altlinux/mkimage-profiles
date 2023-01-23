a34099af0b8142d26618972a3ac2ab27f24b6ec2
Ivan A. Melnikov <iv@altlinux.org>
2023-01-23 14:18:32 +0400

    use/wireless: Add firmware-wireless-regdb
    It seems it's required most WiFi dirviers to work properly,
    so let's make sure it's present.

--- features.in/wireless/config.mk
+++ features.in/wireless/config.mk
@@ -2,6 +2,7 @@
 
 use/wireless: use/kernel/wireless
 	@$(call add_feature)
+	@$(call add,THE_PACKAGES,firmware-wireless-regdb)
 	@$(call add,THE_LISTS,tools/wireless)
 
 use/wireless/full: use/wireless; @:
