ec1f94809e31357f657664f6fd41df60f5d8c7f9
Anton Midyukov <antohami@altlinux.org>
2023-03-18 16:15:48 +0700

    features.in: drop armh-skit
    These SBC is not available for me.

--- features.in/armh-skit/config.mk
+++
@@ -1,8 +0,0 @@
-ifeq (,$(filter-out armh,$(ARCH)))
-use/armh-skit: use/uboot use/tty/S0 use/no-sleep use/auto-resize
-	@$(call add_feature)
-	@$(call set,KFLAVOURS,skit)
-	@$(call add,THE_PACKAGES,extlinux-fdtdir-cleanup-filetrigger)
-	@$(call add,THE_PACKAGES,xorg-drv-fbturbo)
-
-endif
