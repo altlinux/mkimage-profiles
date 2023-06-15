f58e9787839ded39d7e58de5a07b60f69c787e45
Anton Midyukov <antohami@altlinux.org>
2023-06-15 23:20:58 +0700

    phone.mk: do'nt use grub-efi on pinephone
    Until the issue of integrating the patch for adding devicetree to
    the grub config has not been resolved, we will use a working
    solution with extlinux.conf

--- conf.d/phone.mk
+++ conf.d/phone.mk
@@ -6,7 +6,7 @@ mixin/phosh: use/x11/gdm use/x11-autologin +pipewire +nm +nm-native
 	@$(call set,DEFAULT_SESSION,phosh)
 
 ifeq (vm,$(IMAGE_CLASS))
-vm/.phosh: vm/systemd use/efi/grub +systemd \
+vm/.phosh: vm/systemd +systemd \
 	mixin/regular-vm-base mixin/regular-x11 \
 	mixin/regular-desktop mixin/phosh use/deflogin/root \
 	use/deflogin/privileges use/deflogin/xgrp use/deflogin/hardware \
@@ -16,12 +16,13 @@ vm/.phosh: vm/systemd use/efi/grub +systemd \
 	@$(call set,LOCALE,ru_RU)
 	@$(call set,KFLAVOURS,pine)
 
-vm/qemu-phosh: vm/.phosh use/tty/S0 +vmguest; @:
+vm/qemu-phosh: vm/.phosh use/tty/S0 use/efi/grub +vmguest; @:
 endif
 
 ifeq (aarch64,$(ARCH))
 # TODO: devicetree ($root)/boot/dtb/rockchip/rk3399-pinephone-pro.dtb
-mixin/pinephone: use/x11/armsoc use/firmware use/uboot use/tty/S2; @:
+mixin/pinephone: use/x11/armsoc use/firmware use/bootloader/uboot use/tty/S2
+	@$(call set,EFI_BOOTLOADER,)
 
 ifeq (vm,$(IMAGE_CLASS))
 vm/pinephone-phosh: vm/.phosh mixin/pinephone; @:
