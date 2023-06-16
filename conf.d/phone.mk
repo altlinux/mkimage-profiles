9e7ae7f939973ae7063d29a78eb7767204eb99c0
Andrew Savchenko <bircoph@altlinux.org>
2023-06-16 10:49:17 +0300

    start eg25-manager by default

--- conf.d/phone.mk
+++ conf.d/phone.mk
@@ -1,9 +1,11 @@
-mixin/phosh: use/x11/gdm use/x11-autologin +pipewire +nm +nm-native
+mixin/phosh: use/x11/gdm use/x11-autologin +pipewire +nm +nm-native \
+	use/services
 	@$(call add,THE_PACKAGES,phosh mutter-gnome xorg-xwayland)
 	@$(call add,THE_PACKAGES,bluez eg25-manager)
 	@$(call add,THE_LISTS,mobile-apps)
 	@$(call add,DEFAULT_SERVICES_ENABLE,bluetoothd)
 	@$(call set,DEFAULT_SESSION,phosh)
+	@$(call add,DEFAULT_SYSTEMD_SERVICES_ENABLE,eg25-manager.service)
 
 ifeq (vm,$(IMAGE_CLASS))
 vm/.phosh: vm/systemd +systemd \
