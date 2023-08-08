1bff8abaffdafbe35c5b4ef055ea30fd7492193f
Anton Midyukov <antohami@altlinux.org>
2023-08-08 18:47:20 +0700

    mixin.mk: add bookmarks for file manager of cinnamon

--- conf.d/mixin.mk
+++ conf.d/mixin.mk
@@ -87,7 +87,8 @@ mixin/regular-gnustep: use/x11/gnustep use/mediacheck use/browser/seamonkey
 	@$(call add,THE_BRANDING,graphics)
 
 mixin/regular-cinnamon: use/x11/cinnamon use/x11/lightdm/slick +nm \
-	use/fonts/ttf/google use/im; @:
+	use/fonts/ttf/google use/im
+	@$(call add,THE_PACKAGES,xdg-user-dirs-gtk)
 
 mixin/regular-deepin: use/x11/deepin use/browser/chromium +nm; @:
 
