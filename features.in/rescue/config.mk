5ea983baf1a120675c3039fb84a2fb4a3bb7e18c
Anton Midyukov <antohami@altlinux.org>
2023-08-21 08:15:23 +0700

    rescue: add list with grub

--- features.in/rescue/config.mk
+++ features.in/rescue/config.mk
@@ -3,6 +3,7 @@ use/rescue/.base: use/stage2 sub/stage2@rescue
 	@$(call add,RESCUE_LISTS,sysvinit)
 	@$(call add,RESCUE_PACKAGES,startup startup-rescue udev)
 	@$(call add,RESCUE_LISTS,openssh)
+	@$(call add,RESCUE_LISTS,grub)
 
 use/rescue/base: use/rescue/.base
 	@$(call add,RESCUE_PACKAGES,pciutils nfs-utils os-prober)
@@ -12,13 +13,7 @@ use/rescue/base: use/rescue/.base
 use/rescue: use/rescue/.base use/services use/firmware/full +wireless
 	@$(call add,DEFAULT_SERVICES_DISABLE,rpcbind)
 ifeq (,$(filter-out i586 x86_64,$(ARCH)))
-	@$(call add,RESCUE_PACKAGES,grub2-pc lilo syslinux)
-endif
-ifeq (,$(filter-out ppc64le,$(ARCH)))
-	@$(call add,RESCUE_PACKAGES,grub-ieee1275)
-endif
-ifneq (,$(EFI_BOOTLOADER))
-	@$(call add,RESCUE_PACKAGES,grub2-efi)
+	@$(call add,RESCUE_PACKAGES,lilo syslinux)
 endif
 	@$(call add,RESCUE_LISTS,\
 		$(call tags,(base || extra || server || misc || fs) \
