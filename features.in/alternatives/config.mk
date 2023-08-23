27dce17f0682eeb798d0347a9e4e9fee23e759d8
Anton Midyukov <antohami@altlinux.org>
2023-08-23 11:57:04 +0700

    alternatives: add x-terminal-emulator

--- features.in/alternatives/config.mk
+++ features.in/alternatives/config.mk
@@ -4,3 +4,6 @@ use/alternatives:
 
 use/alternatives/xvt/%: use/alternatives
 	@$(call add,ALTERNATIVES,/usr/bin/xvt:/usr/bin/$*)
+
+use/alternatives/x-terminal-emulator/%: use/alternatives
+	@$(call add,ALTERNATIVES,/usr/bin/x-terminal-emulator:/usr/bin/$*)
