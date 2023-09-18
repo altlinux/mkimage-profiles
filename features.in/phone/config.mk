c3431f0b20b5b43d135908794cfc48449f5146a7
Anton Midyukov <antohami@altlinux.org>
2023-09-18 21:26:42 +0700

    phone: add subtarget ttyescape to access tty

--- features.in/phone/config.mk
+++ features.in/phone/config.mk
@@ -1,3 +1,8 @@
 use/phone:
 	@$(call add_feature)
 	@$(call add,THE_PAKAGES,wallpapers-mobile)
+
+# enables tty on the phone using a hotkey
+use/phone/ttyescape: use/phone use/services
+	@$(call add,THE_PACKAGES,hkdm ttyescape)
+	@$(call add,DEFAULT_SYSTEMD_SERVICES_ENABLE,hkdm)
