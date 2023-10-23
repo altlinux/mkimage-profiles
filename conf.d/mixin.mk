09d1f5332938f9cd530c6a968ebea94107e5803d
Anton Midyukov <antohami@altlinux.org>
2023-10-23 17:45:22 +0700

    mixin.mk: fix mixin/kde5 for loongarch64

--- conf.d/mixin.mk
+++ conf.d/mixin.mk
@@ -107,9 +107,9 @@ mixin/regular-kde5: use/x11/kde5 use/browser/falkon \
 	use/x11/kde5-display-manager-lightdm \
 	use/fonts/ttf/google use/fonts/ttf/redhat use/fonts/zerg \
 	+pulse
-	@$(call add,THE_PACKAGES,kde5-telepathy plasma5-xdg-desktop-portal-kde)
+	@$(call add,THE_PACKAGES,plasma5-xdg-desktop-portal-kde)
 	@$(call add,THE_PACKAGES,qt6-wayland)
-ifneq (,$(filter-out e2k%,$(ARCH)))
+ifneq (,$(filter-out e2k% riscv64 loongarch64,$(ARCH)))
 	@$(call add,THE_PACKAGES,falkon-kde5)
 endif
 
