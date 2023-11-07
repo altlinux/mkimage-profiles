90d9eee6185bae3d0b00c2944a3747cc4c467d6f
Anton Midyukov <antohami@altlinux.org>
2023-11-08 00:36:06 +0700

    mixin.mk: add plasma5-discover to regular-kde5

--- conf.d/mixin.mk
+++ conf.d/mixin.mk
@@ -109,6 +109,7 @@ mixin/regular-kde5: use/x11/kde5 use/browser/falkon \
 	use/fonts/ttf/google use/fonts/ttf/redhat use/fonts/zerg
 	@$(call add,THE_PACKAGES,plasma5-xdg-desktop-portal-kde)
 	@$(call add,THE_PACKAGES,qt6-wayland)
+	@$(call add,THE_PACKAGES,plasma5-discover)
 ifneq (,$(filter-out e2k% riscv64 loongarch64,$(ARCH)))
 	@$(call add,THE_PACKAGES,falkon-kde5)
 endif
