34e18698fc1eb3363aed87da903432cd9db4f9b5
Dmitriy Terekhin <jqt4@altlinux.org>
2023-11-16 20:17:35 +0400

    alt-server: Use un-def kernel by default
    Made at the request of Evgeny Sinelnikov <sin@altlinux.org>

--- conf.d/mixin-alt-server.mk
+++ conf.d/mixin-alt-server.mk
@@ -34,7 +34,7 @@ ifeq (,$(filter-out i586 x86_64,$(ARCH)))
 endif
 	@$(call add,THE_BRANDING,alterator)
 ifeq (,$(filter-out i586 x86_64 aarch64,$(ARCH)))
-	@$(call set,KFLAVOURS,un-def std-def)
+	@$(call set,KFLAVOURS,std-def un-def)
 	@$(call add,INSTALL2_PACKAGES,installer-feature-cleanup-kernel-stage3)
 	@$(call add,INSTALL2_PACKAGES,ntfs-3g)
 	@$(call add,BASE_KMODULES,drm)
