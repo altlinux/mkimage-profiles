6f85ede70f4472227414a1b71396ffcdfedfcf11
Anton Midyukov <antohami@altlinux.org>
2023-11-28 21:56:20 +0700

    mixin.mk: add +nm-gtk to cinnamon
    Сinnamon uses gtk plugins in its native interface for NetworkManager.

--- conf.d/mixin.mk
+++ conf.d/mixin.mk
@@ -85,7 +85,7 @@ mixin/regular-icewm: use/fonts/ttf/redhat +icewm +nm-gtk
 mixin/regular-gnustep: use/x11/gnustep use/mediacheck
 	@$(call add,THE_BRANDING,graphics)
 
-mixin/regular-cinnamon: use/x11/cinnamon use/x11/lightdm/slick +nm \
+mixin/regular-cinnamon: use/x11/cinnamon use/x11/lightdm/slick +nm-gtk \
 	use/fonts/ttf/google use/im
 	@$(call add,THE_PACKAGES,xdg-user-dirs-gtk)
 	@$(call add,THE_PACKAGES,gnome-disk-utility gnome-system-monitor)
