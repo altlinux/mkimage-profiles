76e69c4d3ac3de8580f303188dc2f6b5e218ea3c
Anton Midyukov <antohami@altlinux.org>
2023-12-24 00:34:00 +0700

    datetime: set TIME_ZONE, TIME_UTC via kernel cmdline for distro images
    Without an explicit definition of tz in the kernel cmdline, the time
    zone will be overridden based on other criteria.

--- features.in/datetime/config.mk
+++ features.in/datetime/config.mk
@@ -1,5 +1,12 @@
-use/datetime:
+use/datetime: ; @:
+ifeq (,$(filter-out ve vm,$(IMAGE_CLASS)))
 	@$(call add_feature)
 	@$(call add,THE_PACKAGES,alterator-datetime-functions)
 	@$(call xport,TIME_UTC)
 	@$(call xport,TIME_ZONE)
+endif
+ifeq (distro,$(IMAGE_CLASS))
+	@$(call add,LIVE_PACKAGES,livecd-timezone)
+	@$(call add,STAGE2_BOOTARGS,utc=$$(TIME_UTC))
+	@$(call add,STAGE2_BOOTARGS,tz=$$(TIME_ZONE))
+endif
