3812720100f38ffc678d282fcc8eefb4a1c28cf1
Anton Midyukov <antohami@altlinux.org>
2024-02-08 20:51:43 +0700

    mediacheck: show "Error: implantisomd5 is not available" on screen

--- features.in/mediacheck/config.mk
+++ features.in/mediacheck/config.mk
@@ -3,7 +3,7 @@ use/mediacheck: use/stage2 sub/stage2@mediacheck \
 	use/grub/mediacheck.cfg use/syslinux/mediacheck.cfg
 	@$(call add_feature)
 	@if ! command -v implantisomd5 >/dev/null 2>&1; then \
-	    echo "Error: implantisomd5 is not available! Need install isomd5sum"; exit 1; \
+	    echo "Error: implantisomd5 is not available! Need install isomd5sum" >&2; exit 1; \
 	fi
 	@$(call add,POSTPROCESS_TARGETS,90mediacheck)
 else
