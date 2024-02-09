482813989998157d61f2c2a8f5731c67960b2876
Anton Midyukov <antohami@altlinux.org>
2024-02-09 22:08:53 +0700

    server: replace php8.1 with php8.2

--- features.in/server/config.mk
+++ features.in/server/config.mk
@@ -51,10 +51,10 @@ use/server/groups/services: use/server
 	@$(call add,MAIN_GROUPS,server/dhcp server/dns server/mail)
 	@$(call add,MAIN_GROUPS,server/apache2 server/nginx)
 	@$(call add,MAIN_GROUPS,server/mariadb server/pgsql)
-	@$(call add,MAIN_GROUPS,server/php8.1)
+	@$(call add,MAIN_GROUPS,server/php8.2)
 	@$(call add,MAIN_GROUPS,server/ftp server/rsync)
 	@$(call add,MAIN_GROUPS,server/kvm)
 	@$(call add,DEFAULT_SERVICES_ENABLE,libvirtd)
-	@$(call add,DEFAULT_SERVICES_DISABLE,php8.0-fpm php8.1-fpm)
+	@$(call add,DEFAULT_SERVICES_DISABLE,php8.2-fpm)
 
 use/server/groups/base: use/server/groups/tools use/server/groups/services; @:
