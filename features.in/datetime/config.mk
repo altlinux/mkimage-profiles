d0e214e8ad2a4821c09504203c6e8b00f5a3a1a2
Anton Midyukov <antohami@altlinux.org>
2024-03-20 12:26:50 +0700

    datetime: preset utc and timezone for live also
    At the first boot, if there is no network, the wrong time is set,
    since the timezone and utc settings occur late.
    So at least with the default parameters there will be no problem.

--- features.in/datetime/config.mk
+++ features.in/datetime/config.mk
@@ -1,10 +1,8 @@
 use/datetime: ; @:
-ifeq (,$(filter-out ve vm,$(IMAGE_CLASS)))
 	@$(call add_feature)
 	@$(call add,THE_PACKAGES,alterator-datetime-functions)
 	@$(call xport,TIME_UTC)
 	@$(call xport,TIME_ZONE)
-endif
 ifeq (distro,$(IMAGE_CLASS))
 	@$(call add,LIVE_PACKAGES,livecd-timezone)
 	@$(call add,STAGE2_BOOTARGS,utc=$$(TIME_UTC))
