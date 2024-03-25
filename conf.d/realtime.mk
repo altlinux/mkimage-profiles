25e5bbbc163c7c6d474aa4344cb190c2f8ac07c1
Anton Midyukov <antohami@altlinux.org>
2024-03-25 22:52:38 +0700

    realtime.mk: add kernel headers via kernel-headers package list

--- conf.d/realtime.mk
+++ conf.d/realtime.mk
@@ -4,7 +4,7 @@ mixin/cnc-rt: use/l10n +nm-gtk +systemd +systemd-optimal \
 	@$(call add,THE_LISTS,realtime/tests)
 	@$(call add,THE_LISTS,engineering/cnc)
 	@$(call add,THE_LISTS,engineering/misc)
-	@$(call add,THE_PACKAGES,kernel-headers-rt)
+	@$(call add,THE_LISTS,kernel-headers)
 	@$(call add,THE_PACKAGES,gcc-c++ liblinuxcnc-devel)
 
 ifeq (distro,$(IMAGE_CLASS))
