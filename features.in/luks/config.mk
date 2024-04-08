6ed9d5a6ef9c49ae5d9516272fb225d09692d851
Anton Midyukov <antohami@altlinux.org>
2024-04-09 02:42:30 +0700

    luks: do not add alerator-luks to live
    With this we break luks support for the old use/live/install.

--- features.in/luks/config.mk
+++ features.in/luks/config.mk
@@ -3,6 +3,6 @@ LUKS_LISTS := $(call tags,security luks)
 use/luks:
 	@$(call add_feature)
 	@$(call add,BASE_PACKAGES,make-initrd-luks)
-	@$(call add,THE_PACKAGES,alterator-luks)
+	@$(call add,BASE_PACKAGES,alterator-luks)
 	@$(call add,THE_LISTS,$(LUKS_LISTS))
 	@$(call add,RESCUE_LISTS,$(LUKS_LISTS))
