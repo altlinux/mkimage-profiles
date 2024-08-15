6b61320c79659cfb78bc60fcd87a4a107f7a87ba
Anton Midyukov <antohami@altlinux.org>
2024-08-15 07:28:38 +0300

    luks: add use/luks/touchscreen, add alterator-luks to distro targets only
    alterator-luks used only in the installer.
    The new target leverages the new make-initrd feature luks-unl0kr,
    which allows entering the luks password on the touchscreen.
    Needed plymouth.

--- features.in/luks/config.mk
+++ features.in/luks/config.mk
@@ -3,6 +3,12 @@ LUKS_LISTS := $(call tags,security luks)
 use/luks:
 	@$(call add_feature)
 	@$(call add,BASE_PACKAGES,make-initrd-luks)
-	@$(call add,BASE_PACKAGES,alterator-luks)
 	@$(call add,THE_LISTS,$(LUKS_LISTS))
 	@$(call add,RESCUE_LISTS,$(LUKS_LISTS))
+ifeq (distro,$(IMAGE_CLASS))
+	@$(call add,BASE_PACKAGES,alterator-luks)
+endif
+
+use/luks/touchscreen: use/luks use/plymouth/base
+	@$(call add,BASE_PACKAGES,unl0kr)
+	@$(call add,VM_INITRDMODULES,drivers/input/touchscreen)
