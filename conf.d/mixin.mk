d58a8c93ca7676d4e12bd53912ce5dbd1b67c311
Anton Midyukov <antohami@altlinux.org>
2024-08-26 16:37:24 +0300

    mixin.mk: add xdg-desktop-portal-gtk to kde
    Needed for gtk3/gtk4 applications on wayland.

--- conf.d/mixin.mk
+++ conf.d/mixin.mk
@@ -162,6 +162,7 @@ else
 	@$(call add,THE_PACKAGES,plasma5-xdg-desktop-portal-kde)
 	@$(call add,THE_PACKAGES,plasma5-discover)
 endif
+	@$(call add,THE_PACKAGES,xdg-desktop-portal-gtk)
 	@$(call add,THE_PACKAGES,qt6-wayland)
 	@$(call add,THE_PACKAGES,accountsservice)
 	@$(call add,THE_PACKAGES,gtk-theme-breeze)
