59c2169950d693e061f568a723043b8d0d9ecce1
Anton Midyukov <antohami@altlinux.org>
2024-08-26 21:02:49 +0300

    mixin.mk: add kf5-kirigami to regular-kde on Sisyphus
    not all kde applications have migrated to kf6 yet and some of
    their functionality is not available because the package
    kf5-kirigami is not installed.

--- conf.d/mixin.mk
+++ conf.d/mixin.mk
@@ -158,6 +158,7 @@ mixin/regular-kde: use/x11/kde use/browser/falkon \
 ifeq (,$(filter-out sisyphus,$(BRANCH)))
 	@$(call add,THE_PACKAGES,xdg-desktop-portal-kde)
 	@$(call add,THE_PACKAGES,plasma-discover)
+	@$(call add,THE_PACKAGES,kf5-kirigami)
 else
 	@$(call add,THE_PACKAGES,plasma5-xdg-desktop-portal-kde)
 	@$(call add,THE_PACKAGES,plasma5-discover)
