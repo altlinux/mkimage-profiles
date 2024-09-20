6688059bfea813c91ebaf92b18826753e94dea36
Anton Midyukov <antohami@altlinux.org>
2024-09-20 17:00:10 +0300

    x11-autostart: replace installer-feature-runlevel5-stage{3,2}
    installer-feature-runlevel5-stage3 predepends on startup.
    This will dependencies on startup package on systems with systemd.

--- features.in/x11-autostart/config.mk
+++ features.in/x11-autostart/config.mk
@@ -1,5 +1,5 @@
 use/x11-autostart:
 	@$(call add_feature)
 ifeq (distro,$(IMAGE_CLASS))
-	@$(call add,BASE_PACKAGES,installer-feature-runlevel5-stage3)	###
+	@$(call add,BASE_PACKAGES,installer-feature-runlevel5-stage2)	###
 endif
