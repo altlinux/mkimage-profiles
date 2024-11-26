3d1e59d1bbb176c762c10b10ae09882bb6842727
Anton Midyukov <antohami@altlinux.org>
2024-11-26 08:51:30 +0300

    auto-resize: use make-initrd-kickstart

--- features.in/auto-resize/config.mk
+++ features.in/auto-resize/config.mk
@@ -1,3 +1,5 @@
 use/auto-resize:
 	@$(call add_feature)
-	@$(call add,THE_PACKAGES,rpi4-resize-rootpart)
+	@$(call add,BASE_BOOTARGS,ksfile=ks.cfg)
+	@$(call add,BASE_PACKAGES,make-initrd-kickstart)
+	@$(call add,VM_INITRDFEATURES,kickstart)
