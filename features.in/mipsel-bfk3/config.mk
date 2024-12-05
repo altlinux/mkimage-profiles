8cf154959264c8e2f509a34772260a2af2a8595d
Anton Midyukov <antohami@altlinux.org>
2024-12-05 20:48:59 +0300

    mipsel-bfk3: do'nt set NET_ETH
    This is not required for systemd-networkd now.
    And with NetworkManager it was not required before.

--- features.in/mipsel-bfk3/config.mk
+++ features.in/mipsel-bfk3/config.mk
@@ -4,9 +4,6 @@ use/mipsel-bfk3: use/kernel use/tty/S0 use/net-eth/dhcp/timeout/20
 	@$(call set,DTB_NAME,baikal/baikal_bfk3.dtb)
 	@$(call xport,DTB_NAME)
 	@$(call set,VM_FSTYPE,ext2)
-	@$(call add,NET_ETH,eth0:dhcp)
-	@$(call add,NET_ETH,eth1:dhcp)
-	@$(call add,NET_ETH,eth2:dhcp)
 	@$(call add,THE_PACKAGES,fbset-modes-sm750-bfk3)
 	@$(call add,THE_PACKAGES,alt-config-be-t)
 
