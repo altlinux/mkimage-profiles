55a2d7fbb5d3b1f00859b036adc9acc0fd456f17
Anton Midyukov <antohami@altlinux.org>
2024-12-11 09:04:40 +0300

    l10n: add variable GNOME_KB_SWITCH

--- features.in/l10n/config.mk
+++ features.in/l10n/config.mk
@@ -15,13 +15,16 @@ use/l10n:
 	@$(call xport,LOCALES)
 	@$(call xport,XKB_KEYMAPS)
 	@$(call xport,XKB_SWITCH)
+	@$(call xport,GNOME_KB_SWITCH)
 
 # see also alterator-sysconfig backend
 use/l10n/xkb/switch/ctrl_shift: use/l10n
 	@$(call set,XKB_SWITCH,grp:ctrl_shift_toggle)
+	@$(call set,GNOME_KB_SWITCH,<Ctrl>Shift_L)
 
 use/l10n/xkb/switch/alt_shift: use/l10n
 	@$(call set,XKB_SWITCH,grp:alt_shift_toggle)
+	@$(call set,GNOME_KB_SWITCH,<Shift>Alt_L)
 
 # FIXME: derive from locale by default if possible
 use/l10n/ru_RU: use/l10n/xkb/switch/alt_shift
