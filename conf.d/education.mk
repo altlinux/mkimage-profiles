3fbe65620a0b359f7f5158d3ab9f968d19725fdc
Anton Midyukov <antohami@altlinux.org>
2024-12-16 22:27:35 +0300

    education.mk: add use/live/repo to alt-education
    We duplicate education-live a little to make it full-fledged,
    but still not so big.

--- conf.d/education.mk
+++ conf.d/education.mk
@@ -176,7 +176,7 @@ distro/education: distro/alt-education; @:
 distro/alt-education:: distro/.base \
 	mixin/education \
 	mixin/education-installer \
-	+live \
+	+live use/live/repo \
 	use/live/rescue \
 	use/live-install/oem \
 	use/e2k/multiseat/full use/power/acpi \
