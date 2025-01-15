671dcfbc9efc01bacf771b7e78b6089e75f41118
Anton Midyukov <antohami@altlinux.org>
2025-01-15 14:46:26 +0300

    auto-resize: add f2fs-tools
    make-initrd-kickstart required f2fs-tools.

--- features.in/auto-resize/config.mk
+++ features.in/auto-resize/config.mk
@@ -2,4 +2,5 @@ use/auto-resize:
 	@$(call add_feature)
 	@$(call add,BASE_BOOTARGS,ksfile=ks.cfg)
 	@$(call add,BASE_PACKAGES,make-initrd-kickstart)
+	@$(call add,BASE_PACKAGES,f2fs-tools)
 	@$(call add,VM_INITRDFEATURES,kickstart)
