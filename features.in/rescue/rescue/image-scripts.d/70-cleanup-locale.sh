a7b7f34b07d5db81c0a2f5ca8b3d9db27569b82a
Anton Midyukov <antohami@altlinux.org>
2025-01-23 17:21:40 +0300

    rescue: do not cleanup locales
    1. The default locale is now C.UTF-8 (/root/.i18n)
    2. When connecting via ssh, the locale is the one in the system we are connecting from.
    
    That's why we need locales.

--- features.in/rescue/rescue/image-scripts.d/70-cleanup-locale.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# cleanup extra locales
-CLEANUP_DIRS="/usr/lib/locale /usr/share/locale"
-
-for CLEANUP_DIR in $CLEANUP_DIRS; do
-cd "$CLEANUP_DIR"
-rm -fr $(ls -1 |sed '/en_US/d' | tr -s '\r\n' ' ')
-done
