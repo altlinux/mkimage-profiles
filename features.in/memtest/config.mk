8eadcfe42f909107d6c5032e008ea1ad29fe0677
Anton Midyukov <antohami@altlinux.org>
2025-01-27 18:58:47 +0300

    memtest: add memtest86+ to stage1 and base system
    Not needed in stage2.

--- features.in/memtest/config.mk
+++ features.in/memtest/config.mk
@@ -1,7 +1,8 @@
 ifeq (,$(filter-out i586 x86_64 loongarch64,$(ARCH)))
 use/memtest: use/syslinux use/grub
 	@$(call add_feature)
-	@$(call add,SYSTEM_PACKAGES,memtest86+)
+	@$(call add,BASE_PACKAGES,memtest86+)
+	@$(call add,STAGE1_PACKAGES,memtest86+)
 	@$(call add,SYSLINUX_CFG,memtest)
 	@$(call add,GRUB_CFG,memtest)
 else
