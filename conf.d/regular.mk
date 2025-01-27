2989767c330d3bd4a704e60b9f90598fdd12e9e5
Anton Midyukov <antohami@altlinux.org>
2025-01-27 18:20:38 +0300

    regular.mk: remove lists with tag "base + bench" from regular-rescue
    This is clearly unnecessary in rescue.

--- conf.d/regular.mk
+++ conf.d/regular.mk
@@ -144,7 +144,6 @@ distro/regular-rescue: distro/.regular-base mixin/regular-rescue use/rescue/rw \
 	use/grub/rescue_fm.cfg use/grub/rescue_remote.cfg \
 	use/mediacheck use/stage2/kms +wireless
 	@$(call add,RESCUE_PACKAGES,gpm livecd-net-eth)
-#	@$(call add,RESCUE_LISTS,$(call tags,base bench))
 	@$(call add,RESCUE_LISTS,$(call tags,network security))
 
 distro/regular-rescue-live: distro/.regular-base +systemd use/l10n \
