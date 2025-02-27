d476d635004b9e4266e37c1824b75ffc00f52f46
Anton Midyukov <antohami@altlinux.org>
2025-02-27 08:50:57 +0300

    engineering.mk: replace mate with gnome

--- conf.d/engineering.mk
+++ conf.d/engineering.mk
@@ -1,7 +1,7 @@
 ifeq (distro,$(IMAGE_CLASS))
 distro/regular-engineering-live: distro/.regular-x11 +systemd +systemd-optimal \
-	mixin/regular-desktop mixin/regular-mate use/x11/lightdm/gtk  \
-	use/l10n +nm-gtk +plymouth \
+	mixin/regular-desktop mixin/regular-gnome use/x11/gdm  \
+	use/l10n +nm-gtk4 +plymouth \
 	use/live/ru use/live/rw use/live/desktop \
 	use/cleanup/live-no-cleanupdb \
 	use/cleanup/live-no-cleanup-docs
@@ -15,6 +15,6 @@ ifeq (,$(filter-out i586 x86_64 aarch64,$(ARCH)))
 endif
 	@$(call add,LIVE_LISTS,engineering/eda)
 	@$(call add,LIVE_LISTS,engineering/misc)
-	@$(call add,LIVE_PACKAGES,theme-mate-windows)
+	@$(call add,LIVE_PACKAGES,theme-gnome-windows)
 
 endif
