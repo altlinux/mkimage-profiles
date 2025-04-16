76fb8501a3abfddfd2f83ed9a334eb28ac9162bb
Anton Midyukov <antohami@altlinux.org>
2025-04-16 06:34:05 +0300

    repo: proxy variables for main repo
    These variables are used in scripts.d/03-genbasedir, but are not
    assigned anywhere.

--- features.in/repo/lib/50-genbasedir.mk
+++ features.in/repo/lib/50-genbasedir.mk
@@ -1,2 +1,9 @@
 # ../scripts.d/01-genbasedir needs that
 CHROOT_PACKAGES += apt-utils
+
+# Proxy for main repo
+INFO_ARCH := $(ARCH) noarch
+INFO_NAME := $(META_APP_ID)
+INFO_VERSION := $(DISTRO_VERSION)
+INFO_ORIGIN := $(META_PUBLISHER)
+INFO_LABEL := $(BRANCH)
