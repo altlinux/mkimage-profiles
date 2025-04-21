f5d2e95afc19740851d5c913f2bb4167e3cfd81b
Anton Midyukov <antohami@altlinux.org>
2025-04-21 16:22:01 +0300

    auto-resize: add xfsprogs
    xfsprogs needed for initrd feature kickstart.

--- features.in/auto-resize/config.mk
+++ features.in/auto-resize/config.mk
@@ -1,5 +1,5 @@
 use/auto-resize:
 	@$(call add_feature)
 	@$(call add,BASE_PACKAGES,make-initrd-kickstart)
-	@$(call add,BASE_PACKAGES,f2fs-tools)
+	@$(call add,BASE_PACKAGES,f2fs-tools xfsprogs)
 	@$(call add,VM_INITRDFEATURES,kickstart)
