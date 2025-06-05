f4ac3f5d46d4b312bb3aeb16012963317ca77d2f
Anton Midyukov <antohami@altlinux.org>
2025-06-05 15:31:35 +0300

    net-usershares: remove UMASK for home dirs to 076
    These permissions must be set by the user himself if necessary.
    Thanks Mikhail Efremov sem@ and Sergey V Turchin zerg@.
    Link: https://bugzilla.altlinux.org/44144

--- features.in/net-usershares/config.mk
+++ features.in/net-usershares/config.mk
@@ -1,5 +1,10 @@
 # requires thunar-shares-plugin or mate-user-share to make sense
-use/net-usershares: use/services use/deflogin
-	@$(call add,GROUPS,sambashare)
-	@$(call add,DEFAULT_SERVICES_ENABLE,smb nmb)
-	@$(call add,THE_PACKAGES,libshell)
+use/net-usershares: use/control use/services \
+	@$(call add,CONTROL,libnss-role:enabled)
+	@$(call add,CONTROL,role-usershares:enabled)
+	@$(call add,CONTROL,smb-conf-usershares:enabled)
+	@$(call add,SYSTEMD_SERVICES_ENABLE,nmb.service)
+	@$(call add,SYSTEMD_SERVICES_ENABLE,smb.service)
+	@$(call add,THE_PACKAGES,alterator-roles-common)
+	@$(call add,THE_PACKAGES,libnss-role)
+	@$(call add,THE_PACKAGES,samba-usershares)
