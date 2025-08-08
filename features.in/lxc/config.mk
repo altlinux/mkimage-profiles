253bd1050fa7488c81bafeb1da2768223382d3af
Anton Midyukov <antohami@altlinux.org>
2025-08-08 09:43:33 +0300

    lxc: do not use udev-rule-generator-net
    For livecd-net-eth it doesn't matter what names to use. So it's better
    to use those assigned by udev.

--- features.in/lxc/config.mk
+++ features.in/lxc/config.mk
@@ -9,4 +9,3 @@ use/lxc/lxd: use/lxc
 	@$(call add,LIVE_LISTS,openssh)
 	@$(call add,LIVE_PACKAGES,su)
 	@$(call add,LIVE_PACKAGES,livecd-net-eth)
-	@$(call add,LIVE_PACKAGES,udev-rule-generator-net)
