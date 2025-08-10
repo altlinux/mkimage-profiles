bb3408169b8c8dbe6fd7ff941887c6cffaa86998
Anton Midyukov <antohami@altlinux.org>
2025-08-10 09:18:03 +0300

    live-install: use INSTALL2_PACKAGES variable instead LIVE_PACKAGES

--- features.in/live-install/config.mk
+++ features.in/live-install/config.mk
@@ -10,23 +10,23 @@ use/live-install: use/live use/metadata use/repo/main \
 	@$(call set,STAGE2_LIVE_INST,yes)
 	@$(call try,MAIN_KERNEL_SAVE,no)
 	@$(call xport,MAIN_KERNEL_SAVE)
-	@$(call add,LIVE_PACKAGES,installer-common-stage2)
+	@$(call add,INSTALL2_PACKAGES,installer-common-stage2)
 	@$(call add,THE_PACKAGES,alterator-wizardface)
 	@$(call add,THE_LISTS,$(call tags,basesystem && !alterator))
 	@$(call add,THE_PACKAGES,e2fsprogs mdadm lvm2 cryptsetup)
 	@$(call add,BASE_PACKAGES,make-initrd-mdadm make-initrd-lvm)
 	@$(call add,BASE_PACKAGES,make-initrd-luks)
-	@$(call add,LIVE_PACKAGES,$$(LIVE_INSTALL_PKG))
+	@$(call add,INSTALL2_PACKAGES,$$(LIVE_INSTALL_PKG))
 	@$(call add,THE_PACKAGES,alterator-postinstall) # for auto install
-	@$(call add,LIVE_PACKAGES,xterm) # for vnc support
+	@$(call add,INSTALL2_PACKAGES,xterm) # for vnc support
 	@$(call try,INSTALLER,regular)	# might be replaced later
-	@$(call add,LIVE_PACKAGES,installer-distro-$$(INSTALLER)-stage2)
-	@$(call add,LIVE_PACKAGES,branding-$$(BRANDING)-alterator)
+	@$(call add,INSTALL2_PACKAGES,installer-distro-$$(INSTALLER)-stage2)
+	@$(call add,INSTALL2_PACKAGES,branding-$$(BRANDING)-alterator)
 	@$(call add,THE_PACKAGES,branding-$$(BRANDING)-release)
 	@$(call add,BASE_PACKAGES,installer-distro-$$(INSTALLER)-stage3)
-	@$(call add,LIVE_PACKAGES,glibc-gconv-modules) # for guile22
-	@$(call add,LIVE_PACKAGES,curl) # for net install
-	@$(call add,LIVE_PACKAGES,lsof) # for debug alterator-vm
+	@$(call add,INSTALL2_PACKAGES,glibc-gconv-modules) # for guile22
+	@$(call add,INSTALL2_PACKAGES,curl) # for net install
+	@$(call add,INSTALL2_PACKAGES,lsof) # for debug alterator-vm
 	@$(call try,LIVE_INSTALL_PKG,installer-livecd-install)
 	@$(call try,GLOBAL_LIVE_INSTALL,1)
 	@$(call xport,BASE_BOOTLOADER)
@@ -41,7 +41,7 @@ use/live-install/pkg: use/live-install
 
 ifneq (,$(filter-out p10,$(BRANCH)))
 use/live-install/desktop: use/live-install
-	@$(call add,LIVE_PACKAGES,installer-common-desktop)
+	@$(call add,INSTALL2_PACKAGES,installer-common-desktop)
 	@$(call add,BASE_PACKAGES,installer-alterator-livecd-stage3)
 else
 use/live-install/desktop: use/live-install; @:
@@ -49,7 +49,7 @@ endif
 
 # set up remote repositories within installed system out-of-box
 use/live-install/repo: use/live-install; @:
-	@$(call add,LIVE_PACKAGES,installer-feature-online-repo)
+	@$(call add,INSTALL2_PACKAGES,installer-feature-online-repo)
 
 # this one expects external vncviewer to come
 use/live-install/vnc/listen: use/live-install \
@@ -67,6 +67,6 @@ use/live-install/suspend:
 	@$(call add,BASE_PACKAGES,installer-feature-desktop-suspend-stage2)
 
 use/live-install/oem: use/live-install
-	@$(call add,LIVE_PACKAGES,installer-feature-oem-stage2)
+	@$(call add,INSTALL2_PACKAGES,installer-feature-oem-stage2)
 	@$(call add,MAIN_PACKAGES,alterator-setup)
 	@$(call add,MAIN_PACKAGES,installer-feature-alterator-setup-stage2)
